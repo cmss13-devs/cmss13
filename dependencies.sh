ae3a293ae8ee0f4c537192a97ce316f98ec22fb5
Nanu308 <1784456-Nanu308@users.noreply.gitlab.com>
2022-02-19 20:28:38 +0000

    CI GitHub

--- dependencies.sh
+++ dependencies.sh
@@ -7,8 +7,18 @@
 export BYOND_MAJOR="513"
 export BYOND_MINOR="1542"
 
+#rust_g git tag
+export RUST_G_VERSION=0.4.7
+
 #node version
 export NODE_VERSION=12
+export NODE_VERSION_PRECISE=12.20.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.5
+export SPACEMAN_DMM_VERSION=suite-1.7.1
+
+# Extools git tag
+export EXTOOLS_VERSION=v0.0.7
+
+# Python version for mapmerge and other tools
+export PYTHON_VERSION=3.6.8

654050e2f5e02948e8dd817b1616185e7a02c5e1
Watermelon914 <3052169-Watermelon914@users.noreply.gitlab.com>
2022-04-16 14:33:24 +0100

    Modifications

--- global_variable_time.py
+++ global_variable_time.py
@@ -5,12 +5,21 @@ var_array = []
 for file in glob.glob("./**/*.dm", recursive=True):
     # I want to record all variable instances into an array called var_array
     with open(file, "r", encoding="utf-8") as f:
+        info = []
         for line in f.readlines():
-            search = re.search(r"^/*var/(global/|)list/([a-zA-Z_]+/)+([a-zA-Z_]+)", line)
+            search = re.search(r"^/*var/(global/|)list/([a-zA-Z_/]+)/+([a-zA-Z_]+) = (.*)", line)
             if search is None:
+                info.append(line)
                 continue
             var_array.append(search.group(3))
+            if(search.group(2) == "global"):
+                info.append(line)
+                continue
+            info.append("GLOBAL_LIST_INIT_TYPED({}, /{}, {})".format(search.group(3), search.group(2), search.group(4)))
 
+        print(info)
+        with open(file, "w", encoding="utf-8") as f:
+            f.write(line.replace(search.group(3), "var_" + search.group(3)))
 
 
 

94bcbc89e2bef75fe29ecbb4ac24c8912d84288a
PaulMullen <mullenpaul90@gmail.com>
2022-09-15 01:18:42 +0100

    got icons resized and added search bar

--- tgui/packages/tgui/interfaces/VendingSorted.tsx
+++ tgui/packages/tgui/interfaces/VendingSorted.tsx
@@ -1,7 +1,12 @@
-import { useBackend } from '../backend';
-import { Button, Section, Flex, Box, Tooltip } from '../components';
+import { useBackend, useLocalState } from '../backend';
+import { Button, Section, Flex, Box, Tooltip, TextArea, NoticeBox, Input } from '../components';
 import { Window } from '../layouts';
 import { logger } from '../logging';
+
+const THEME_COMP = 0;
+const THEME_USCM = 1;
+const THEME_CLF = 2;
+const THEME_UPP = 3;
 
 const VENDOR_ITEM_REGULAR = 1
 const VENDOR_ITEM_MANDATORY = 2
@@ -40,6 +45,7 @@ type VenableItem = {
 }
 
 const VendableItem = (props: VenableItem, context) => {
+  const [searchTerm, _] = useLocalState(context, 'searchTerm', "");
   const { act } = useBackend(context);
   const { record } = props;
   const available = record.prod_available > 0
@@ -48,10 +54,18 @@ const VendableItem = (props: VenableItem, context) => {
     : record.prod_color == VENDOR_ITEM_MANDATORY
       ? "orange"
       : "green";
+  if (!record.prod_name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+    return <></>
+  }
+  const vendstyle = {
+    textAlign: 'center',
+    margin: '0 auto',
+    display: 'block'
+  }
   return (
     <Flex align="center" justify="space-between" align-items="stretch">
       <Flex.Item>
-        <img src={record.prod_icon.href}/>
+        <img src={record.prod_icon.href} style={{width: "150%"}}/>
       </Flex.Item>
 
       <Flex.Item>
@@ -77,34 +91,78 @@ const VendableItem = (props: VenableItem, context) => {
       <Flex.Item justify="right">
         <Button
           style={{textAlign: 'center'}}
+          icon="eject"
           onClick={() => act('vend', record)}
           disabled={!available}
-          width="80px">
-          {available ? "vend" : "SOLD OUT"}
-        </Button>
+          width="90px">
+            {available ? "vend" : "SOLD OUT"}
+            </Button>
       </Flex.Item>
     </Flex>)
 }
 
+type VendingCategoryProps = {
+  category: VendingCategory;
+  key: any;
+}
+
+export const ViewVendingCategory = (props: VendingCategoryProps, context) => {
+  const {category, key} = props;
+
+  return <Section title={category.name ?? ""} key={key}>
+    <Flex direction="column">
+      {category.items.map(record => (
+        <Flex.Item mb={1} key={record.prod_index}>
+          <Tooltip position="bottom" content={record.prod_desc}>
+            <VendableItem record={record}/>
+          </Tooltip>
+        </Flex.Item>)
+      )}
+    </Flex>
+  </Section>
+}
+
+
+function getTheme(value: string|number): string {
+  switch (value) {
+    case THEME_UPP:
+      return "abductor"
+    case THEME_CLF:
+      return "retro"
+    case THEME_COMP:
+      return "weyland"
+    default:
+      return "usmc"
+  }
+}
 
 export const VendingSorted = (_, context) => {
   const { data } = useBackend<VendingData>(context);
   const categories = data.displayed_categories ?? []
-  return (<Window width={600} height={700}>
+  const [searchTerm, setSearchTerm] = useLocalState(context, 'searchTerm', "");
+  return (<Window
+      width={600}
+      height={700}
+      theme={getTheme(data.theme)}
+    >
     <Window.Content scrollable={true}>
-        {categories.map((category, i) => (
-          <Section title={category.name ?? ""} key={i}>
-            <Flex direction="column">
-              {category.items.map(record => (
-                <Flex.Item mb={1} key={record.prod_index}>
-                  <Tooltip position="bottom" content={record.prod_desc}>
-                    <VendableItem record={record}/>
-                  </Tooltip>
-                </Flex.Item>)
-              )}
-            </Flex>
-          </Section>
-        ))}
+      <Box className='Section'>
+        <Flex align="center" justify="space-between" align-items="stretch" className="Section__title">
+          <Flex.Item>
+              <span className="Section__titleText">Search</span>
+          </Flex.Item>
+          <Flex.Item>
+            <Input
+                style={{border: '1px solid white', color: 'white'}}
+
+                value={searchTerm}
+                onInput={(_, value) => setSearchTerm(value)}
+                width="160px"
+              />
+          </Flex.Item>
+        </Flex>
+      </Box>
+        {categories.map((category, i) => (<ViewVendingCategory category={category} key={i}/>))}
     </Window.Content>
   </Window>)
 }

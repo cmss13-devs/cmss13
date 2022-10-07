ea39dbe4f3daea420c8e581f93872cfc28bb6340
Stan_Albatross <stant9@gmx.com>
2022-10-07 20:23:11 +0100

    font size

--- tgui/packages/tgui/interfaces/STUI.js
+++ tgui/packages/tgui/interfaces/STUI.js
@@ -1,5 +1,5 @@
 import { useBackend, useLocalState } from '../backend';
-import { Tabs, Section, Box, Flex, Input } from '../components';
+import { Tabs, Section, Box, Flex, Input, Slider, LabeledList } from '../components';
 import { Window } from '../layouts';
 
 export const STUI = (props, context) => {
@@ -9,9 +9,18 @@ export const STUI = (props, context) => {
     selectedTab,
     setSelectedTab,
   ] = useLocalState(context, 'progress', tabs[0]);
-  const logs = data.logs[selectedTab].length ? data.logs[selectedTab] : ["No logs"];
+  const logs = data.logs[selectedTab].length ? data.logs[selectedTab] : [""];
 
   const [searchTerm, setSearchTerm] = useLocalState(context, 'searchTerm', "");
+
+  const [logsfontnumber, setLogsFontSize]
+  = useLocalState(context, "logsfontnumber", 7);
+  const logsfontsize = logsfontnumber + "pt";
+
+  const bigfontnumber = logsfontnumber + 5;
+  const bigfontsize = bigfontnumber + "pt";
+
+
 
   return (
     <Window
@@ -24,22 +33,29 @@ export const STUI = (props, context) => {
               <STUItabs />
             </Section>
           </Flex.Item>
+          <Box height="10px" />
           <Flex.Item>
-            <Box width="10px" />
-            <Box>
-              <Flex align="center" justify="space-between" align-items="stretch">
-                <Flex.Item>
-                  Search:
-                </Flex.Item>
-                <Flex.Item>
-                  <Input
-                    value={searchTerm}
-                    onInput={(_, value) => setSearchTerm(value)}
-                    width="840px"
-                  />
-                </Flex.Item>
-              </Flex>
-            </Box>
+            <LabeledList>
+              <LabeledList.Item label="Search">
+                <Input
+                  value={searchTerm}
+                  onInput={(_, value) => setSearchTerm(value)}
+                  width="810px"
+                />
+              </LabeledList.Item>
+              <LabeledList.Item label="Font Size">
+                <Slider
+                  inline
+                  maxValue={20}
+                  minValue={4}
+                  value={logsfontnumber}
+                  onChange={(e, value) => setLogsFontSize(value)}
+                  unit="pt"
+                  step={0.5}
+                  stepPixelSize={20}
+                />
+              </LabeledList.Item>
+            </LabeledList>
           </Flex.Item>
           <Flex.Item mt={1} grow={1} basis={0}>
             <Section fill scrollable >
@@ -48,11 +64,13 @@ export const STUI = (props, context) => {
                   return;
                 }
                 return (
-                  <Box key={log}>{log}</Box>
+                  <Box fontSize={logsfontsize} key={log}>{log}</Box>
                 );
               })}
               {logs.length > 1 && (
-                <Box align="center">--- End of Logs --</Box>
+                <Box align="center" fontSize={bigfontsize}>--- End of Logs --</Box>
+              ) || (
+                <Box align="center" fontSize={bigfontsize}>--- No Logs --</Box>
               )}
             </Section>
           </Flex.Item>

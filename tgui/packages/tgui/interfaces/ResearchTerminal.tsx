a15deb26dbab2295d9ce9e7be5d9f3e96e20db9e
paulrpg <mullenpaul90@gmail.com>
2022-10-15 01:08:25 +0100

    reworked research terminal into yellow crt

--- tgui/packages/tgui/interfaces/ResearchTerminal.tsx
+++ tgui/packages/tgui/interfaces/ResearchTerminal.tsx
@@ -1,13 +1,15 @@
 import { useBackend, useLocalState } from '../backend';
-import { Button, Section, Flex, Box, Tooltip, Input, NoticeBox, Icon, Stack } from '../components';
+import { Button, Stack, Section, Flex, Tabs, Box } from '../components';
 import { Window } from '../layouts';
-import { classes } from 'common/react';
 import { logger } from '../logging';
 import { Table, TableCell, TableRow } from '../components/Table';
+import { classes } from '../../common/react';
+
 
 interface TerminalProps {
   "clearance_level": number;
-  "research_documents": ResearchDocs;
+  "research_documents": Map<string, {string: string}>;
+  "published_documents": Map<string, Map<string, string>>;
   "rsc_credits": number;
   "broker_cost": number;
   "base_purchase_cost": number;
@@ -16,101 +18,191 @@ interface TerminalProps {
   "clearance_x_access": number;
 }
 
-interface ResearchDocs {
-
-}
-
-const ClearanceController = (_, context) => {
-  const { data, act } = useBackend<TerminalProps>(context);
-  return (<Box>
-    <Stack>
-      <Stack.Item>
-        Clearance {data.clearance_level}
-      </Stack.Item>
-      <Stack.Item>
-        <Button onClick={() => act("broker_clearance")}>Purchase Clearance</Button>
-      </Stack.Item>
-      <Stack.Item>
-        <Button onClick={() => act("request_clearance_x_access")}>Purchase X</Button>
-      </Stack.Item>
-    </Stack>
-
-  </Box>)
-}
-
 const PurchaseDocs = (_, context) => {
   const { data, act } = useBackend<TerminalProps>(context);
-  return (<Box>
-    <Stack>
-      <Stack.Item>
-        <Button onClick={() => act("purchase_document", {purchase_tier: 1})}>1</Button>
-      </Stack.Item>
-      <Stack.Item>
-        <Button onClick={() => act("purchase_document", {purchase_tier: 2})}>2</Button>
-      </Stack.Item>
-      <Stack.Item>
-        <Button onClick={() => act("purchase_document", {purchase_tier: 3})}>3</Button>
-      </Stack.Item>
-      <Stack.Item>
-        <Button onClick={() => act("purchase_document", {purchase_tier: 4})}>4</Button>
-      </Stack.Item>
-      <Stack.Item>
-        <Button onClick={() => act("purchase_document", {purchase_tier: 5})}>5</Button>
-      </Stack.Item>
-    </Stack>
-
-  </Box>)
-}
+  const clearance_level = data.clearance_level;
+  const all_levels = ["1", "2", "3", "4", "5"];
+  const costs = { "1": 7, "2": 9, "3": 11, "4": 13, "5": 15 };
+  const available_levels = Array.from(Array(clearance_level).keys())
+    .map(x => (x + 1).toString());
+  return (
+    <Section title="Purchase Reports">
+      <Flex wrap justify="space-between">
+        {all_levels.map(x => (
+          <Flex.Item key={x} xs={3}>
+            <Button
+              className={classes(["DoButton", !available_levels.includes(x) && "HiddenButton"])}
+              disabled={!available_levels.includes(x)}
+              onClick={() => act("purchase_document", { purchase_tier: x })}
+            >
+              Level {x} ${costs[x]}
+            </Button>
+          </Flex.Item>))}
+      </Flex>
+    </Section>);
+};
 
 const CompoundTable = (_, context) => {
   const { data, act } = useBackend<TerminalProps>(context);
-  const keys = Object.keys(data.research_documents)[0]
-  const scans = data.research_documents[keys]
-  const formulas = Object.keys(scans)
-  return (<Table>
-    <TableRow>
-      <TableCell>Compound</TableCell>
-      <TableCell>Actions</TableCell>
-    </TableRow>
-    {formulas.map(x => (<TableRow>
-        <TableCell>{scans[x]}</TableCell>
-        <TableCell>
-          <Button onClick={() => act("read_document", {"print_type": keys, "print_title": x})}>
-            read
-          </Button>
-          <Button onClick={() => act("print", {"print_type": keys, "print_title": x})}>
-            print
-          </Button>
-          <Button onClick={() => act("publish_document", {"print_type": keys, "print_title": x})}>
-            publish
-          </Button>
-          <Button onClick={() => act("unpublish_document", {"print_type": keys, "print_title": x})}>
-            unpublish
-          </Button>
-        </TableCell>
-    </TableRow>))}
-  </Table>)
-}
+  const isMainTerminal = data.main_terminal === 1;
+  const hasScans = (docs) => Object.keys(docs).includes('XRF Scans');
 
-export const ResearchTerminal = (props, context) => {
+  const published = !hasScans(data.published_documents)
+    ? []
+    : Array.from(Object.keys(data.published_documents['XRF Scans']));
+
+  const documents = !hasScans(data.research_documents)
+    ? []
+    : Array.from(Object.keys(data.research_documents['XRF Scans']));
+
+  const researchDocs = documents.map(x => {
+    return { id: x, type: data.research_documents['XRF Scans'][x], isPublished: published.includes(x) };
+  });
+
+  logger.info(researchDocs);
+  return (
+    <Section>
+      <Table>
+        <TableRow>
+          <TableCell textAlign="center">
+            <span>Compound</span>
+          </TableCell>
+          <TableCell textAlign="center">
+            <span>Actions</span>
+          </TableCell>
+        </TableRow>
+        {researchDocs.map(x =>
+          (
+            <TableRow key={x.id}>
+              <TableCell>{x.type}</TableCell>
+              <TableCell>
+                <Button onClick={() => act("read_document", { "print_type": 'XRF Scans', "print_title": x.id })}>
+                  read
+                </Button>
+                <Button onClick={() => act("print", { "print_type": 'XRF Scans', "print_title": x.id })}>
+                  print
+                </Button>
+                {isMainTerminal
+                  && (
+                    <Button onClick={() => act("publish_document", { "print_type": x.id, "print_title": x.type })}>
+                      publish
+                    </Button>)}
+                {isMainTerminal && x.isPublished
+                  && (
+                    <Button onClick={() => act("unpublish_document", { "print_type": x.id, "print_title": x.type })}>
+                      unpublish
+                    </Button>)}
+              </TableCell>
+            </TableRow>
+          )
+        )}
+      </Table>
+    </Section>);
+};
+
+const ResearchManager = (_, context) => {
   const { data, act } = useBackend<TerminalProps>(context);
-  logger.info(data)
-  return (<Window
-      height={800}
+  const clearance_level = data.clearance_level;
+  const x_access = data.clearance_x_access;
+  return (
+    <Box>
+      <Stack vertical>
+        <Stack.Item>
+          <span>
+            Credits available: {data.rsc_credits}
+          </span>
+        </Stack.Item>
+        <hr className="sep" />
+        <Stack.Item>
+          <Flex justify="space-between" align="center">
+            {x_access === 0
+              && (
+                <Flex.Item>
+                  <span>Upgrade Clearance</span>
+                </Flex.Item>)}
+            {x_access !== 0
+              && (
+                <Flex.Item>
+                  <span>Maximum clearance reached</span>
+                </Flex.Item>)}
+            {clearance_level < 5
+              && (
+                <Flex.Item>
+                  <Button
+                    className="DoButton"
+                    disabled={clearance_level === 5}
+                    onClick={() => act("broker_clearance")}
+                  >
+                    Improve ({data.broker_cost})
+                  </Button>
+                </Flex.Item>)}
+            {(clearance_level === 5 && x_access === 0)
+              && (
+                <Flex.Item>
+                  <Button
+                    className="DoButton"
+                    onClick={() => act("request_clearance_x_access")}
+                  >
+                    Request X (5)
+                  </Button>
+                </Flex.Item>)}
+          </Flex>
+        </Stack.Item>
+      </Stack>
+      <hr className="sep" />
+      <PurchaseDocs />
+      <hr className="sep" />
+    </Box>
+  );
+};
+
+const ResearchOverview = (_, context) => {
+  const [selectedTab, setSelectedTab] = useLocalState(context, 'research_tab', 1);
+  return (
+    <>
+      <Tabs fluid fill >
+        <Tabs.Tab
+          selected={selectedTab === 1}
+          onClick={() => setSelectedTab(1)}
+          icon="gear"
+          color="black"
+        >
+          Manage Research
+        </Tabs.Tab>
+        <Tabs.Tab
+          selected={selectedTab === 2}
+          onClick={() => setSelectedTab(2)}
+          icon="flask"
+          color="black"
+        >
+          View Chemicals
+        </Tabs.Tab>
+      </Tabs>
+      {selectedTab === 1 && <ResearchManager />}
+      {selectedTab === 2 && <CompoundTable />}
+    </>
+  );
+};
+
+export const ResearchTerminal = (_, context) => {
+  const { data, act } = useBackend<TerminalProps>(context);
+  const [sharpen, setSharpen] = useLocalState(context, 'sharpen', 0);
+  logger.info(data);
+  return (
+    <Window
+      theme="crt"
+      height={400}
       width={400}
     >
-      <Window.Content scrollable>
-        <Stack vertical>
-          <Stack.Item>
-            <ClearanceController/>
-          </Stack.Item>
-          <Stack.Item>
-            <PurchaseDocs />
-          </Stack.Item>
-          <Stack.Item>
-            <CompoundTable />
-          </Stack.Item>
-        </Stack>
+      <Window.Content scrollable className={classes([!sharpen && 'crt'])}>
+        <Section title={`Clearance Level ${data.clearance_level}`} className="container">
+          <ResearchOverview />
+        </Section>
+        <Section>
+          <Button className="DoButton" onClick={() => setSharpen(sharpen === 0 ? 1 : 0)}>
+            Toggle Sharpen
+          </Button>
+        </Section>
       </Window.Content>
-    </Window>)
-}
+    </Window>);
+};

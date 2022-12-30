16da3a14b6abea8f886e93b0c0c7e408619d6346
harryob <me@harryob.live>
2022-12-30 15:33:51 +0000

    makes the tests run

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -9,9 +9,6 @@ tools/deploy.sh ci_test
 mkdir ci_test/config
 mkdir ci_test/data
 
-#test config
-cp tools/ci/ci_config.txt ci_test/config/config.txt
-
 #set the map
 cp maps/$MAP.json ci_test/data/next_map.json
 cp maps/almayer.json ci_test/data/next_ship.json

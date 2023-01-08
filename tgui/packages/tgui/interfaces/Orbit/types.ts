29f8ccb37c27ad87b324d871ef3900ebe09cd82c
harryob <me@harryob.live>
2023-01-08 19:58:09 +0000

    orbit update

---
+++ tgui/packages/tgui/interfaces/Orbit/types.ts
@@ -0,0 +1,27 @@
+import { BooleanLike } from 'common/react';
+
+export type OrbitData = {
+  auto_observe: BooleanLike;
+  humans: Observable[];
+  marines: Observable[];
+  survivors: Observable[];
+  xenos: Observable[];
+  ert_members: Observable[];
+  synthetics: Observable[];
+  predators: Observable[];
+  dead: Observable[];
+  ghosts: Observable[];
+  misc: Observable[];
+  npcs: Observable[];
+  vehicles: Observable[];
+};
+
+export type Observable = {
+  caste?: string;
+  health?: number;
+  job?: string;
+  full_name: string;
+  nickname?: string;
+  orbiters?: number;
+  ref: string;
+};

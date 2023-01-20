ea91ecbec884a4ded4b48817b70c3ebc811ac556
Stan_Albatross <swt91a@gmail.com>
2023-01-20 13:20:43 +0000

    init changes, will figure out localecompare later

--- tgui/packages/tgui/interfaces/Autolathe.js
+++ tgui/packages/tgui/interfaces/Autolathe.js
@@ -39,6 +39,7 @@ const MaterialsData = (props, context) => {
     printables,
     selectable_categories,
     queuemax,
+    currently_making,
   } = data;
 
   return (
@@ -57,7 +58,7 @@ const MaterialsData = (props, context) => {
           </Flex.Item>
         ))}
       </Flex>
-      <CurrentlyMaking />
+      {currently_making ? <CurrentlyMaking /> : null}
     </Section>
   );
 };
@@ -66,22 +67,19 @@ const CurrentlyMaking = (props, context) => {
   const { act, data } = useBackend(context);
   const { currently_making } = data;
 
-  return currently_making ? (
-    <Fragment>
+  const MakingName =
+    'Currently making:' +
+    capitalize(currently_making.name) +
+    (currently_making.multiplier > 1
+      ? ' (x' + currently_making.multiplier + ')'
+      : '');
+
+  return (
+    <>
       <Box height="5px" />
-      <Button
-        fluid
-        textAlign="center"
-        content={
-          'Currently making: ' +
-          capitalize(currently_making.name) +
-          (currently_making.multiplier > 1
-            ? ' (x' + currently_making.multiplier + ')'
-            : '')
-        }
-      />
-    </Fragment>
-  ) : null;
+      <Button fluid textAlign="center" content={MakingName} />
+    </>
+  );
 };
 
 const QueueList = (props, context) => {
@@ -98,38 +96,36 @@ const QueueList = (props, context) => {
   return (
     <Section title="Queue">
       <Flex direction="column">
-        {queued.map((item) => {
-          return (
-            <Flex.Item key={item}>
-              <Flex direction="row">
-                <Flex.Item>
-                  <Button
-                    content={
-                      item.index +
-                      ': ' +
-                      capitalize(item.name) +
-                      (item.multiplier > 1 ? ' (x' + item.multiplier + ')' : '')
-                    }
-                  />
-                  <Box width="5px" />
-                </Flex.Item>
-                <Flex.Item>
-                  <Button
-                    icon="xmark"
-                    tooltip="Remove"
-                    onClick={() =>
-                      act('cancel', {
-                        index: item.index,
-                        name: item.name,
-                        multiplier: item.multiplier,
-                      })
-                    }
-                  />
-                </Flex.Item>
-              </Flex>
-            </Flex.Item>
-          );
-        })}
+        {queued.map((item, index) => (
+          <Flex.Item key={index}>
+            <Flex direction="row">
+              <Flex.Item>
+                <Button
+                  content={
+                    item.index +
+                    ': ' +
+                    capitalize(item.name) +
+                    (item.multiplier > 1 ? ' (x' + item.multiplier + ')' : '')
+                  }
+                />
+                <Box width="5px" />
+              </Flex.Item>
+              <Flex.Item>
+                <Button
+                  icon="xmark"
+                  tooltip="Remove"
+                  onClick={() =>
+                    act('cancel', {
+                      index: item.index,
+                      name: item.name,
+                      multiplier: item.multiplier,
+                    })
+                  }
+                />
+              </Flex.Item>
+            </Flex>
+          </Flex.Item>
+        ))}
       </Flex>
     </Section>
   );
@@ -157,12 +153,9 @@ const PrintablesSection = (props, context) => {
   );
 
   const categories = [];
-  for (let i = 0; i < printables.length; i++) {
-    let data = printables[i];
-    if (categories.includes(data.recipe_category)) continue;
-
-    categories.push(data.recipe_category);
-  }
+  printables
+    .filter((x) => categories.includes(x.recipe_category))
+    .map((x) => x.recipe_category);
 
   const [currentCategory, setCategory] = useLocalState(
     context,
@@ -203,65 +196,63 @@ const PrintablesSection = (props, context) => {
         </Stack.Item>
         <Stack.Item grow>
           <Flex direction="column">
-            {filteredPrintables.map((val, index) => {
-              return (
-                <Flex.Item key={index}>
-                  <Box>
-                    <Flex direction="row">
-                      <Flex.Item grow>
-                        <Button
-                          fluid
-                          content={capitalize(val.name)}
-                          disabled={!val.can_make}
-                          color={val.hidden ? 'red' : null}
-                          onClick={() =>
-                            act('make', {
-                              index: val.index,
-                              multiplier: 1,
-                            })
-                          }>
-                          {' (' + // sorry for this shitcode, also yes this will break if an autolathe uses more than 2 material types
-                            (val.materials[Object.keys(materials)[0]] &&
+            {filteredPrintables.map((val, index) => (
+              <Flex.Item key={index}>
+                <Box>
+                  <Flex direction="row">
+                    <Flex.Item grow>
+                      <Button
+                        fluid
+                        content={capitalize(val.name)}
+                        disabled={!val.can_make}
+                        color={val.hidden ? 'red' : null}
+                        onClick={() =>
+                          act('make', {
+                            index: val.index,
+                            multiplier: 1,
+                          })
+                        }>
+                        {' (' + // sorry for this shitcode, also yes this will break if an autolathe uses more than 2 material types
+                          (val.materials[Object.keys(materials)[0]] &&
+                          val.materials[Object.keys(materials)[1]]
+                            ? val.materials[Object.keys(materials)[0]] +
+                            ', ' +
                             val.materials[Object.keys(materials)[1]]
-                              ? val.materials[Object.keys(materials)[0]] +
-                              ', ' +
-                              val.materials[Object.keys(materials)[1]]
-                              : val.materials[Object.keys(materials)[0]]
-                                ? val.materials[Object.keys(materials)[0]]
-                                : val.materials[Object.keys(materials)[1]]) +
-                            ') '}
-                        </Button>
-                      </Flex.Item>
+                            : val.materials[Object.keys(materials)[0]]
+                              ? val.materials[Object.keys(materials)[0]]
+                              : val.materials[Object.keys(materials)[1]]) +
+                          ') '}
+                      </Button>
+                    </Flex.Item>
 
-                      {(val.has_multipliers && (
-                        <Fragment>
-                          <Box width="2.5px" />
-                          <Flex.Item>
-                            <Flex direction="row">
-                              {Object.keys(val.multipliers).map((entry) => (
-                                <Flex.Item key={entry}>
-                                  <Button
-                                    content={'x' + entry}
-                                    onClick={() =>
-                                      act('make', {
-                                        index: val.index,
-                                        multiplier: entry,
-                                      })
-                                    }
-                                  />
-                                </Flex.Item>
-                              ))}
-                            </Flex>
-                          </Flex.Item>
-                        </Fragment>
-                      )) ||
-                        null}
-                    </Flex>
-                  </Box>
-                  <Box height="2.5px" />
-                </Flex.Item>
-              );
-            })}
+                    {(val.has_multipliers && (
+                      <Fragment>
+                        <Box width="2.5px" />
+                        <Flex.Item>
+                          <Flex direction="row">
+                            {Object.keys(val.multipliers).map((entry) => (
+                              <Flex.Item key={entry}>
+                                <Button
+                                  content={'x' + entry}
+                                  onClick={() =>
+                                    act('make', {
+                                      index: val.index,
+                                      multiplier: entry,
+                                    })
+                                  }
+                                />
+                              </Flex.Item>
+                            ))}
+                          </Flex>
+                        </Flex.Item>
+                      </Fragment>
+                    )) ||
+                      null}
+                  </Flex>
+                </Box>
+                <Box height="2.5px" />
+              </Flex.Item>
+            ))}
           </Flex>
           <Box height="10px" />
           <ElectricalPanel />

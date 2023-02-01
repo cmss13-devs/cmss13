cmss13-devs/cmss13

commit 6a58ebc68158caeffaf4e5913ab5a88ba616bc82

Revives Geeves' PR for Uncloaked Action GUI elements. (#2456)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

# About the pull request

This PR is a direct revival of
https://github.com/cmss13-devs/cmss13/pull/1842 , it fixes the issue
https://github.com/cmss13-devs/cmss13/issues/1665 .

# Explain why it's good for the game

Quoted from #1842, "[...] it was a bit silly that you could track things
down not by seeing them, but by seeing the game UI floating above their
heads. This should make it a little more engaging to track down the bad
guy while giving them a chance to Metal Gear their way out of trouble."

# Testing Photographs and Procedure

<details>
<summary>It runs and cloaked players no longer have interaction circles
above their heads. </summary>

Put screenshots and videos here with an empty line between the
screenshots and the `<details>` tags.

</details>


# Changelog

<!-- If your PR modifies aspects of the game that can be concretely
observed by players or admins you should add a changelog. If your change
does NOT meet this description, remove this section. Be sure to properly
mark your PRs to prevent unnecessary GBP loss. Please note that
maintainers freely reserve the right to remove and add tags should they
deem it appropriate. You can attempt to finagle the system all you want,
but it's best to shoot for clear communication right off the bat. -->
<!-- If you add a name after the ':cl', that name will be used in the
changelog. You must add your CKEY after the CL if your GitHub name
doesn't match. Be sure to properly mark your PRs to prevent unnecessary
GBP loss. Maintainers freely reserve the right to remove and add tags
should they deem it appropriate. -->

:cl: LynxSolstice, Geeves
balance: interaction circles that appear of above your head while
cloaked are now harder to see (does not include circles above interacted

code/__HELPERS/unsorted.dm
@@ -1043,8 +1043,8 @@ var/global/image/action_purple_power_up
 		has_target = TRUE
 
 	// Only living mobs can perform timed actions.
-	var/mob/living/L = user
-	if(!istype(L))
+	var/mob/living/busy_user = user
+	if(!istype(busy_user))
 		return FALSE
 
 	// This var will only be used for checks that require target to be living.
@@ -1054,12 +1054,15 @@ var/global/image/action_purple_power_up
 		target_is_mob = TRUE
 
 	var/image/busy_icon
+	var/has_busy_icon = FALSE
 	if(show_busy_icon)
 		busy_icon = get_busy_icon(show_busy_icon)
 		if(busy_icon)
 			busy_icon.appearance_flags = RESET_ALPHA|KEEP_APART
 			busy_icon.alpha = 255
-			L.overlays += busy_icon
+			if(busy_user.alpha == 255)
+				busy_user.overlays += busy_icon
+				has_busy_icon = TRUE
 
 	var/image/target_icon
 	if(show_target_icon) //putting a busy overlay on top of the target
@@ -1070,28 +1073,28 @@ var/global/image/action_purple_power_up
 			target.overlays += target_icon
 
 	if(user_flags & BEHAVIOR_IMMOBILE)
-		L.status_flags |= IMMOBILE_ACTION
+		busy_user.status_flags |= IMMOBILE_ACTION
 
-	L.action_busy++ // target is not tethered by action, the action is tethered by target though
-	L.resisting = FALSE
-	L.clicked_something = list()
+	busy_user.action_busy++ // target is not tethered by action, the action is tethered by target though
+	busy_user.resisting = FALSE
+	busy_user.clicked_something = list()
 	if(has_target && target_is_mob)
 		T.resisting = FALSE
 		T.clicked_something = list()
 
-	var/cur_user_zone_sel = L.zone_selected
+	var/cur_user_zone_sel = busy_user.zone_selected
 	var/cur_target_zone_sel
 	var/delayfraction = Ceiling(delay/numticks)
-	var/user_orig_loc = L.loc
-	var/user_orig_turf = get_turf(L)
+	var/user_orig_loc = busy_user.loc
+	var/user_orig_turf = get_turf(busy_user)
 	var/target_orig_loc
 	var/target_orig_turf
 	if(has_target)
 		target_orig_loc = target.loc
 		target_orig_turf = get_turf(target)
-	var/obj/user_holding = L.get_active_hand()
+	var/obj/user_holding = busy_user.get_active_hand()
 	var/obj/target_holding
-	var/cur_user_lying = L.lying
+	var/cur_user_lying = busy_user.lying
 	var/cur_target_lying
 	var/expected_total_time = delayfraction*numticks
 	var/time_remaining = expected_total_time
@@ -1105,46 +1108,54 @@ var/global/image/action_purple_power_up
 	for(var/i in 1 to numticks)
 		sleep(delayfraction)
 		time_remaining -= delayfraction
-		if(!istype(L) || has_target && !istype(target)) // Checks if L exists and is not dead and if the target exists and is not destroyed
+		if(!istype(busy_user) || has_target && !istype(target)) // Checks if busy_user exists and is not dead and if the target exists and is not destroyed
 			. = FALSE
+		if(show_busy_icon)
+			if(has_busy_icon && user.alpha < 255)
+				busy_user.overlays += busy_icon
+				busy_icon.alpha = user.alpha
+				has_busy_icon = TRUE
+			else if(!has_busy_icon && user.alpha == 255)
+				busy_user.overlays += busy_icon
+				has_busy_icon = TRUE
 			break
-		if(user_flags & INTERRUPT_DIFF_LOC && L.loc != user_orig_loc || \
+		if(user_flags & INTERRUPT_DIFF_LOC && busy_user.loc != user_orig_loc || \
 			has_target && (target_flags & INTERRUPT_DIFF_LOC && target.loc != target_orig_loc)
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_DIFF_TURF && get_turf(L) != user_orig_turf || \
+		if(user_flags & INTERRUPT_DIFF_TURF && get_turf(busy_user) != user_orig_turf || \
 			has_target && (target_flags & INTERRUPT_DIFF_TURF && get_turf(target) != target_orig_turf)
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_UNCONSCIOUS && L.stat || \
+		if(user_flags & INTERRUPT_UNCONSCIOUS && busy_user.stat || \
 			target_is_mob && (target_flags & INTERRUPT_UNCONSCIOUS && T.stat)
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_KNOCKED_DOWN && L.knocked_down || \
+		if(user_flags & INTERRUPT_KNOCKED_DOWN && busy_user.knocked_down || \
 			target_is_mob && (target_flags & INTERRUPT_KNOCKED_DOWN && T.knocked_down)
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_STUNNED && L.stunned || \
+		if(user_flags & INTERRUPT_STUNNED && busy_user.stunned || \
 			target_is_mob && (target_flags & INTERRUPT_STUNNED && T.stunned)
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_DAZED && L.dazed)
+		if(user_flags & INTERRUPT_DAZED && busy_user.dazed)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_EMOTE && !L.flags_emote)
+		if(user_flags & INTERRUPT_EMOTE && !busy_user.flags_emote)
 			. = FALSE
 			break
 		if(user_flags & INTERRUPT_NEEDHAND)
 			if(user_holding)
-				if(!user_holding.loc || L.get_active_hand() != user_holding) //no longer holding the required item
+				if(!user_holding.loc || busy_user.get_active_hand() != user_holding) //no longer holding the required item
 					. = FALSE
 					break
-			else if(L.get_active_hand()) //something in active hand when we need it to stay empty
+			else if(busy_user.get_active_hand()) //something in active hand when we need it to stay empty
 				. = FALSE
 				break
 		if(target_is_mob && target_flags & INTERRUPT_NEEDHAND)
@@ -1155,65 +1166,65 @@ var/global/image/action_purple_power_up
 			else if(T.get_active_hand())
 				. = FALSE
 				break
-		if(user_flags & INTERRUPT_RESIST && L.resisting || \
+		if(user_flags & INTERRUPT_RESIST && busy_user.resisting || \
 			target_is_mob && (target_flags & INTERRUPT_RESIST && T.resisting)
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_DIFF_SELECT_ZONE && cur_user_zone_sel != L.zone_selected || \
+		if(user_flags & INTERRUPT_DIFF_SELECT_ZONE && cur_user_zone_sel != busy_user.zone_selected || \
 			target_is_mob && (target_flags & INTERRUPT_DIFF_SELECT_ZONE && cur_target_zone_sel != T.zone_selected)
 		)
 			. = FALSE
 			break
-		if((user_flags|target_flags) & INTERRUPT_OUT_OF_RANGE && target && get_dist(L, target) > max_dist)
+		if((user_flags|target_flags) & INTERRUPT_OUT_OF_RANGE && target && get_dist(busy_user, target) > max_dist)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_LCLICK && L.clicked_something["left"] || \
+		if(user_flags & INTERRUPT_LCLICK && busy_user.clicked_something["left"] || \
 			target_is_mob && (target_flags & INTERRUPT_LCLICK && T.clicked_something["left"])
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_RCLICK && L.clicked_something["right"] || \
+		if(user_flags & INTERRUPT_RCLICK && busy_user.clicked_something["right"] || \
 			target_is_mob && (target_flags & INTERRUPT_RCLICK && T.clicked_something["right"])
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_SHIFTCLICK && L.clicked_something["left"] && L.clicked_something["shift"] || \
+		if(user_flags & INTERRUPT_SHIFTCLICK && busy_user.clicked_something["left"] && busy_user.clicked_something["shift"] || \
 			target_is_mob && (target_flags & INTERRUPT_SHIFTCLICK && T.clicked_something["left"] && T.clicked_something["shift"])
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_ALTCLICK && L.clicked_something["left"] && L.clicked_something["alt"] || \
+		if(user_flags & INTERRUPT_ALTCLICK && busy_user.clicked_something["left"] && busy_user.clicked_something["alt"] || \
 			target_is_mob && (target_flags & INTERRUPT_ALTCLICK && T.clicked_something["left"] && T.clicked_something["alt"])
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_CTRLCLICK && L.clicked_something["left"] && L.clicked_something["ctrl"] || \
+		if(user_flags & INTERRUPT_CTRLCLICK && busy_user.clicked_something["left"] && busy_user.clicked_something["ctrl"] || \
 			target_is_mob && (target_flags & INTERRUPT_CTRLCLICK && T.clicked_something["left"] && T.clicked_something["ctrl"])
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_MIDDLECLICK && L.clicked_something["middle"] || \
+		if(user_flags & INTERRUPT_MIDDLECLICK && busy_user.clicked_something["middle"] || \
 			target_is_mob && (target_flags & INTERRUPT_MIDDLECLICK && T.clicked_something["middle"])
 		)
 			. = FALSE
 			break
-		if(user_flags & INTERRUPT_CHANGED_LYING && L.lying != cur_user_lying || \
+		if(user_flags & INTERRUPT_CHANGED_LYING && busy_user.lying != cur_user_lying || \
 			target_is_mob && (target_flags & INTERRUPT_CHANGED_LYING && T.lying != cur_target_lying)
 		)
 			. = FALSE
 			break
 
-	if(L && busy_icon)
-		L.overlays -= busy_icon
+	if(busy_user && busy_icon)
+		busy_user.overlays -= busy_icon
 	if(target && target_icon)
 		target.overlays -= target_icon
 
-	L.action_busy--
-	L.resisting = FALSE
+	busy_user.action_busy--
+	busy_user.resisting = FALSE
 	if(target_is_mob)
 		T.resisting = FALSE
-	L.status_flags &= ~IMMOBILE_ACTION
+	busy_user.status_flags &= ~IMMOBILE_ACTION
 
 	if (show_remaining_time)
 		return (. ? 0 : time_remaining/expected_total_time) // If action was not interrupted, return 0 for no time left, otherwise return ratio of time remaining

commit 93698f3886e2d5e2825906ef34f33035e6e6df91

Fix for do_after() and alpha check. (#2541)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

# About the pull request

Followup to #2456. In as much as reverting something and redoing it from
scratch can be called a followup.
I was going to go on entire _rant_ about how when you copypaste
somebody's code to retry making it, you should watch carefully whence
and whither you are copypasting (that `break` belonged in its own `if`
and not in the one inserted into the middle of it), how you should test
that your version actually works, how `TRUE` and `FALSE` are different
things and one thing is not another (this one should be obvious,
but...). Then I switched to going to go on a rant about some extreme
edge cases and how terrible Byond is at handling something about icons
and alphas. Then I went to actually read documentation and realised that
we have been trying to reinvent a bicycle and that it was not even going
to liftoff anyway.
When all you had to do was revert the (relevant part of)
[this](https://github.com/cmss13-devs/cmss13/commit/d523bb75462cc9367cc55ee8ba3f07e16c9d3acf).

# Explain why it's good for the game

Everything still broke from #2456, though. This has made a lot of people
very angry and been widely regarded as a bad move.

# Testing Photographs and Procedure
...and I still reserve the right to say this: _actually test that your
code actually works_.


# Changelog

<!-- If your PR modifies aspects of the game that can be concretely
observed by players or admins you should add a changelog. If your change
does NOT meet this description, remove this section. Be sure to properly
mark your PRs to prevent unnecessary GBP loss. Please note that
maintainers freely reserve the right to remove and add tags should they
deem it appropriate. You can attempt to finagle the system all you want,
but it's best to shoot for clear communication right off the bat. -->

code/__HELPERS/unsorted.dm
@@ -1054,22 +1054,15 @@ var/global/image/action_purple_power_up
 		target_is_mob = TRUE
 
 	var/image/busy_icon
-	var/has_busy_icon = FALSE
 	if(show_busy_icon)
 		busy_icon = get_busy_icon(show_busy_icon)
 		if(busy_icon)
-			busy_icon.appearance_flags = RESET_ALPHA|KEEP_APART
-			busy_icon.alpha = 255
-			if(busy_user.alpha == 255)
-				busy_user.overlays += busy_icon
-				has_busy_icon = TRUE
+			busy_user.overlays += busy_icon
 
 	var/image/target_icon
 	if(show_target_icon) //putting a busy overlay on top of the target
 		target_icon = get_busy_icon(show_target_icon)
 		if(target_icon)
-			target_icon.appearance_flags = RESET_ALPHA|KEEP_APART
-			target_icon.alpha = 255
 			target.overlays += target_icon
 
 	if(user_flags & BEHAVIOR_IMMOBILE)
@@ -1110,14 +1103,6 @@ var/global/image/action_purple_power_up
 		time_remaining -= delayfraction
 		if(!istype(busy_user) || has_target && !istype(target)) // Checks if busy_user exists and is not dead and if the target exists and is not destroyed
 			. = FALSE
-		if(show_busy_icon)
-			if(has_busy_icon && user.alpha < 255)
-				busy_user.overlays += busy_icon
-				busy_icon.alpha = user.alpha
-				has_busy_icon = TRUE
-			else if(!has_busy_icon && user.alpha == 255)
-				busy_user.overlays += busy_icon
-				has_busy_icon = TRUE
 			break
 		if(user_flags & INTERRUPT_DIFF_LOC && busy_user.loc != user_orig_loc || \
 			has_target && (target_flags & INTERRUPT_DIFF_LOC && target.loc != target_orig_loc)
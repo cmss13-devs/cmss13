cmss13-devs/cmss13

commit 1c2fbe4e7ce2f0568e15221e4cd08fdfa13c4ef9

Restores missing icon states to miscellaneous items  (#2880)
# About the pull request
This PR restores EVERY. SINGLE. missing icon state or fixes all missing
icon state errors for miscellaneous paths that weren't in bulk.

# Testing Photographs and Procedure
The following list contains all miscellaneous items with missing icon
states:
```
/obj/item/device/debugger
/obj/item/device/rsp
/obj/item/stack/sheet/xenochitin
/obj/item/stack/sheet/mineral/enruranium
/obj/item/storage/fancy
/obj/item/storage/fancy/cigarettes
/obj/item/reagent_container/food/drinks/cans/ale
/obj/item/reagent_container/food/snacks/grown/peanut
/obj/item/reagent_container/food/snacks/meat/fish/squid
/obj/item/reagent_container/food/snacks/meat/fish/squid/alt
/obj/item/facepaint
/obj/item/shard/phoron
/obj/item/shard/shrapnel
/obj/item/book/manual/hydroponics_beekeeping
/obj/item/tape/random
/obj/item/xenos_claw
/obj/item/weed_extract
/obj/item/tool/barricade_hammer //renamed to carpenters_hammer
/obj/item/tool/pickaxe/hammer
/obj/item/defenses/handheld
/obj/item/organ/brain/xeno
/obj/item/light_bulb/tube/prison
/obj/item/light_bulb/bulb/fire
/obj/item/hardpoint
```

![image](https://user-images.githubusercontent.com/13158711/226214071-4e3b20e6-dff2-41b7-8b21-d7c0473daf56.png)
</details>


# Changelog

:cl: Puckaboo2
code: Renamed some text strings to apply existing icon states.
imageadd: Added dozens of new icon states to missing objects.
fix: A huge list of items can now be used. 
imageadd: Adds a few missing goodies.
/:cl:

<!-- Both :cl:'s are required for the changelog to work! -->

code/game/objects/items/devices/coins.dm
@@ -19,44 +19,69 @@
 
 /obj/item/coin/gold
 	name = "gold coin"
+	desc = "A coin of the purest gold."
 	icon_state = "coin_gold"
 	black_market_value = 30
 
 /obj/item/coin/silver
 	name = "silver coin"
+	desc = "A coin of the purest silver."
 	icon_state = "coin_silver"
 	black_market_value = 25
 
+/obj/item/coin/copper
+	name = "gold coin"
+	desc = "A familiar, but cheap form of currency."
+	icon_state = "coin_copper"
+	black_market_value = 30
+
 /obj/item/coin/diamond
 	name = "diamond coin"
+	desc = "A coin of the most uncorrupt diamond."
 	icon_state = "coin_diamond"
 	black_market_value = 35
 
 /obj/item/coin/iron
 	name = "iron coin"
+	desc = "You fear this might get rusty."
 	icon_state = "coin_iron"
 	black_market_value = 15
 
 /obj/item/coin/phoron
 	name = "solid phoron coin"
+	desc = "There are better uses for phoron."
 	icon_state = "coin_phoron"
 	black_market_value = 35
 
 /obj/item/coin/uranium
 	name = "uranium coin"
+	desc = "Don't touch it!"
 	icon_state = "coin_uranium"
 	black_market_value = 35
 
 /obj/item/coin/platinum
 	name = "platinum coin"
-	icon_state = "coin_adamantine"
+	desc = "This is quite valuable."
+	icon_state = "coin_platinum"
 	black_market_value = 35
 
 /obj/item/coin/marine/synth
 	name = "synthetic experimental tool redemption token"
 	desc = "Insert this into a synthetic experimental tools vendor in order to access a variety of experimental support tools."
-	icon_state = "coin_adamantine"
+	icon_state = "coin_synth"
 	black_market_value = 0
+
+/obj/item/coin/chitin
+	name = "chitin coin"
+	desc = "Durable chitin pressed into a coin. Why would anyone make this?"
+	icon_state = "coin_chitin"
+	black_market_value = 35
+
+/obj/item/coin/clown
+	name = "clown coin"
+	desc = "Smells like bananas. A miniature clown pressed his face lovingly into every one of these. HONK!"
+	icon_state = "coin_clown"
+	black_market_value = 35
 
 /obj/item/coin/attackby(obj/item/W as obj, mob/user as mob)
 	if(istype(W,/obj/item/stack/cable_coil))

code/game/objects/items/stacks/sheets/mineral.dm
@@ -253,7 +253,7 @@ GLOBAL_LIST_INIT(iron_recipes, list ( \
 	icon_state = "sheet-enruranium"
 
 	perunit = 1000
-	stack_id = "uranium"
+	stack_id = "enuranium"
 
 //Valuable resource, cargo can now actually sell it.
 /obj/item/stack/sheet/mineral/platinum

code/game/objects/items/cosmetics.dm
@@ -1,7 +1,7 @@
 /obj/item/facepaint
 	gender = PLURAL
 	name = "facepaint"
-	desc = "Paint, for your face. Who woulda thought?. Wipe it off your face with some paper if you need to."
+	desc = "Paint, for your face. Wipe it off your face with some paper if you need to. This one is a deep, forest green."
 	icon = 'icons/obj/items/items.dmi'
 	icon_state = "camo"
 	var/paint_type = "green"

code/game/objects/items/devices/RSP.dm
@@ -1,8 +1,8 @@
 /obj/item/device/rsp
 	name = "\improper Rapid-Seed-Producer (RSP)"
 	desc = "A device used to rapidly deploy seeds."
-	icon = 'icons/obj/items/items.dmi'
-	icon_state = "rcd"
+	icon = 'icons/obj/items/devices.dmi'
+	icon_state = "rsp"
 	opacity = FALSE
 	density = FALSE
 	anchored = FALSE

commit 09153c20aa75cd1bac367c64056306cccb5e324a

Fixes all missing icon states for toys and props (#2922)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

# About the pull request
Even though mercs don't really play with toys, I added icon states for
the missing ones, anyway. I also added icon states for some missing
props, added, re-shaded, and renamed some coins to better-portray their
use, and deleted an object that had no function for over 6 years.

This PR also fixes stuff that broke in #2880, which will be mentioned in
the following list.

Edit: All traces of adamantium are now re-named to platinum. In
addition, I changed Platinum's metal color to a much brighter shade of
gray. Also added lead sheets.

# Testing Photographs and Procedure

The list of missing toys and props are as follows:
```
/obj/item/paper/flag
/obj/item/toy/therapy_red
/obj/item/toy/therapy_purple
/obj/item/toy/therapy_blue
/obj/item/toy/therapy_yellow
/obj/item/toy/therapy_orange
/obj/item/toy/therapy_green
/obj/item/prop/flower_vase
/obj/item/prop/helmetgarb
/obj/item/prop/torch_frame
/obj/item/prop/almayer/lantern_pod //Deleted.
/obj/item/coin/marine //This broke with #2880 because I re-named the icon state of the coin in the .dmi file. Fixed.
/obj/item/coin/chitin //This also broke because I incorrectly named the icon state in the .dmi file. Woops.
/obj/item/hardpoint //This also, also broke because I forgot to actually name the icon state.

```

![image](https://user-images.githubusercontent.com/13158711/227842346-5830cdb5-aa5e-4fd8-80b2-87b78d84d8b5.png)

# Changelog

:cl: Puckaboo2
imageadd: Added several icon states for miscellaneous items that did not
have any.
imageadd: Added lead sheets to shield from radiation poisoning and to
possibly soundproof rooms.
imageadd: Platinum sheet now have a new sprite that looks like platinum.
fix: The dark indigo blocks that were Platinum have been repurposed as
Chitin blocks. Not to be confused with sheets of chitin.

code/game/objects/items/devices/coins.dm
@@ -37,7 +37,7 @@
 
 /obj/item/coin/diamond
 	name = "diamond coin"
-	desc = "A coin of the most uncorrupt diamond."
+	desc = "A coin of the most flawless diamond."
 	icon_state = "coin_diamond"
 	black_market_value = 35
 

code/game/objects/items/props/helmetgarb.dm
@@ -1,10 +1,8 @@
 // Holds props for helmet garb
 
 /obj/item/prop/helmetgarb
-	name = "Placeholder"
-	desc = "placeholder"
 	icon = 'icons/obj/items/helmet_garb.dmi'
-	icon_state = "placeholder"
+	icon_state = null
 	w_class = SIZE_TINY
 	garbage = TRUE
 

code/game/objects/items/stacks/sheets/mineral.dm
@@ -83,7 +83,7 @@ GLOBAL_LIST_INIT(iron_recipes, list ( \
 
 /obj/item/stack/sheet/mineral/iron
 	name = "iron"
-	desc = "Iron is the most basic building material in space, a metal solid at room temperature, easy to shape and available in immense quantities."
+	desc = "Iron is a transition metal and the most basic building material in space. It is solid at room temperature, easy to shape, and available in immense quantities."
 	singular_name = "iron sheet"
 	icon_state = "sheet-silver"
 
@@ -98,7 +98,7 @@ GLOBAL_LIST_INIT(iron_recipes, list ( \
 
 /obj/item/stack/sheet/mineral/sandstone
 	name = "sandstone brick"
-	desc = "Sandstone is a combination of sand and stone. A common building material for primitive civilisations, can still make a good enough wall."
+	desc = "Sandstone is sand cemented into stone. A common building material for primitive civilizations, but it can still make a good enough wall."
 	singular_name = "sandstone brick"
 	icon_state = "sheet-sandstone"
 	throw_speed = SPEED_VERY_FAST
@@ -116,7 +116,7 @@ GLOBAL_LIST_INIT(iron_recipes, list ( \
 
 /obj/item/stack/sheet/mineral/sandstone/runed
 	name = "runed sandstone brick"
-	desc = "Sandstone is a combination of sand and stone. A common building material for primitive civilisations, can still make a good enough wall."
+	desc = "Sandstone is sand cemented into stone. A common building material for primitive civilizations, but it can still make a good enough wall. This one has strange runes embued into the brick."
 	singular_name = "runed sandstone brick"
 	icon_state = "sheet-runedsandstone"
 	black_market_value = 15
@@ -136,7 +136,7 @@ GLOBAL_LIST_INIT(iron_recipes, list ( \
 
 /obj/item/stack/sheet/mineral/diamond
 	name = "diamond"
-	desc = "Diamond is a specific arrangement of carbon created under extreme pressure and heat. Valued for its look and properties, despite artificial manufacturing possibilities."
+	desc = "Diamond is highly-pressurized and heated carbon in a diamond cubic crystal lattice. It is highly-valued for its look and hardness, despite being artifically-manufactured, these days."
 	singular_name = "diamond gem"
 	icon_state = "sheet-diamond"
 
@@ -170,7 +170,7 @@ GLOBAL_LIST_INIT(iron_recipes, list ( \
 
 /obj/item/stack/sheet/mineral/phoron
 	name = "solid phoron"
-	desc = "Phoron is an extremely rare mineral with exotic properties, often used in cutting-edge research. Just getting it into a stable, solid form is already hard enough."
+	desc = "Phoron is an extremely rare mineral with exotic properties, often used in cutting-edge research. Just getting it into a stable, solid form is already difficult enough, considering it can be quite toxic and flammable."
 	singular_name = "phoron ingot"
 	icon_state = "sheet-phoron"
 
@@ -230,7 +230,7 @@ GLOBAL_LIST_INIT(iron_recipes, list ( \
 
 /obj/item/stack/sheet/mineral/silver
 	name = "silver"
-	desc = "Silver is a transition metal. It is known for its namesake silver, gray color. It is used both for cosmetics as a cheaper alternative to gold, or for engineering."
+	desc = "Silver is a transition metal. It is known for its namesake silver, gray color. It is used both for cosmetics as a cheaper alternative to gold, or for engineering for its unparalleled electrical and thermal conductivity and reflectivity."
 	singular_name = "silver ingot"
 	icon_state = "sheet-silver"
 
@@ -260,11 +260,23 @@ GLOBAL_LIST_INIT(iron_recipes, list ( \
 	name = "platinum"
 	desc = "Platinum is a transition metal. Relatively rare and pretty, it is used for its cosmetic value and chemical properties as a catalytic agent. It is also used in electrodes."
 	singular_name = "platinum ingot"
-	icon_state = "sheet-adamantine"
+	icon_state = "sheet-platinum"
 
 	sheettype = "platinum"
 	perunit = 2000
 	stack_id = "platinum"
+	black_market_value = 35
+
+
+/obj/item/stack/sheet/mineral/lead
+	name = "lead"
+	desc = "Lead is a heavy metal. It is quite dense, yet soft and malleable in its solid state. Though toxic if consumed, lead sheets are used as a shielding material in walls to protect from radiation, and also to absorb sound and vibrations."
+	singular_name = "lead brick"
+	icon_state = "sheet-lead"
+
+	sheettype = "lead"
+	perunit = 2000
+	stack_id = "lead"
 	black_market_value = 35
 
 //Extremely valuable to Research.
@@ -294,11 +306,22 @@ GLOBAL_LIST_INIT(iron_recipes, list ( \
 /obj/item/stack/sheet/mineral/osmium
 	name = "osmium"
 	desc = "Osmium is a transition metal. The densest naturally occurring element known to man, it is obviously known for its extreme hardness and durability and used as such."
-	singular_name = "tritium ingot"
+	singular_name = "osmium ingot"
 	icon_state = "sheet-silver"
 	sheettype = "osmium"
 
 	color = "#9999FF"
 	perunit = 2000
 	stack_id = "osmium"
+	black_market_value = 35
+
+/obj/item/stack/sheet/mineral/chitin
+	name = "chitin"
+	desc = "Chitin is the building block of an arthropod--such as an insect or crustracean's--exoskeleton. This sheet, in particular, came from aliens."
+	singular_name = "chitin brick"
+	icon_state = "sheet-chitin"
+
+	sheettype = "chitin"
+	perunit = 2000
+	stack_id = "chitin"
 	black_market_value = 35

code/game/objects/items/toys/toys.dm
@@ -321,48 +321,48 @@
 
 /obj/item/toy/therapy_red
 	name = "red therapy doll"
-	desc = "A toy for therapeutic and recreational purposes. This one is red."
-	icon = 'icons/obj/items/weapons/weapons.dmi'
+	desc = "A therapeutic toy to assist marines in recovering from mental and behavioral disorders after experiencing the trauma of battles. This one is red."
+	icon = 'icons/obj/items/toy.dmi'
 	icon_state = "therapyred"
 	item_state = "egg4" // It's the red egg in items_left/righthand
 	w_class = SIZE_TINY
 
 /obj/item/toy/therapy_purple
 	name = "purple therapy doll"
-	desc = "A toy for therapeutic and recreational purposes. This one is purple."
-	icon = 'icons/obj/items/weapons/weapons.dmi'
+	desc = "A therapeutic toy to assist marines in recovering from mental and behavioral disorders after experiencing the trauma of battles. This one is purple."
+	icon = 'icons/obj/items/toy.dmi'
 	icon_state = "therapypurple"
 	item_state = "egg1" // It's the magenta egg in items_left/righthand
 	w_class = SIZE_TINY
 
 /obj/item/toy/therapy_blue
 	name = "blue therapy doll"
-	desc = "A toy for therapeutic and recreational purposes. This one is blue."
-	icon = 'icons/obj/items/weapons/weapons.dmi'
+	desc = "A therapeutic toy to assist marines in recovering from mental and behavioral disorders after experiencing the trauma of battles. This one is blue."
+	icon = 'icons/obj/items/toy.dmi'
 	icon_state = "therapyblue"
 	item_state = "egg2" // It's the blue egg in items_left/righthand
 	w_class = SIZE_TINY
 
 /obj/item/toy/therapy_yellow
 	name = "yellow therapy doll"
-	desc = "A toy for therapeutic and recreational purposes. This one is yellow."
-	icon = 'icons/obj/items/weapons/weapons.dmi'
+	desc = "A therapeutic toy to assist marines in recovering from mental and behavioral disorders after experiencing the trauma of battles. This one is yellow."
+	icon = 'icons/obj/items/toy.dmi'
 	icon_state = "therapyyellow"
 	item_state = "egg5" // It's the yellow egg in items_left/righthand
 	w_class = SIZE_TINY
 
 /obj/item/toy/therapy_orange
 	name = "orange therapy doll"
-	desc = "A toy for therapeutic and recreational purposes. This one is orange."
-	icon = 'icons/obj/items/weapons/weapons.dmi'
+	desc = "A therapeutic toy to assist marines in recovering from mental and behavioral disorders after experiencing the trauma of battles. This one is orange."
+	icon = 'icons/obj/items/toy.dmi'
 	icon_state = "therapyorange"
 	item_state = "egg4" // It's the red one again, lacking an orange item_state and making a new one is pointless
 	w_class = SIZE_TINY
 
 /obj/item/toy/therapy_green
 	name = "green therapy doll"
-	desc = "A toy for therapeutic and recreational purposes. This one is green."
-	icon = 'icons/obj/items/weapons/weapons.dmi'
+	desc = "A therapeutic toy to assist marines in recovering from mental and behavioral disorders after experiencing the trauma of battles. This one is green."
+	icon = 'icons/obj/items/toy.dmi'
 	icon_state = "therapygreen"
 	item_state = "egg3" // It's the green egg in items_left/righthand
 	w_class = SIZE_TINY

code/game/objects/prop.dm
@@ -31,7 +31,7 @@
 
 /obj/item/prop/flower_vase
 	name = "flower vase"
-	icon_state = "vase"
+	icon_state = "flowervase"
 	w_class = SIZE_SMALL
 	desc = "An empty glass flower vase."
 

code/game/objects/structures/props.dm
@@ -696,6 +696,7 @@
 
 /obj/item/prop/torch_frame
 	name = "unlit torch"
+	icon = 'icons/obj/structures/structures.dmi'
 	desc = "It's a torch, but it's not lit or placed down. Click on a wall to place it."
 	icon_state = "torch_frame"
 	luminosity = 0

code/modules/almayer/machinery.dm
@@ -30,12 +30,6 @@
 	icon_state = "flight_recorder"
 	w_class = SIZE_LARGE
 
-/obj/item/prop/almayer/lantern_pod
-	name = "\improper LANTERN pod"
-	desc = "A long green box mounted into a dropship to provide various optical support for its ground targeting systems."
-	icon_state = "lantern_pod"
-	w_class = SIZE_LARGE
-
 /obj/item/prop/almayer/flare_launcher
 	name = "\improper MJU-77/C case"
 	desc = "A flare launcher that usually gets mounted onto dropships to help survivability against infrared tracking missiles."

code/modules/cm_marines/equipment/gear.dm
@@ -147,7 +147,7 @@
 /obj/item/coin/marine
 	name = "marine specialist weapon token"
 	desc = "Insert this into a specialist vendor in order to access a single highly dangerous weapon."
-	icon_state = "coin_adamantine"
+	icon_state = "coin_platinum"
 
 /obj/item/coin/marine/attackby(obj/item/W as obj, mob/user as mob) //To remove attaching a string functionality
 	return

code/modules/cm_marines/smartgun_mount.dm
@@ -16,7 +16,7 @@
 /obj/item/coin/marine/engineer
 	name = "marine engineer support token"
 	desc = "Insert this into an engineer vendor in order to access a support weapon."
-	icon_state = "coin_adamantine"
+	icon_state = "coin_platinum"
 
 // First thing we need is the ammo drum for this thing.
 /obj/item/ammo_magazine/m56d

code/modules/paperwork/paper.dm
@@ -52,7 +52,7 @@
 	updateinfolinks()
 
 /obj/item/paper/update_icon()
-	if(icon_state == "paper_talisman" || icon_state == "paper_wy_words" || icon_state == "paper_uscm" || icon_state == "fortune")
+	if(icon_state == "paper_talisman" || icon_state == "paper_wy_words" || icon_state == "paper_uscm" || icon_state == "fortune" || icon_state == "paper_flag")
 		return
 	if(info)
 		if(icon_state == "paper_wy")
@@ -60,6 +60,10 @@
 			return
 		if(icon_state == "paper_uscm")
 			icon_state = "paper_uscm_words"
+			return
+		if(icon_state == "paper_flag")
+			icon_state = "paper_flag_words"
+			item_state = "paper_flag"
 			return
 		icon_state = "paper_words"
 		return
@@ -495,8 +499,11 @@
 	info = "<B>Hello USCM Orbital Cannon System Owner!</B><BR><BR>We regret to inform you that a communications mishap has resulted in your orbital bombardment warheads being recycled for spare metal! Worry not, the metal has been put to good use in High Command's chest freezer."
 
 /obj/item/paper/flag
-	icon_state = "flag_neutral"
-	item_state = "paper"
+	name = "paper flag"
+	desc = "Somebody attached a blank piece of paper to a stick. You feel like waving it around like an idiot."
+	icon_state = "paper_flag"
+	item_state = "paper_flag"
+
 	anchored = TRUE
 
 /obj/item/paper/jobs
d31ccfa01d1f01b80541e4fdaebb693c46740958
Drulikar <drathek@gmail.com>
2023-07-11 17:41:30 -0700

    Colors

--- tools/dmi/test.py
+++ tools/dmi/test.py
@@ -3,6 +3,14 @@ import sys
 from dmi import *
 
 
+def green(text):
+    return "\033[32m" + str(text) + "\033[0m"
+
+
+def red(text):
+    return "\033[31m" + str(text) + "\033[0m"
+
+
 def _self_test():
     # test: can we load every DMI in the tree
     count = 0
@@ -18,24 +26,24 @@ def _self_test():
                     dmi_states = dmi.states
                     number_of_icon_states = len(dmi.states)
                     if number_of_icon_states > 512:
-                        print("{0} has too many icon states: {1}/512.".format(fullpath, number_of_icon_states))
+                        print("{0} {1} has too many icon states: {2}/512.".format(red("FAIL"), fullpath, number_of_icon_states))
                         failed += 1
                     existing_states = []
                     for state in dmi_states:
                         if state.name in existing_states:
-                            print("{0} has a duplicate state '{1}'.".format(fullpath, state.name))
+                            print("{0} {1} has a duplicate state '{2}'.".format(red("FAIL"), fullpath, state.name))
                             failed += 1
                             continue
                         existing_states.append(state.name)
                 except Exception:
-                    print('Failed on:', fullpath)
+                    print("{0} {1} threw an exception.".format(red("FAIL"), fullpath))
                     failed += 1
                     raise
                 count += 1
 
-    print(f"{os.path.relpath(__file__)}: successfully parsed {count-failed} .dmi files")
+    print(f"{os.path.relpath(__file__)}: {green(f'successfully parsed {count-failed} .dmi files')}")
     if failed > 0:
-        print(f"{os.path.relpath(__file__)}: failed to parse {failed} .dmi files")
+        print(f"{os.path.relpath(__file__)}: {red(f'failed to parse {failed} .dmi files')}")
         exit(1)
 
 

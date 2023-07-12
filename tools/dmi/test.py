980611dba3abb9e4076151b60c36e9b19b93f091
Drulikar <drathek@gmail.com>
2023-07-11 17:27:20 -0700

    Use dmi.test.py instead

--- tools/dmi/test.py
+++ tools/dmi/test.py
@@ -6,6 +6,7 @@ from dmi import *
 def _self_test():
     # test: can we load every DMI in the tree
     count = 0
+    failed = 0
     for dirpath, dirnames, filenames in os.walk('.'):
         if '.git' in dirnames:
             dirnames.remove('.git')
@@ -13,13 +14,29 @@ def _self_test():
             if filename.endswith('.dmi'):
                 fullpath = os.path.join(dirpath, filename)
                 try:
-                    Dmi.from_file(fullpath)
+                    dmi = Dmi.from_file(fullpath)
+                    dmi_states = dmi.states
+                    number_of_icon_states = len(dmi.states)
+                    if number_of_icon_states > 512:
+                        print("{0} has too many icon states: {1}/512.".format(fullpath, number_of_icon_states))
+                        failed += 1
+                    existing_states = []
+                    for state in dmi_states:
+                        if state.name in existing_states:
+                            print("{0} has a duplicate state '{1}'.".format(fullpath, state.name))
+                            failed += 1
+                            continue
+                        existing_states.append(state.name)
                 except Exception:
                     print('Failed on:', fullpath)
+                    failed += 1
                     raise
                 count += 1
 
-    print(f"{os.path.relpath(__file__)}: successfully parsed {count} .dmi files")
+    print(f"{os.path.relpath(__file__)}: successfully parsed {count-failed} .dmi files")
+    if failed > 0:
+        print(f"{os.path.relpath(__file__)}: failed to parse {failed} .dmi files")
+        exit(1)
 
 
 def _usage():

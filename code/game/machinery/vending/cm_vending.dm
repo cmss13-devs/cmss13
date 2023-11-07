cmss13-devs/cmss13

commit 4b794ded6c26077c32f15051291e6ecf6d98d353

Whiskey Outpost Reqs Tweaks (#4803)
# About the pull request

<!-- Remove this text and explain what the purpose of your PR is.

Mention if you have tested your changes. If you changed a map, make sure
you used the mapmerge tool.
If this is an Issue Correction, you can type "Fixes Issue #169420" to
link the PR to the corresponding Issue number #169420.

Remember: something that is self-evident to you might not be to others.
Explain your rationale fully, even if you feel it goes without saying.
-->

Right now reqs on WO is a pain for three major reasons:
 * Marines have less gearing option making Req Line an huge burden
* A lot of stuff is defunct or removed such as the recycler, parcel
network, and vendors not containing now basic gear nowadays
* Because of all that there is little incentive to run Reqs beyond the
attachies line, meaning it becomes a pure roaming game as assistant
engineers

This attempts to address it in the following ways:
* Fixing the recycler. It now outputs materials, and can now recycle all
the crates that previously littered Reqs for metal
* Fixing req vendors. They did not benefit from eg. attachies vendor
dispensing to table like Almayers'
* Standardize the vendors contents with Almayers' to bring useful things
like binos or grenades that were missing. The ammo vendor retains the
spec ammo in addition to this.
* Readds a delivery chute, related piping, paper wrap, and destination
taggers, allowing Reqs to once again fax whole crates to pillboxes
* Tweak supply pad deliveries weights and contents to make it more
interesting and hopefully not ignored. This means higher chances to get
materials, and new packs such as mortar shells, while reducing highly
redundant packs such as floodlights.
 * Minor Req mapping adjsutments 

Additionally it adds the two missing synth vendors from the Almayer to
the map.

**!!! Note that this enables recycler output also for other instances of

code/game/machinery/vending/cm_vending.dm
@@ -37,8 +37,14 @@
 	var/vend_delay = 0 //delaying vending of an item (for drinks machines animation, for example). Make sure to synchronize this with animation duration
 	var/vend_sound //use with caution. Potential spam
 
+	/// X Offset to vend to
 	var/vend_x_offset = 0
+	/// Y Offset to vend to
 	var/vend_y_offset = 0
+	/// Vending direction from adjacent users, if not using vend_x_offset or vend_y_offset
+	var/vend_dir
+	/// Direction to adjacent user from which we're allowed to do offset vending
+	var/list/vend_dir_whitelist
 
 	var/list/listed_products = list()
 
@@ -125,11 +131,20 @@ GLOBAL_LIST_EMPTY(vending_products)
 			GLOB.vending_products[typepath] = 1
 
 //get which turf the vendor will dispense its products on.
-/obj/structure/machinery/cm_vending/proc/get_appropriate_vend_turf()
-	var/turf/T = loc
+/obj/structure/machinery/cm_vending/proc/get_appropriate_vend_turf(mob/living/carbon/human/user)
+	var/turf/turf = loc
 	if(vend_x_offset != 0 || vend_y_offset != 0) //this check should be more less expensive than using locate to locate your own tile every vending.
-		T = locate(x + vend_x_offset, y + vend_y_offset, z)
-	return T
+		turf = locate(x + vend_x_offset, y + vend_y_offset, z)
+		return turf
+	if(vend_dir)
+		if(vend_dir_whitelist)
+			var/user_dir = get_dir(loc, user)
+			if(!(user_dir in vend_dir_whitelist))
+				return turf
+		var/turf/relative_turf = get_step(user, vend_dir)
+		if(relative_turf.Adjacent(src))
+			return relative_turf
+	return turf
 
 /obj/structure/machinery/cm_vending/get_examine_text(mob/living/carbon/human/user)
 	. = ..()

code/game/area/WhiskeyOutpost.dm
@@ -65,7 +65,7 @@
 	icon_state = "livingspace"
 
 /area/whiskey_outpost/inside/supply
-	name = "\improper Supply Depo"
+	name = "\improper Supply Depot"
 	icon_state = "req"
 
 /*

code/game/gamemodes/colonialmarines/whiskey_outpost.dm
@@ -322,9 +322,9 @@
 		OT = "sup" //no breaking anything.
 
 	else if (OT == "sup")
-		randpick = rand(0,50)
+		randpick = rand(0,90)
 		switch(randpick)
-			if(0 to 5)//Marine Gear 10% Chance.
+			if(0 to 3)//Marine Gear 3% Chance.
 				crate = new /obj/structure/closet/crate/secure/gear(T)
 				choosemax = rand(5,10)
 				randomitems = list(/obj/item/clothing/head/helmet/marine,
@@ -340,19 +340,19 @@
 								/obj/effect/landmark/wo_supplies/storage/webbing,
 								/obj/item/device/binoculars)
 
-			if(6 to 10)//Lights and shiet 10%
+			if(4 to 6)//Lights and shiet 2%
 				new /obj/structure/largecrate/supply/floodlights(T)
 				new /obj/structure/largecrate/supply/supplies/flares(T)
 
 
-			if(11 to 13) //6% Chance to drop this !FUN! junk.
+			if(7 to 10) //3% Chance to drop this !FUN! junk.
 				crate = new /obj/structure/closet/crate/secure/gear(T)
 				spawnitems = list(/obj/item/storage/belt/utility/full,
 									/obj/item/storage/belt/utility/full,
 									/obj/item/storage/belt/utility/full,
 									/obj/item/storage/belt/utility/full)
 
-			if(14 to 18)//Materials 10% Chance.
+			if(11 to 22)//Materials 12% Chance.
 				crate = new /obj/structure/closet/crate/secure/gear(T)
 				choosemax = rand(3,8)
 				randomitems = list(/obj/item/stack/sheet/metal,
@@ -363,7 +363,7 @@
 								/obj/item/stack/sandbags_empty/half,
 								/obj/item/stack/sandbags_empty/half)
 
-			if(19 to 20)//Blood Crate 4% chance
+			if(23 to 25)//Blood Crate 2% chance
 				crate = new /obj/structure/closet/crate/medical(T)
 				spawnitems = list(/obj/item/reagent_container/blood/OMinus,
 								/obj/item/reagent_container/blood/OMinus,
@@ -371,7 +371,7 @@
 								/obj/item/reagent_container/blood/OMinus,
 								/obj/item/reagent_container/blood/OMinus)
 
-			if(21 to 25)//Advanced meds Crate 10%
+			if(26 to 30)//Advanced meds Crate 5%
 				crate = new /obj/structure/closet/crate/medical(T)
 				spawnitems = list(/obj/item/storage/firstaid/fire,
 								/obj/item/storage/firstaid/regular,
@@ -386,7 +386,7 @@
 								/obj/item/clothing/glasses/hud/health,
 								/obj/item/device/defibrillator)
 
-			if(26 to 30)//Random Medical Items 10% as well. Made the list have less small junk
+			if(31 to 34)//Random Medical Items 4%. Made the list have less small junk
 				crate = new /obj/structure/closet/crate/medical(T)
 				spawnitems = list(/obj/item/storage/belt/medical/lifesaver/full,
 								/obj/item/storage/belt/medical/lifesaver/full,
@@ -394,7 +394,7 @@
 								/obj/item/storage/belt/medical/lifesaver/full,
 								/obj/item/storage/belt/medical/lifesaver/full)
 
-			if(31 to 35)//Random explosives Crate 10% because the lord commeth and said let there be explosives.
+			if(35 to 40)//Random explosives Crate 5% because the lord commeth and said let there be explosives.
 				crate = new /obj/structure/closet/crate/ammo(T)
 				choosemax = rand(1,5)
 				randomitems = list(/obj/item/storage/box/explosive_mines,
@@ -404,7 +404,7 @@
 								/obj/item/explosive/grenade/high_explosive,
 								/obj/item/storage/box/nade_box
 								)
-			if(36 to 40) // Junk
+			if(41 to 44)
 				crate = new /obj/structure/closet/crate/ammo(T)
 				spawnitems = list(
 									/obj/item/attachable/heavy_barrel,
@@ -412,20 +412,75 @@
 									/obj/item/attachable/heavy_barrel,
 									/obj/item/attachable/heavy_barrel)
 
-			if(40 to 48)//Weapon + supply beacon drop. 6%
+			if(45 to 50)//Weapon + supply beacon drop. 5%
 				crate = new /obj/structure/closet/crate/ammo(T)
 				spawnitems = list(/obj/item/device/whiskey_supply_beacon,
 								/obj/item/device/whiskey_supply_beacon,
 								/obj/item/device/whiskey_supply_beacon,
 								/obj/item/device/whiskey_supply_beacon)
 
-			if(49 to 50)//Rare weapons. Around 4%
+			if(51 to 57)//Rare weapons. Around 6%
 				crate = new /obj/structure/closet/crate/ammo(T)
 				spawnitems = list(/obj/effect/landmark/wo_supplies/ammo/box/rare/m41aap,
 								/obj/effect/landmark/wo_supplies/ammo/box/rare/m41aapmag,
 								/obj/effect/landmark/wo_supplies/ammo/box/rare/m41aextend,
 								/obj/effect/landmark/wo_supplies/ammo/box/rare/smgap,
 								/obj/effect/landmark/wo_supplies/ammo/box/rare/smgextend)
+
+			if(58 to 65) // Sandbags kit
+				crate = new /obj/structure/closet/crate(T)
+				spawnitems = list(/obj/item/tool/shovel/etool,
+								/obj/item/stack/sandbags_empty/half,
+								/obj/item/stack/sandbags_empty/half,
+								/obj/item/stack/sandbags_empty/half)
+
+			if(66 to 70) // Mortar shells. Pew Pew!
+				crate = new /obj/structure/closet/crate/secure/mortar_ammo(T)
+				choosemax = rand(6,10)
+				randomitems = list(/obj/item/mortar_shell/he,
+								/obj/item/mortar_shell/incendiary,
+								/obj/item/mortar_shell/flare,
+								/obj/item/mortar_shell/frag)
+
+			if(71 to 79)
+				crate = new /obj/structure/closet/crate/ammo(T)
+				choosemax = rand(2, 3)
+				randomitems = list(/obj/item/ammo_box/rounds,
+								/obj/item/ammo_box/rounds/ap,
+								/obj/item/ammo_box/rounds/smg,
+								/obj/item/ammo_box/rounds/smg/ap,
+								/obj/item/ammo_box/magazine/ap,
+								/obj/item/ammo_box/magazine/ext,
+								/obj/item/ammo_box/magazine/m4ra/ap,
+								/obj/item/ammo_box/magazine/m4ra/ap,
+								/obj/item/ammo_box/magazine/m39/ap,
+								/obj/item/ammo_box/magazine/m39/ext,
+				)
+
+			if(80 to 82)
+				crate = new /obj/structure/closet/crate/ammo(T)
+				choosemax = rand(2, 3)
+				randomitems = list(/obj/item/ammo_magazine/rifle/lmg/holo_target,
+								/obj/item/ammo_magazine/rifle/lmg/holo_target,
+								/obj/item/ammo_magazine/rifle/lmg,
+								/obj/item/ammo_magazine/rifle/lmg,
+				)
+
+			if(83 to 86)
+				crate = new /obj/structure/closet/crate/ammo(T)
+				spawnitems = list(
+									/obj/item/attachable/magnetic_harness,
+									/obj/item/attachable/magnetic_harness,
+									/obj/item/attachable/magnetic_harness,
+									/obj/item/attachable/magnetic_harness)
+
+			if(86 to 90)
+				crate = new /obj/structure/closet/crate/secure/gear(T)
+				spawnitems = list(
+									/obj/item/device/binoculars/range,
+									/obj/item/device/binoculars/range,
+				)
+
 	if(crate)
 		crate.storage_capacity = 60
 

code/game/machinery/vending/vendor_types/requisitions.dm
@@ -175,22 +175,8 @@
 //Special cargo-specific vendor with vending offsets
 /obj/structure/machinery/cm_vending/sorted/cargo_guns/cargo
 	vend_flags = VEND_CLUTTER_PROTECTION | VEND_LIMITED_INVENTORY | VEND_LOAD_AMMO_BOXES //We want to vend to turf not hand, since we are in requisitions
-
-/obj/structure/machinery/cm_vending/sorted/cargo_guns/cargo/get_appropriate_vend_turf(mob/living/carbon/human/H)
-	var/turf/turf_to_vent_to
-	if(vend_x_offset != 0 || vend_y_offset != 0) //this will allow to avoid code below that suits only Almayer.
-		turf_to_vent_to = locate(x + vend_x_offset, y + vend_y_offset, z)
-	else
-		turf_to_vent_to = get_turf(get_step(src, NORTH))
-		if(H.loc == turf_to_vent_to)
-			turf_to_vent_to = get_turf(get_step(H.loc, WEST))
-		else
-			turf_to_vent_to = get_turf(get_step(src, SOUTH))
-			if(H.loc == turf_to_vent_to)
-				turf_to_vent_to = get_turf(get_step(H.loc, WEST))
-			else
-				turf_to_vent_to = H.loc
-	return turf_to_vent_to
+	vend_dir = WEST
+	vend_dir_whitelist = list(NORTH, SOUTH)
 
 /obj/structure/machinery/cm_vending/sorted/cargo_guns/cargo/blend
 	icon_state = "req_guns_wall"
@@ -209,6 +195,8 @@
 	req_access = list(ACCESS_MARINE_CARGO)
 	vendor_theme = VENDOR_THEME_USCM
 	vend_flags = VEND_CLUTTER_PROTECTION | VEND_LIMITED_INVENTORY | VEND_TO_HAND | VEND_LOAD_AMMO_BOXES
+	vend_dir = WEST
+	vend_dir_whitelist = list(SOUTHWEST, NORTHWEST)
 
 /obj/structure/machinery/cm_vending/sorted/cargo_ammo/vend_fail()
 	return
@@ -307,22 +295,6 @@
 /obj/structure/machinery/cm_vending/sorted/cargo_ammo/cargo
 	vend_flags = VEND_CLUTTER_PROTECTION | VEND_LIMITED_INVENTORY | VEND_LOAD_AMMO_BOXES //We want to vend to turf not hand, since we are in requisitions
 
-/obj/structure/machinery/cm_vending/sorted/cargo_ammo/cargo/get_appropriate_vend_turf(mob/living/carbon/human/H)
-	var/turf/turf_to_vent_to
-	if(vend_x_offset != 0 || vend_y_offset != 0) //this will allow to avoid code below that suits only Almayer.
-		turf_to_vent_to = locate(x + vend_x_offset, y + vend_y_offset, z)
-	else
-		turf_to_vent_to = get_turf(get_step(src, NORTHWEST))
-		if(H.loc == turf_to_vent_to)
-			turf_to_vent_to = get_turf(get_step(H.loc, WEST))
-		else
-			turf_to_vent_to = get_turf(get_step(src, SOUTHWEST))
-			if(H.loc == turf_to_vent_to)
-				turf_to_vent_to = get_turf(get_step(H.loc, WEST))
-			else
-				turf_to_vent_to = H.loc
-	return turf_to_vent_to
-
 //------------ATTACHMENTS VENDOR---------------
 
 /obj/structure/machinery/cm_vending/sorted/attachments
@@ -331,6 +303,9 @@
 	req_access = list(ACCESS_MARINE_CARGO)
 	vendor_theme = VENDOR_THEME_USCM
 	icon_state = "req_attach"
+	vend_dir = WEST
+	vend_dir_whitelist = list(SOUTHEAST, NORTHEAST)
+	vend_flags = VEND_CLUTTER_PROTECTION | VEND_LIMITED_INVENTORY //We want to vend to turf not hand, since we are in requisitions
 
 /obj/structure/machinery/cm_vending/sorted/attachments/vend_fail()
 	return
@@ -379,22 +354,6 @@
 		list("M44 Magnum Sharpshooter Stock", round(scale * 4.5), /obj/item/attachable/stock/revolver, VENDOR_ITEM_REGULAR)
 		)
 
-/obj/structure/machinery/cm_vending/sorted/attachments/get_appropriate_vend_turf(mob/living/carbon/human/H)
-	var/turf/turf_to_vent_to
-	if(vend_x_offset != 0 || vend_y_offset != 0) //this will allow to avoid code below that suits only Almayer.
-		turf_to_vent_to = locate(x + vend_x_offset, y + vend_y_offset, z)
-	else
-		turf_to_vent_to = get_turf(get_step(src, NORTHEAST))
-		if(H.loc == turf_to_vent_to)
-			turf_to_vent_to = get_turf(get_step(H.loc, WEST))
-		else
-			turf_to_vent_to = get_turf(get_step(src, SOUTHEAST))
-			if(H.loc == turf_to_vent_to)
-				turf_to_vent_to = get_turf(get_step(H.loc, WEST))
-			else
-				turf_to_vent_to = loc
-	return turf_to_vent_to
-
 /obj/structure/machinery/cm_vending/sorted/attachments/blend
 	icon_state = "req_attach_wall"
 	tiles_with = list(
@@ -402,7 +361,6 @@
 		/obj/structure/machinery/door/airlock,
 		/turf/closed/wall/almayer,
 	)
-	vend_flags = VEND_CLUTTER_PROTECTION | VEND_LIMITED_INVENTORY //We want to vend to turf not hand, since we are in requisitions
 
 //------------UNIFORM VENDOR---------------
 

code/game/machinery/vending/vendor_types/wo_vendors.dm
@@ -110,52 +110,13 @@
 	)
 
 //------------REQ AMMUNITION VENDOR---------------
-/obj/structure/machinery/cm_vending/sorted/cargo_ammo/wo
+/obj/structure/machinery/cm_vending/sorted/cargo_ammo/cargo/wo
 	req_access = list(ACCESS_MARINE_CARGO)
 	req_one_access = list()
 
-/obj/structure/machinery/cm_vending/sorted/cargo_ammo/wo/populate_product_list(scale)
-	listed_products = list(
-		list("REGULAR AMMUNITION", -1, null, null),
-		list("Box Of Buckshot Shells", round(scale * 5), /obj/item/ammo_magazine/shotgun/buckshot, VENDOR_ITEM_REGULAR),
-		list("Box Of Flechette Shells", round(scale * 5), /obj/item/ammo_magazine/shotgun/flechette, VENDOR_ITEM_REGULAR),
-		list("Box Of Shotgun Slugs", round(scale * 5), /obj/item/ammo_magazine/shotgun/slugs, VENDOR_ITEM_REGULAR),
-		list("M4RA magazine (10x24mm)", round(scale * 10), /obj/item/ammo_magazine/rifle/m4ra, VENDOR_ITEM_REGULAR),
-		list("M39 HV Magazine (10x20mm)", round(scale * 10), /obj/item/ammo_magazine/smg/m39, VENDOR_ITEM_REGULAR),
-		list("M41A MK1 Magazine (10x24mm)", round(scale * 10), /obj/item/ammo_magazine/rifle/m41aMK1, VENDOR_ITEM_REGULAR),
-		list("M41A MK2 Magazine (10x24mm)", round(scale * 10), /obj/item/ammo_magazine/rifle, VENDOR_ITEM_REGULAR),
-		list("M44 Speed Loader (.44)", round(scale * 10), /obj/item/ammo_magazine/revolver, VENDOR_ITEM_REGULAR),
-		list("M4A3 Magazine (9mm)", round(scale * 10), /obj/item/ammo_magazine/pistol, VENDOR_ITEM_REGULAR),
-
-		list("ARMOR-PIERCING AMMUNITION", -1, null, null),
-		list("88 Mod 4 AP Magazine (9mm)", round(scale * 5), /obj/item/ammo_magazine/pistol/mod88, VENDOR_ITEM_REGULAR),
-		list("M4RA AP Magazine (10x24mm)", round(scale * 10), /obj/item/ammo_magazine/rifle/m4ra/ap, VENDOR_ITEM_REGULAR),
-		list("M39 AP Magazine (10x20mm)", round(scale * 5), /obj/item/ammo_magazine/smg/m39/ap, VENDOR_ITEM_REGULAR),
-		list("M41A MK1 AP Magazine (10x24mm)", round(scale * 10), /obj/item/ammo_magazine/rifle/m41aMK1/ap, VENDOR_ITEM_REGULAR),
-		list("M41A MK2 AP Magazine (10x24mm)", round(scale * 10), /obj/item/ammo_magazine/rifle/ap, VENDOR_ITEM_REGULAR),
-		list("M4A3 AP Magazine (9mm)", round(scale * 5), /obj/item/ammo_magazine/pistol/ap, VENDOR_ITEM_REGULAR),
-
-		list("EXTENDED AMMUNITION", -1, null, null),
-		list("M39 Extended Magazine (10x20mm)", round(scale * 1), /obj/item/ammo_magazine/smg/m39/extended, VENDOR_ITEM_REGULAR),
-		list("M41A MK2 Extended Magazine (10x24mm)", round(scale * 3), /obj/item/ammo_magazine/rifle/extended, VENDOR_ITEM_REGULAR),
-
-		list("INCENDIARY AMMUNITION", -1, null, null),
-		list("M4RA Incendiary Magazine (10x24mm)", round(scale * 3), /obj/item/ammo_magazine/rifle/m4ra/incendiary, VENDOR_ITEM_REGULAR),
-		list("M39 Incendiary Magazine (10x20mm)", round(scale * 2), /obj/item/ammo_magazine/smg/m39/incendiary, VENDOR_ITEM_REGULAR),
-		list("M41A MK2 Incendiary Magazine (10x24mm)", round(scale * 3), /obj/item/ammo_magazine/rifle/incendiary, VENDOR_ITEM_REGULAR),
-		list("M4A3 Incendiary Magazine (9mm)", round(scale * 1), /obj/item/ammo_magazine/pistol/incendiary, VENDOR_ITEM_REGULAR),
-
-		list("SPECIAL AMMUNITION", -1, null, null),
-		list("Incinerator Tank", round(scale * 2.5), /obj/item/ammo_magazine/flamer_tank, VENDOR_ITEM_REGULAR),
-		list("M41AE2 Ammo Box (10x24mm)", round(scale * 5), /obj/item/ammo_magazine/rifle/lmg, VENDOR_ITEM_REGULAR),
-		list("M44 Heavy Speed Loader (.44)", round(scale * 2), /obj/item/ammo_magazine/revolver/heavy, VENDOR_ITEM_REGULAR),
-		list("M44 Marksman Speed Loader (.44)", round(scale * 2), /obj/item/ammo_magazine/revolver/marksman, VENDOR_ITEM_REGULAR),
-		list("M4A3 HP Magazine (9mm)", round(scale * 5), /obj/item/ammo_magazine/pistol/hp, VENDOR_ITEM_REGULAR),
-		list("M56 Battery", round(scale * 5), /obj/item/smartgun_battery, VENDOR_ITEM_REGULAR),
-		list("M56 Smartgun Drum", round(scale * 2), /obj/item/ammo_magazine/smartgun, VENDOR_ITEM_REGULAR),
-		list("SU-6 Smartpistol Magazine (.45)", round(scale * 6), /obj/item/ammo_magazine/pistol/smart, VENDOR_ITEM_REGULAR),
-		list("VP78 Magazine", round(scale * 6), /obj/item/ammo_magazine/pistol/vp78, VENDOR_ITEM_REGULAR),
-
+/obj/structure/machinery/cm_vending/sorted/cargo_ammo/cargo/wo/populate_product_list(scale)
+	..()
+	listed_products += list(
 		list("EXTRA SCOUT AMMUNITION", -1, null, null, null),
 		list("A19 High Velocity Impact Magazine (10x24mm)", round(scale * 1), /obj/item/ammo_magazine/rifle/m4ra/custom/impact, VENDOR_ITEM_REGULAR),
 		list("A19 High Velocity Incendiary Magazine (10x24mm)", round(scale * 1), /obj/item/ammo_magazine/rifle/m4ra/custom/incendiary, VENDOR_ITEM_REGULAR),
@@ -181,96 +142,17 @@
 		list("Large Incinerator Tank", round(scale * 1), /obj/item/ammo_magazine/flamer_tank/large, VENDOR_ITEM_REGULAR),
 		list("Large Incinerator Tank (B) (Green Flame)", round(scale * 1), /obj/item/ammo_magazine/flamer_tank/large/B, VENDOR_ITEM_REGULAR),
 		list("Large Incinerator Tank (X) (Blue Flame)", round(scale * 1), /obj/item/ammo_magazine/flamer_tank/large/X, VENDOR_ITEM_REGULAR),
-
-		list("AMMUNITION BOXES", -1, null, null),
-		list("Rifle Ammunition Box (10x24mm)", round(scale * 0.9), /obj/item/ammo_box/rounds, VENDOR_ITEM_REGULAR),
-		list("Rifle Ammunition Box (10x24mm AP)", round(scale * 0.75), /obj/item/ammo_box/rounds/ap, VENDOR_ITEM_REGULAR),
-		list("SMG Ammunition Box (10x20mm HV)", round(scale * 0.9), /obj/item/ammo_box/rounds/smg, VENDOR_ITEM_REGULAR),
-		list("SMG Ammunition Box (10x20mm AP)", round(scale * 0.75), /obj/item/ammo_box/rounds/smg/ap, VENDOR_ITEM_REGULAR),
 		)
 
 //------------ARMAMENTS VENDOR---------------
 
-/obj/structure/machinery/cm_vending/sorted/cargo_guns/wo
+/obj/structure/machinery/cm_vending/sorted/cargo_guns/cargo/wo
 	req_access = list(ACCESS_MARINE_CARGO)
+	vend_dir = NORTH
+	vend_dir_whitelist = list(EAST, WEST)
 
-/obj/structure/machinery/cm_vending/sorted/cargo_guns/wo/populate_product_list(scale)
-	listed_products = list(
-		list("PRIMARY FIREARMS", -1, null, null),
-		list("M4RA Battle Rifle", round(scale * 20), /obj/item/weapon/gun/rifle/m4ra, VENDOR_ITEM_REGULAR),
-		list("M37A2 Pump Shotgun", round(scale * 10), /obj/item/weapon/gun/shotgun/pump, VENDOR_ITEM_REGULAR),
-		list("M39 Submachinegun", round(scale * 15), /obj/item/weapon/gun/smg/m39, VENDOR_ITEM_REGULAR),
-		list("M41A Pulse Rifle MK1", round(scale * 20), /obj/item/weapon/gun/rifle/m41aMK1, VENDOR_ITEM_REGULAR),
-		list("M41A Pulse Rifle MK2", round(scale * 20), /obj/item/weapon/gun/rifle/m41a, VENDOR_ITEM_REGULAR),
-		list("MK221 Tactical Shotgun", round(scale * 3), /obj/item/weapon/gun/shotgun/combat, VENDOR_ITEM_REGULAR),
-
-		list("SIDEARMS", -1, null, null),
-		list("88 Mod 4 Combat Pistol", round(scale * 15), /obj/item/weapon/gun/pistol/mod88, VENDOR_ITEM_REGULAR),
-		list("M44 Combat Revolver", round(scale * 10), /obj/item/weapon/gun/revolver/m44, VENDOR_ITEM_REGULAR),
-		list("M4A3 Service Pistol", round(scale * 20), /obj/item/weapon/gun/pistol/m4a3, VENDOR_ITEM_REGULAR),
-		list("SU-6 Smartpistol", round(scale * 2), /obj/item/weapon/gun/pistol/smart, VENDOR_ITEM_REGULAR),
-		list("M82F Flare Gun", round(scale * 5), /obj/item/weapon/gun/flare, VENDOR_ITEM_REGULAR),
-
-		list("RESTRICTED FIREARMS", -1, null, null),
-		list("VP78 Pistol", round(scale * 4), /obj/item/storage/box/guncase/vp78, VENDOR_ITEM_REGULAR),
-		list("SU-6 Smart Pistol", round(scale * 3), /obj/item/storage/box/guncase/smartpistol, VENDOR_ITEM_REGULAR),
-		list("M41AE2 Heavy Pulse Rifle", round(scale * 2), /obj/item/storage/box/guncase/lmg, VENDOR_ITEM_REGULAR),
-		list("M56D Heavy Machine Gun", round(scale * 2), /obj/item/storage/box/guncase/m56d, VENDOR_ITEM_REGULAR),
-		list("M2C Heavy Machine Gun", round(scale * 2), /obj/item/storage/box/guncase/m2c, VENDOR_ITEM_REGULAR),
-		list("M240 Incinerator Unit", round(scale * 2), /obj/item/storage/box/guncase/flamer, VENDOR_ITEM_REGULAR),
-
-		list("EXPLOSIVES", -1, null, null),
-		list("M15 Fragmentation Grenade", round(scale * 2), /obj/item/explosive/grenade/high_explosive/m15, VENDOR_ITEM_REGULAR),
-		list("M20 Claymore Anti-Personnel Mine", round(scale * 5), /obj/item/explosive/mine, VENDOR_ITEM_REGULAR),
-		list("M40 HEDP Grenade Box", round(scale * 1), /obj/item/storage/box/nade_box, VENDOR_ITEM_REGULAR),
-		list("M40 HIDP Incendiary Grenade", round(scale * 3), /obj/item/explosive/grenade/incendiary, VENDOR_ITEM_REGULAR),
-		list("M40 HSDP Smoke Grenade", round(scale * 5), /obj/item/explosive/grenade/smokebomb, VENDOR_ITEM_REGULAR),
-
-		list("BACKPACKS", -1, null, null),
-		list("Lightweight IMP Backpack", round(scale * 15), /obj/item/storage/backpack/marine, VENDOR_ITEM_REGULAR),
-		list("Shotgun Scabbard", round(scale * 10), /obj/item/storage/large_holster/m37, VENDOR_ITEM_REGULAR),
-		list("USCM Pyrotechnician G4-1 Fueltank", round(scale * 2), /obj/item/storage/backpack/marine/engineerpack/flamethrower/kit, VENDOR_ITEM_REGULAR),
-		list("USCM Technician Welderpack", round(scale * 2), /obj/item/storage/backpack/marine/engineerpack, VENDOR_ITEM_REGULAR),
-
-		list("BELTS", -1, null, null),
-		list("G8-A General Utility Pouch", round(scale * 3), /obj/item/storage/backpack/general_belt, VENDOR_ITEM_REGULAR),
-		list("M276 Pattern Ammo Load Rig", round(scale * 15), /obj/item/storage/belt/marine, VENDOR_ITEM_REGULAR),
-		list("M276 Pattern General Pistol Holster Rig", round(scale * 10), /obj/item/storage/belt/gun/m4a3, VENDOR_ITEM_REGULAR),
-		list("M276 Pattern M39 Holster Rig", round(scale * 5), /obj/item/storage/belt/gun/m39, VENDOR_ITEM_REGULAR),
-		list("M276 Pattern M44 Holster Rig", round(scale * 5), /obj/item/storage/belt/gun/m44, VENDOR_ITEM_REGULAR),
-		list("M276 M82F Holster Rig", round(scale * 2), /obj/item/storage/belt/gun/flaregun, VENDOR_ITEM_REGULAR),
-		list("M276 Pattern Shotgun Shell Loading Rig", round(scale * 10), /obj/item/storage/belt/shotgun, VENDOR_ITEM_REGULAR),
-
-		list("WEBBINGS", -1, null, null),
-		list("Black Webbing Vest", round(scale * 5), /obj/item/clothing/accessory/storage/black_vest, VENDOR_ITEM_REGULAR),
-		list("Brown Webbing Vest", round(scale * 5), /obj/item/clothing/accessory/storage/black_vest/brown_vest, VENDOR_ITEM_REGULAR),
-		list("Shoulder Holster", round(scale * 5), /obj/item/clothing/accessory/storage/holster, VENDOR_ITEM_REGULAR),
-		list("Webbing", round(scale * 5), /obj/item/clothing/accessory/storage/webbing, VENDOR_ITEM_REGULAR),
-		list("Knife Webbing", round(scale * 3), /obj/item/clothing/accessory/storage/knifeharness, VENDOR_ITEM_REGULAR),
-		list("Drop Pouch", round(scale * 5), /obj/item/clothing/accessory/storage/droppouch, VENDOR_ITEM_REGULAR),
-
-		list("POUCHES", -1, null, null),
-		list("Construction Pouch", round(scale * 2), /obj/item/storage/pouch/construction, VENDOR_ITEM_REGULAR),
-		list("Explosive Pouch", round(scale * 2), /obj/item/storage/pouch/explosive, VENDOR_ITEM_REGULAR),
-		list("First-Aid Pouch (Full)", round(scale * 5), /obj/item/storage/pouch/firstaid/full, VENDOR_ITEM_REGULAR),
-		list("First Responder Pouch", round(scale * 2), /obj/item/storage/pouch/first_responder, VENDOR_ITEM_REGULAR),
-		list("Flare Pouch (Full)", round(scale * 5), /obj/item/storage/pouch/flare/full, VENDOR_ITEM_REGULAR),
-		list("Fuel Tank Strap Pouch", round(scale * 4), /obj/item/storage/pouch/flamertank, VENDOR_ITEM_REGULAR),
-		list("Large Pistol Magazine Pouch", round(scale * 5), /obj/item/storage/pouch/magazine/pistol/large, VENDOR_ITEM_REGULAR),
-		list("Magazine Pouch", round(scale * 5), /obj/item/storage/pouch/magazine, VENDOR_ITEM_REGULAR),
-		list("Medical Pouch", round(scale * 2), /obj/item/storage/pouch/medical, VENDOR_ITEM_REGULAR),
-		list("Medium General Pouch", round(scale * 2), /obj/item/storage/pouch/general/medium, VENDOR_ITEM_REGULAR),
-		list("Medkit Pouch", round(scale * 2), /obj/item/storage/pouch/medkit, VENDOR_ITEM_REGULAR),
-		list("Sidearm Pouch", round(scale * 15), /obj/item/storage/pouch/pistol, VENDOR_ITEM_REGULAR),
-		list("Syringe Pouch", round(scale * 2), /obj/item/storage/pouch/syringe, VENDOR_ITEM_REGULAR),
-		list("Tools Pouch (Full)", round(scale * 2), /obj/item/storage/pouch/tools/full, VENDOR_ITEM_REGULAR),
-
-		list("MISCELLANEOUS", -1, null, null),
-		list("Combat Flashlight", round(scale * 5), /obj/item/device/flashlight/combat, VENDOR_ITEM_REGULAR),
-		list("Entrenching Tool (ET)", round(scale * 4), /obj/item/tool/shovel/etool, VENDOR_ITEM_REGULAR),
-		list("Gas Mask", round(scale * 10), /obj/item/clothing/mask/gas, VENDOR_ITEM_REGULAR),
-		list("M89-S Signal Flare Pack", round(scale * 2), /obj/item/storage/box/m94/signal, VENDOR_ITEM_REGULAR),
-		list("M94 Marking Flare Pack", round(scale * 10), /obj/item/storage/box/m94, VENDOR_ITEM_REGULAR),
-		list("Machete Scabbard (Full)", round(scale * 20), /obj/item/storage/large_holster/machete/full, VENDOR_ITEM_REGULAR),
-		list("MB-6 Folding Barricades (x3)", round(scale * 1), /obj/item/stack/folding_barricade/three, VENDOR_ITEM_REGULAR)
-		)
+//---- ATTACHIES
+/obj/structure/machinery/cm_vending/sorted/attachments/wo
+	req_access = list(ACCESS_MARINE_CARGO)
+	vend_dir = NORTH
+	vend_dir_whitelist = list(SOUTHWEST, SOUTHEAST)

commit 13621bfea5ad66599be07015b7b499dbe8182610

Fix req ammo vendor not dispensing to the table (#4856)
# About the pull request

This PR fixes #4852 caused by #4803 simply because the vendor is not
adjacent to the table it is supposed to vend to.

# Explain why it's good for the game

It was unintentional for this to change.

# Testing Photographs and Procedure
<details>
<summary>Screenshots & Videos</summary>



https://github.com/cmss13-devs/cmss13/assets/76988376/e56607a3-7641-4c91-b585-fddeefb97098

After changing from turf to get_turf(user) per Fira:



https://github.com/cmss13-devs/cmss13/assets/76988376/a7a55df4-531c-40ea-a8a3-9b28d550f656

</details>


# Changelog
:cl: Drathek
fix: Fix the ColMarTechAutomated Munition Vendor in req not vending to
the table.
/:cl:

code/game/machinery/vending/cm_vending.dm
@@ -140,9 +140,9 @@ GLOBAL_LIST_EMPTY(vending_products)
 		if(vend_dir_whitelist)
 			var/user_dir = get_dir(loc, user)
 			if(!(user_dir in vend_dir_whitelist))
-				return turf
+				return get_turf(user)
 		var/turf/relative_turf = get_step(user, vend_dir)
-		if(relative_turf.Adjacent(src))
+		if(relative_turf)
 			return relative_turf
 	return turf
 
9642570f4661d489b76f689d623f3a0c7738dd0e
Drulikar <drathek@gmail.com>
2023-12-26 17:06:14 -0800

    Fix missing equipment button for fulton panel and fallback support panel

--- tgui/packages/tgui/interfaces/MfdPanels/FultonPanel.tsx
+++ tgui/packages/tgui/interfaces/MfdPanels/FultonPanel.tsx
@@ -70,6 +70,9 @@ export const FultonMfdPanel = (props: MfdProps, context) => {
           },
         },
       ]}
+      topButtons={[
+        { children: 'EQUIP', onClick: () => setPanelState('equipment') },
+      ]}
       bottomButtons={[
         {
           children: 'EXIT',

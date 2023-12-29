cmss13-devs/cmss13

commit 74887d9b1e59a3d6c39d1978e67321960f0b44ee

Fixes the the majority of "The the" in chat messages (Part 2) (#5121)
# About the pull request
Fixes instances of `"The the item"` and `"A the item"` in chat messages
so that they display as `"The item"` and `"A item"` instead.

See #5087 for more details.

# Explain why it's good for the game

Better english I guess.

# Testing Photographs and Procedure
<details>
<summary>Screenshots</summary>

**Before:**

![old](https://github.com/cmss13-devs/cmss13/assets/57483089/649bf896-e712-4f8c-9e7f-d64403a1e45a)

![pizza
before](https://github.com/cmss13-devs/cmss13/assets/57483089/d00a158f-fcab-4aec-b044-f5d591b12aad)

![cigarette
before](https://github.com/cmss13-devs/cmss13/assets/57483089/463a0b36-1ee9-4b3b-bf63-36326edd04c3)

![barricade
before](https://github.com/cmss13-devs/cmss13/assets/57483089/92bffce0-c2f4-47c9-8358-b9c36474c787)

**After:**

![new](https://github.com/cmss13-devs/cmss13/assets/57483089/991a230c-2e5a-4dac-8d32-4af91b957715)

![pizza
after](https://github.com/cmss13-devs/cmss13/assets/57483089/9bfb1388-d712-4386-beca-0c924e4f1adf)

![cigarette
after](https://github.com/cmss13-devs/cmss13/assets/57483089/fd707f3e-583e-49da-97c9-9972f9013de3)

![barricade
after](https://github.com/cmss13-devs/cmss13/assets/57483089/65d8d0c3-921a-41c2-a640-27f184db01d8)

</details>


# Changelog
:cl:
spellcheck: Fixed instances of "The the" and "A the" in chat messages so
that they're just "The" instead. (Part 2)
/:cl:

code/game/objects/items/storage/large_holster.dm
@@ -250,12 +250,12 @@
 		return FALSE
 
 	if(user.back != src)
-		to_chat(user, "The [src] must be equipped before you can switch types")
+		to_chat(user, SPAN_WARNING("[src] must be equipped before you can switch types."))
 		return
 
 	var/obj/item/weapon/gun/flamer/M240T/flamer = user.get_active_hand()
 	if(!istype(flamer))
-		to_chat(user, "You must be holding the M240-T incinerator unit to use [src]")
+		to_chat(user, SPAN_WARNING("You must be holding [flamer] to use [src]."))
 		return
 
 	if(!active_fuel)

code/game/objects/items/reagent_containers/food/snacks.dm
@@ -217,10 +217,10 @@
 	if(isanimal(M))
 		if(iscorgi(M))
 			if(bitecount == 0 || prob(50))
-				M.emote("nibbles away at the [src]")
+				M.emote("nibbles away at [src]")
 			bitecount++
 			if(bitecount >= 5)
-				var/sattisfaction_text = pick("burps from enjoyment", "yaps for more", "woofs twice", "looks at the area where the [src] was")
+				var/sattisfaction_text = pick("burps from enjoyment", "yaps for more", "woofs twice", "looks at the area where [src] was")
 				if(sattisfaction_text)
 					M.emote("[sattisfaction_text]")
 				qdel(src)
@@ -2801,7 +2801,7 @@
 	if( open && pizza )
 		user.put_in_hands( pizza )
 
-		to_chat(user, SPAN_DANGER("You take the [src.pizza] out of the [src]."))
+		to_chat(user, SPAN_DANGER("You take the [src.pizza] out of [src]."))
 		src.pizza = null
 		update_icon()
 		return
@@ -2852,11 +2852,11 @@
 				box.update_icon()
 				update_icon()
 
-				to_chat(user, SPAN_DANGER("You put the [box] ontop of the [src]!"))
+				to_chat(user, SPAN_DANGER("You put [box] ontop of [src]!"))
 			else
 				to_chat(user, SPAN_DANGER("The stack is too high!"))
 		else
-			to_chat(user, SPAN_DANGER("Close the [box] first!"))
+			to_chat(user, SPAN_DANGER("Close [box] first!"))
 
 		return
 
@@ -2868,9 +2868,9 @@
 
 			update_icon()
 
-			to_chat(user, SPAN_DANGER("You put the [I] in the [src]!"))
+			to_chat(user, SPAN_DANGER("You put [I] in [src]!"))
 		else
-			to_chat(user, SPAN_DANGER("You try to push the [I] through the lid but it doesn't work!"))
+			to_chat(user, SPAN_DANGER("You try to push [I] through the lid but it doesn't work!"))
 		return
 
 	if( istype(I, /obj/item/tool/pen/) )

code/game/objects/items/reagent_containers/glass.dm
@@ -237,7 +237,7 @@
 		overlays += lid
 
 /obj/item/reagent_container/glass/minitank
-	name = "MS-11 Smart Refill Tank"
+	name = "\improper MS-11 Smart Refill Tank"
 	desc = "A robust little tank capable of refilling autoinjectors that previously required a nanomed system to refill. Using the wonders of microchips, it automatically sorts the correct chemicals into most single reagent autoinjectors. It is unable to partially fill them however. A valve exists on the top to transfer reagents to another container or to flush it entirely."
 	icon = 'icons/obj/items/tank.dmi'
 	icon_state = "mini_reagent_tank"
@@ -277,7 +277,7 @@
 	if(istype(W, /obj/item/reagent_container/hypospray/autoinjector))
 		var/obj/item/reagent_container/hypospray/autoinjector/A = W
 		if(A.mixed_chem)
-			to_chat(user, SPAN_WARNING("The autoinjector doesn't fit into the [src]'s valve. It's probably not compatible."))
+			to_chat(user, SPAN_WARNING("The autoinjector doesn't fit into [src]'s valve. It's probably not compatible."))
 			return
 		if(reagents.has_reagent(A.chemname, A.volume))
 			reagents.trans_id_to(A, A.chemname, A.volume)
@@ -285,10 +285,10 @@
 			A.update_icon()
 			playsound(src.loc, 'sound/effects/refill.ogg', 25, 1, 3)
 		else
-			to_chat(user, SPAN_WARNING("A small LED on \the [src] blinks. The tank can't refill \the [A] - it's either incompatible or out of chemicals to fill it with!"))
+			to_chat(user, SPAN_WARNING("A small LED on [src] blinks. The tank can't refill [A] - it's either incompatible or out of chemicals to fill it with!"))
 			. = ..()
 			return
-		to_chat(user,SPAN_INFO("You successfully refill \the [W.name] with \the [src]!"))
+		to_chat(user, SPAN_INFO("You successfully refill [A] with [src]!"))
 
 /obj/item/reagent_container/glass/minitank/verb/flush_tank(mob/user)
 	set category = "Object"
@@ -299,7 +299,7 @@
 		to_chat(user, SPAN_WARNING("It's already empty!"))
 		return
 	playsound(src.loc, 'sound/effects/slosh.ogg', 25, 1, 3)
-	to_chat(user, SPAN_WARNING("You work the flush valve and successfully flush \the [src]'s contents!"))
+	to_chat(user, SPAN_WARNING("You work the flush valve and successfully flush [src]'s contents!"))
 	reagents.clear_reagents()
 	update_icon() // just to be sure
 	return
@@ -672,5 +672,5 @@
 	if(istype(AM) && (src in user))
 		user.visible_message("[user] starts to wipe down [AM] with [src]!")
 		if(do_after(user,30, INTERRUPT_ALL, BUSY_ICON_GENERIC))
-			user.visible_message("[user] finishes wiping off the [AM]!")
+			user.visible_message("[user] finishes wiping off [AM]!")
 			AM.clean_blood()

code/game/objects/items/robot/robot_upgrades.dm
@@ -12,7 +12,7 @@
 
 /obj/item/robot/upgrade/proc/action(mob/living/silicon/robot/R)
 	if(R.stat == DEAD)
-		to_chat(usr, SPAN_DANGER("The [src] will not function on a deceased robot."))
+		to_chat(usr, SPAN_DANGER("[src] will not function on a deceased robot."))
 		return 1
 	return 0
 

code/game/objects/items/tools/flame_tools.dm
@@ -233,12 +233,12 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 		light(SPAN_NOTICE("[user] fiddles with [W], and manages to light their [name]."))
 
 	else if(istype(W, /obj/item/attachable/attached_gun/flamer))
-		light(SPAN_NOTICE("[user] lights their [src] with the [W]."))
+		light(SPAN_NOTICE("[user] lights their [name] with [W]."))
 
 	else if(istype(W, /obj/item/weapon/gun/flamer))
 		var/obj/item/weapon/gun/flamer/F = W
 		if(!(F.flags_gun_features & GUN_TRIGGER_SAFETY))
-			light(SPAN_NOTICE("[user] lights their [src] with the pilot light of the [F]."))
+			light(SPAN_NOTICE("[user] lights their [name] with the pilot light of [F]."))
 		else
 			to_chat(user, SPAN_WARNING("Turn on the pilot light first!"))
 
@@ -246,20 +246,20 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 		var/obj/item/weapon/gun/G = W
 		for(var/slot in G.attachments)
 			if(istype(G.attachments[slot], /obj/item/attachable/attached_gun/flamer))
-				light(SPAN_NOTICE("[user] lights their [src] with [G.attachments[slot]]."))
+				light(SPAN_NOTICE("[user] lights their [name] with [G.attachments[slot]]."))
 				break
 
 	else if(istype(W, /obj/item/tool/surgery/cautery))
-		light(SPAN_NOTICE("[user] lights their [src] with the [W]."))
+		light(SPAN_NOTICE("[user] lights their [name] with [W]."))
 
 	else if(istype(W, /obj/item/clothing/mask/cigarette))
 		var/obj/item/clothing/mask/cigarette/C = W
 		if(C.item_state == icon_on)
-			light(SPAN_NOTICE("[user] lights their [src] with the [C] after a few attempts."))
+			light(SPAN_NOTICE("[user] lights their [name] with [C] after a few attempts."))
 
 	else if(istype(W, /obj/item/tool/candle))
 		if(W.heat_source > 200)
-			light(SPAN_NOTICE("[user] lights their [src] with the [W] after a few attempts."))
+			light(SPAN_NOTICE("[user] lights their [name] with [W] after a few attempts."))
 
 	return
 
@@ -529,12 +529,12 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 		light(SPAN_NOTICE("[user] fiddles with [W], and manages to light their [name] with the power of science."))
 
 	else if(istype(W, /obj/item/attachable/attached_gun/flamer))
-		light(SPAN_NOTICE("[user] lights their [src] with the [W], bet that would have looked cooler if it was attached to something first!"))
+		light(SPAN_NOTICE("[user] lights their [name] with [W], bet that would have looked cooler if it was attached to something first!"))
 
 	else if(istype(W, /obj/item/weapon/gun/flamer))
 		var/obj/item/weapon/gun/flamer/F = W
 		if(!(F.flags_gun_features & GUN_TRIGGER_SAFETY))
-			light(SPAN_NOTICE("[user] lights their [src] with the pilot light of the [F], the glint of pyromania in their eye."))
+			light(SPAN_NOTICE("[user] lights their [name] with the pilot light of [F], the glint of pyromania in their eye."))
 		else
 			to_chat(user, SPAN_WARNING("Turn on the pilot light first!"))
 
@@ -546,16 +546,16 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 				break
 
 	else if(istype(W, /obj/item/tool/surgery/cautery))
-		light(SPAN_NOTICE("[user] lights their [src] with the [W], that can't be sterile!"))
+		light(SPAN_NOTICE("[user] lights their [name] with [W], that can't be sterile!"))
 
 	else if(istype(W, /obj/item/clothing/mask/cigarette))
 		var/obj/item/clothing/mask/cigarette/C = W
 		if(C.item_state == icon_on)
-			light(SPAN_NOTICE("[user] lights their [src] with the [C] after a few attempts."))
+			light(SPAN_NOTICE("[user] lights their [name] with [C] after a few attempts."))
 
 	else if(istype(W, /obj/item/tool/candle))
 		if(W.heat_source > 200)
-			light(SPAN_NOTICE("[user] lights their [src] with the [W] after a few attempts."))
+			light(SPAN_NOTICE("[user] lights their [name] with [W] after a few attempts."))
 
 /////////////////
 //SMOKING PIPES//
@@ -751,7 +751,7 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 			else
 				playsound(src.loc,"lighter",10, 1, 3)
 				if(prob(95))
-					user.visible_message(SPAN_NOTICE("After a few attempts, [user] manages to light the [src]."))
+					user.visible_message(SPAN_NOTICE("After a few attempts, [user] manages to light [src]."))
 
 				else
 					to_chat(user, SPAN_WARNING("You burn yourself while lighting the lighter."))
@@ -759,7 +759,7 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 						user.apply_damage(2,BURN,"l_hand")
 					else
 						user.apply_damage(2,BURN,"r_hand")
-					user.visible_message(SPAN_NOTICE("After a few attempts, [user] manages to light the [src], they however burn their finger in the process."))
+					user.visible_message(SPAN_NOTICE("After a few attempts, [user] manages to light [src], they however burn their finger in the process."))
 
 			set_light_range(2)
 			set_light_on(TRUE)
@@ -777,10 +777,10 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 		item_state = icon_off
 		if(!silent)
 			if(istype(src, /obj/item/tool/lighter/zippo) )
-				bearer.visible_message("<span class='rose'>You hear a quiet click, as [bearer] shuts off [src] without even looking at what they're doing.")
+				bearer.visible_message(SPAN_ROSE("You hear a quiet click, as [bearer] shuts off [src] without even looking at what they're doing."))
 				playsound(src.loc,"zippo_close",10, 1, 3)
 			else
-				bearer.visible_message(SPAN_NOTICE("[bearer] quietly shuts off the [src]."))
+				bearer.visible_message(SPAN_NOTICE("[bearer] quietly shuts off [src]."))
 
 		set_light_on(FALSE)
 		STOP_PROCESSING(SSobj, src)
@@ -805,4 +805,3 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 				cig.light(SPAN_NOTICE("[user] holds the [name] out for [M], and lights the [cig.name]."))
 	else
 		..()
-

code/game/objects/items/tools/kitchen_tools.dm
@@ -218,7 +218,7 @@
 	var/cooldown = 0
 
 /obj/item/tool/kitchen/tray/attack(mob/living/carbon/M, mob/living/carbon/user)
-	to_chat(user, SPAN_WARNING("You accidentally slam yourself with the [src]!"))
+	to_chat(user, SPAN_WARNING("You accidentally slam yourself with [src]!"))
 	user.apply_effect(1, WEAKEN)
 	user.take_limb_damage(2)
 

code/game/objects/items/tools/maintenance_tools.dm
@@ -96,8 +96,8 @@
 		if(E)
 			var/safety = H.get_eye_protection()
 			if(!safety)
-				to_chat(user, SPAN_DANGER("You stab [H] in the eyes with the [src]!"))
-				visible_message(SPAN_DANGER("[user] stabs [H] in the eyes with the [src]!"))
+				user.visible_message(SPAN_DANGER("[user] stabs [H] in the eyes with [src]!"),
+					SPAN_DANGER("You stab [H] in the eyes with [src]!"))
 				E.take_damage(rand(8,20))
 	return ..()
 /obj/item/tool/screwdriver/tactical

code/game/objects/items/tools/misc_tools.dm
@@ -132,7 +132,7 @@
 			qdel(I) //delete the paper item
 			labels_left = initial(labels_left)
 		else
-			to_chat(user, SPAN_NOTICE("The [src] is already full."))
+			to_chat(user, SPAN_NOTICE("[src] is already full."))
 
 /*
 	Instead of updating labels_left to user every label used,

commit 5a24a69b1d6a17e63f928d51c6ce636b5c24cb12

Fixes the failure message when trying to switch pyro fuels (#5321)
# About the pull request

Edits the pyro spec's fuel pack a bit to make it *slightly* more
bug-proof, and to fix it sometimes giving weird failure messages like
these:

![image](https://github.com/cmss13-devs/cmss13/assets/57483089/2fa746d0-acc6-402b-ad17-e8fff5f4e3cd)

![image](https://github.com/cmss13-devs/cmss13/assets/57483089/67cef060-55a4-4897-9e52-f6d01e43d3d1)

This was caused by me in #5121, presumably because I misread how the
check was handled.

# Explain why it's good for the game

I broked it (sorry)

# Testing Photographs and Procedure
<details>
<summary>Screenshots & Videos</summary>

Put screenshots and videos here with an empty line between the
screenshots and the `<details>` tags.

</details>


# Changelog
:cl:
fix: Fixed the pyro spec's fuel pack sometimes giving weird failure
messages when trying to switch fuel.
/:cl:

code/game/objects/items/storage/large_holster.dm
@@ -253,9 +253,12 @@
 		to_chat(user, SPAN_WARNING("[src] must be equipped before you can switch types."))
 		return
 
-	var/obj/item/weapon/gun/flamer/M240T/flamer = user.get_active_hand()
-	if(!istype(flamer))
-		to_chat(user, SPAN_WARNING("You must be holding [flamer] to use [src]."))
+	if(!linked_flamer)
+		to_chat(user, SPAN_WARNING("An incinerator unit must be linked in order to switch fuel types."))
+		return
+
+	if(user.get_active_hand() != linked_flamer)
+		to_chat(user, SPAN_WARNING("You must be holding [linked_flamer] to use [src]."))
 		return
 
 	if(!active_fuel)
@@ -276,8 +279,8 @@
 
 	to_chat(user, "You switch the fuel tank to <b>[active_fuel.caliber]</b>")
 	playsound(src, 'sound/machines/click.ogg', 25, TRUE)
-	flamer.current_mag = active_fuel
-	flamer.update_icon()
+	linked_flamer.current_mag = active_fuel
+	linked_flamer.update_icon()
 
 	return TRUE
 
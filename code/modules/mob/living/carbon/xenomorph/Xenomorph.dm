cmss13-devs/cmss13

commit d9982451a5737062f6ab2c2990cb404fe384b746

Xeno strains refactor (#5542)
# About the pull request

Refactors xeno strains by taking out the old
`/datum/xeno_mutator`/`/datum/mutator_set` system and replacing it with
a new `datum/xeno_strain`, since as far as I'm aware mutators haven't
been touched in almost 5 years.

As a side note, the DMI changes are from me deleting the icon states of
the unused 'Tremor' Burrower strain, since I figured it fit within the
bounds of the PR. (The strain's sprites are identical to that of the
base caste.)

# Explain why it's good for the game

The new datum should be a lot more streamlined than the old system, and
require much less boilerplate when making a new strain. Ideally this
could even replace `/datum/behavior_delagate` with signal handlers in
the future, although a few more refactors would be required before
that's possible.


# Testing Photographs and Procedure
<details>
<summary>Screenshots & Videos</summary>

Put screenshots and videos here with an empty line between the
screenshots and the `<details>` tags.

</details>


# Changelog
:cl:
refactor: Refactored xeno strains.
add: Added a keybind setting to open the 'Purchase Strain' window. (Also
renamed the macro)
/:cl:

code/modules/mob/living/carbon/xenomorph/Xenomorph.dm
@@ -130,10 +130,8 @@
 	var/weed_level = WEED_LEVEL_STANDARD
 	var/acid_level = 0
 
-	// Mutator-related and other important vars
-	var/mutation_icon_state = null
-	var/mutation_type = null
-	var/datum/mutator_set/individual_mutators/mutators = new
+	/// The xeno's strain, if they've taken one.
+	var/datum/xeno_strain/strain = null
 
 	// Hive-related vars
 	var/datum/hive_status/hive
@@ -388,8 +386,6 @@
 	if(hive)
 		for(var/trait in hive.hive_inherant_traits)
 			ADD_TRAIT(src, trait, TRAIT_SOURCE_HIVE)
-
-	mutators.xeno = src
 
 	//Set caste stuff
 	if(caste_type && GLOB.xeno_datum_list[caste_type])
@@ -661,8 +657,8 @@
 			. += "It appears to belong to [hive?.name ? "the [hive.name]" : "a different hive"]."
 
 	if(isxeno(user) || isobserver(user))
-		if(mutation_type != "Normal")
-			. += "It has specialized into a [mutation_type]."
+		if(strain)
+			. += "It has specialized into a [strain.name]."
 
 	if(iff_tag)
 		. += SPAN_NOTICE("It has an IFF tag sticking out of its carapace.")
@@ -693,7 +689,7 @@
 	selected_ability = null
 	queued_action = null
 
-	QDEL_NULL(mutators)
+	QDEL_NULL(strain)
 	QDEL_NULL(behavior_delegate)
 
 	built_structures = null
@@ -709,17 +705,10 @@
 	if(hardcore)
 		attack_log?.Cut() // Completely clear out attack_log to limit mem usage if we fail to delete
 
-	. = ..()
-
-	// Everything below fits the "we have to clear by principle it but i dont wanna break stuff" bill
-	mutators = null
-
-
+	return ..()
 
 /mob/living/carbon/xenomorph/slip(slip_source_name, stun_level, weaken_level, run_only, override_noslip, slide_steps)
 	return FALSE
-
-
 
 /mob/living/carbon/xenomorph/start_pulling(atom/movable/AM, lunge, no_msg)
 	if(SEND_SIGNAL(AM, COMSIG_MOVABLE_XENO_START_PULLING, src) & COMPONENT_ALLOW_PULL)
@@ -823,10 +812,10 @@
 
 
 //*********************************************************//
-//********************Mutator functions********************//
+// ******************** Strain Procs **********************//
 //*********************************************************//
 
-//Call this function when major changes happen - evolutions, upgrades, mutators getting removed
+//Call this proc when major changes happen - evolutions, upgrades, mutators getting removed
 /mob/living/carbon/xenomorph/proc/recalculate_everything()
 	recalculate_stats()
 	recalculate_actions()
@@ -850,8 +839,6 @@
 	tackle_min = caste.tackle_min
 	tackle_max = caste.tackle_max
 	tackle_chance = caste.tackle_chance + tackle_chance_modifier
-	tacklestrength_min = caste.tacklestrength_min + mutators.tackle_strength_bonus + hive.mutators.tackle_strength_bonus
-	tacklestrength_max = caste.tacklestrength_max + mutators.tackle_strength_bonus + hive.mutators.tackle_strength_bonus
 
 /mob/living/carbon/xenomorph/proc/recalculate_health()
 	var/new_max_health = nocrit ? health_modifier + maxHealth : health_modifier + caste.max_health
@@ -907,18 +894,8 @@
 /mob/living/carbon/xenomorph/proc/recalculate_actions()
 	recalculate_acid()
 	recalculate_weeds()
-	pull_multiplier = mutators.pull_multiplier
-	if(isrunner(src))
-		//Xeno runners need a small nerf to dragging speed mutator
-		pull_multiplier = 1 - (1 - mutators.pull_multiplier) * 0.85
-		if(is_zoomed)
-			zoom_out()
-	if(iscarrier(src))
-		var/mob/living/carbon/xenomorph/carrier/carrier = src
-		carrier.huggers_max = caste.huggers_max
-		carrier.eggs_max = caste.eggs_max
-	need_weeds = mutators.need_weeds
-
+	// Modified on subtypes
+	pull_multiplier = initial(pull_multiplier)
 
 /mob/living/carbon/xenomorph/proc/recalculate_acid()
 	if(caste)
@@ -1043,7 +1020,7 @@
 		handle_ghost_message()
 
 /mob/living/carbon/xenomorph/proc/handle_ghost_message()
-	notify_ghosts("[src] ([mutation_type] [caste_type]) has ghosted and their body is up for grabs!", source = src)
+	notify_ghosts("[src] ([get_strain_name()] [caste_type]) has ghosted and their body is up for grabs!", source = src)
 
 /mob/living/carbon/xenomorph/larva/handle_ghost_message()
 	if(locate(/obj/effect/alien/resin/special/pylon/core) in range(2, get_turf(src)))

commit 1675e4eeb90fd4051de60688c6d6c5e24dec555b

Fixes xeno tacklestrength (#5773)
# About the pull request

Fixes #5765.
This was caused by me in #5542 deleting a couple of lines from
`/mob/living/carbon/xenomorph/proc/recalculate_tackle()`, when I should
have only removed the `mutators`/`hive.mutators` part.


![image](https://github.com/cmss13-devs/cmss13/assets/57483089/b54a3e03-03cd-49a3-a093-ab0765059a7b)

# Explain why it's good for the game

Whoops

# Testing Photographs and Procedure
<details>
<summary>Screenshots & Videos</summary>

`tacklestrength_min` and `tacklestrength_max` are being applied
correctly:

![image](https://github.com/cmss13-devs/cmss13/assets/57483089/8404ddf3-17e0-42a5-a3cf-91a85beb1e50)

</details>


# Changelog
:cl:
fix: Fixed Xenomorphs not having their caste's tackle duration values
applied to them.
/:cl:

code/modules/mob/living/carbon/xenomorph/Xenomorph.dm
@@ -839,6 +839,8 @@
 	tackle_min = caste.tackle_min
 	tackle_max = caste.tackle_max
 	tackle_chance = caste.tackle_chance + tackle_chance_modifier
+	tacklestrength_min = caste.tacklestrength_min
+	tacklestrength_max = caste.tacklestrength_max
 
 /mob/living/carbon/xenomorph/proc/recalculate_health()
 	var/new_max_health = nocrit ? health_modifier + maxHealth : health_modifier + caste.max_health
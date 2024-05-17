a8ee00642b73ee82884a37c0b5f04dc4daa4f5d3
Drulikar <drathek@gmail.com>
2024-05-16 21:30:52 -0700

    Rename auto observe tooltip to full observe; I don't know why TG named it that.

--- tgui/packages/tgui/interfaces/Orbit/index.tsx
+++ tgui/packages/tgui/interfaces/Orbit/index.tsx
@@ -96,7 +96,7 @@ const ObservableSearch = (props: {
             color={auto_observe ? 'good' : 'transparent'}
             icon={auto_observe ? 'toggle-on' : 'toggle-off'}
             onClick={() => act('toggle_auto_observe')}
-            tooltip={multiline`Toggle Auto-Observe. When active, you'll
+            tooltip={multiline`Toggle Full Observe. When active, you'll
             see the UI / full inventory of whoever you're orbiting. Neat!`}
             tooltipPosition="bottom-start"
           />

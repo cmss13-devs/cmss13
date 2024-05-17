915d4762b825f06435b8c87d84f2aa015086ea53
Drulikar <drathek@gmail.com>
2024-05-17 00:18:37 -0700

    Fix colorbox dot and remove unused prop

--- tgui/packages/tgui-panel/ping/PingIndicator.jsx
+++ tgui/packages/tgui-panel/ping/PingIndicator.jsx
@@ -26,7 +26,6 @@ export const PingIndicator = (props) => {
       width="50px"
       className="Ping"
       color="transparent"
-      hover
       py="0.125em" // Override what light theme does to this
       px="0.25em" // Override what light theme does to this
       tooltip="Ping relays"

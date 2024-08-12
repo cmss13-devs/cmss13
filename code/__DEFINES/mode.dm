cmss13-devs/cmss13

commit df6fce4ac691b23d23c3f1ceb1846b295463b30c

Separates Factions In Observe/Follow Tab (#5058)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

# About the pull request

Separates the factions from a generic 'ERT Faction' tab into their own
separate one's for each of the main factions, this includes.

- WY
- UPP
- CLF
- Freelancers
- Mercenaries
- Contractors
- Dutch's Dozen
- TWE RMC
- CMB Marshals

The remaining factions (mostly memes, unused or admin ones) are kept in
the current 'ERT' panel.

# Explain why it's good for the game

Better gauge as a observer who the present factions are, helps in the
uncommon circumstances where multiple factions are in play.


# Testing Photographs and Procedure
<!-- Include any screenshots/videos/debugging steps of the modified code
functioning successfully, ideally including edge cases. -->
<details>
<summary>Screenshots & Videos</summary>

Put screenshots and videos here with an empty line between the
screenshots and the `<details>` tags.

</details>


# Changelog

<!-- If your PR modifies aspects of the game that can be concretely
observed by players or admins you should add a changelog. If your change
does NOT meet this description, remove this section. Be sure to properly
label your changes in the changelog. Please note that maintainers freely
reserve the right to remove and add tags should they deem it
appropriate. You can attempt to finagle the system all you want, but
it's best to shoot for clear communication right off the bat. -->
<!-- If you add a name after the ':cl', that name will be used in the
changelog. You must add your CKEY after the CL if your GitHub name
doesn't match. Maintainers freely reserve the right to remove and add
tags should they deem it appropriate. -->

:cl:
add: Factions are now properly separated in the observe menu as a ghost.
/:cl:

<!-- Both :cl:'s are required for the changelog to work! -->

---------

Co-authored-by: Steelpoint <alexander.henley@hotmai.com>

code/__DEFINES/mode.dm
@@ -241,8 +241,16 @@ GLOBAL_LIST_INIT(whitelist_hierarchy, list(WHITELIST_NORMAL, WHITELIST_COUNCIL, 
 
 #define FACTION_LIST_MARINE list(FACTION_MARINE)
 #define FACTION_LIST_HUMANOID list(FACTION_MARINE, FACTION_PMC, FACTION_WY, FACTION_WY_DEATHSQUAD, FACTION_CLF, FACTION_CONTRACTOR, FACTION_MARSHAL, FACTION_UPP, FACTION_FREELANCER, FACTION_SURVIVOR, FACTION_NEUTRAL, FACTION_COLONIST, FACTION_MERCENARY, FACTION_DUTCH, FACTION_HEFA, FACTION_GLADIATOR, FACTION_PIRATE, FACTION_PIZZA, FACTION_SOUTO, FACTION_YAUTJA, FACTION_ZOMBIE, FACTION_TWE)
-#define FACTION_LIST_ERT list(FACTION_PMC, FACTION_WY_DEATHSQUAD, FACTION_CLF, FACTION_CONTRACTOR, FACTION_UPP, FACTION_FREELANCER, FACTION_MERCENARY, FACTION_DUTCH, FACTION_HEFA, FACTION_GLADIATOR, FACTION_PIRATE, FACTION_PIZZA, FACTION_SOUTO, FACTION_MARSHAL, FACTION_TWE)
+#define FACTION_LIST_ERT list(FACTION_HEFA, FACTION_GLADIATOR, FACTION_PIRATE, FACTION_PIZZA, FACTION_SOUTO)
 #define FACTION_LIST_WY list(FACTION_PMC, FACTION_WY_DEATHSQUAD, FACTION_WY)
+#define FACTION_LIST_UPP list(FACTION_UPP)
+#define FACTION_LIST_CLF list(FACTION_CLF)
+#define FACTION_LIST_TWE list(FACTION_TWE)
+#define FACTION_LIST_FREELANCER list(FACTION_FREELANCER)
+#define FACTION_LIST_CONTRACTOR list(FACTION_CONTRACTOR)
+#define FACTION_LIST_MERCENARY list(FACTION_MERCENARY)
+#define FACTION_LIST_MARSHAL list(FACTION_MARSHAL)
+#define FACTION_LIST_DUTCH list(FACTION_DUTCH)
 #define FACTION_LIST_MARINE_WY list(FACTION_MARINE, FACTION_PMC, FACTION_WY_DEATHSQUAD, FACTION_WY)
 #define FACTION_LIST_MARINE_UPP list(FACTION_MARINE, FACTION_UPP)
 #define FACTION_LIST_MARINE_TWE list(FACTION_MARINE, FACTION_TWE)

code/modules/mob/dead/observer/orbit.dm
@@ -53,6 +53,15 @@
 	var/list/survivors = list()
 	var/list/xenos = list()
 	var/list/ert_members = list()
+	var/list/upp = list()
+	var/list/clf = list()
+	var/list/wy = list()
+	var/list/twe = list()
+	var/list/freelancer = list()
+	var/list/contractor = list()
+	var/list/mercenary = list()
+	var/list/dutch = list()
+	var/list/marshal = list()
 	var/list/synthetics = list()
 	var/list/predators = list()
 	var/list/animals = list()
@@ -132,12 +141,30 @@
 
 				if(SSticker.mode.is_in_endgame == TRUE && !is_mainship_level(M.z) && !(human.faction in FACTION_LIST_ERT))
 					escaped += list(serialized)
+				else if(human.faction in FACTION_LIST_WY)
+					wy += list(serialized)
 				else if(issynth(human) && !isinfiltratorsynthetic(human))
 					synthetics += list(serialized)
 				else if(isyautja(human))
 					predators += list(serialized)
 				else if(human.faction in FACTION_LIST_ERT)
 					ert_members += list(serialized)
+				else if(human.faction in FACTION_LIST_UPP)
+					upp += list(serialized)
+				else if(human.faction in FACTION_LIST_CLF)
+					clf += list(serialized)
+				else if(human.faction in FACTION_LIST_TWE)
+					twe += list(serialized)
+				else if(human.faction in FACTION_LIST_FREELANCER)
+					freelancer += list(serialized)
+				else if(human.faction in FACTION_LIST_CONTRACTOR)
+					contractor += list(serialized)
+				else if(human.faction in FACTION_LIST_MERCENARY)
+					mercenary += list(serialized)
+				else if(human.faction in FACTION_LIST_MARSHAL)
+					marshal += list(serialized)
+				else if(human.faction in FACTION_LIST_DUTCH)
+					dutch += list(serialized)
 				else if(human.faction in FACTION_LIST_MARINE)
 					marines += list(serialized)
 				else if(issurvivorjob(human.job))
@@ -156,6 +183,15 @@
 	data["survivors"] = survivors
 	data["xenos"] = xenos
 	data["ert_members"] = ert_members
+	data["upp"] = upp
+	data["clf"] = clf
+	data["wy"] = wy
+	data["twe"] = twe
+	data["freelancer"] = freelancer
+	data["contractor"] = contractor
+	data["mercenary"] = mercenary
+	data["dutch"] = dutch
+	data["marshal"] = marshal
 	data["synthetics"] = synthetics
 	data["predators"] = predators
 	data["animals"] = animals

code/modules/gear_presets/contractor.dm
@@ -4,7 +4,7 @@
 	rank = JOB_CONTRACTOR
 	idtype = /obj/item/card/id/data
 	faction = FACTION_CONTRACTOR
-	faction_group = FACTION_LIST_ERT
+	faction_group = list(FACTION_CONTRACTOR)
 	languages = list(LANGUAGE_ENGLISH, LANGUAGE_SPANISH, LANGUAGE_RUSSIAN)
 	var/human_versus_human = FALSE
 	var/headset_type = /obj/item/device/radio/headset/distress/contractor

tgui/packages/tgui/interfaces/Orbit/index.tsx
@@ -118,6 +118,15 @@ const ObservableContent = (props, context) => {
     survivors = [],
     xenos = [],
     ert_members = [],
+    upp = [],
+    clf = [],
+    wy = [],
+    twe = [],
+    freelancer = [],
+    mercenary = [],
+    contractor = [],
+    dutch = [],
+    marshal = [],
     synthetics = [],
     predators = [],
     animals = [],
@@ -144,6 +153,43 @@ const ObservableContent = (props, context) => {
         color="light-grey"
         section={synthetics}
         title="Synthetics"
+      />
+      <ObservableSection
+        color="green"
+        section={upp}
+        title="Union of Progressive Peoples"
+      />
+      <ObservableSection
+        color="teal"
+        section={clf}
+        title="Colonial Liberation Front"
+      />
+      <ObservableSection color="white" section={wy} title="Weyland Yutani" />
+      <ObservableSection
+        color="red"
+        section={twe}
+        title="Royal Marines Commando"
+      />
+      <ObservableSection
+        color="orange"
+        section={freelancer}
+        title="Freelancers"
+      />
+      <ObservableSection
+        color="label"
+        section={mercenary}
+        title="Mercenaries"
+      />
+      <ObservableSection
+        color="light-grey"
+        section={contractor}
+        title="Military Contractors"
+      />
+      <ObservableSection color="good" section={dutch} title="Dutchs Dozen" />
+      <ObservableSection
+        color="dark-blue"
+        section={marshal}
+        title="Colonial Marshal Bureau"
       />
       <ObservableSection color="green" section={predators} title="Predators" />
       <ObservableSection color="olive" section={escaped} title="Escaped" />

tgui/packages/tgui/interfaces/Orbit/types.ts
@@ -7,6 +7,15 @@ export type OrbitData = {
   survivors: Observable[];
   xenos: Observable[];
   ert_members: Observable[];
+  upp: Observable[];
+  twe: Observable[];
+  clf: Observable[];
+  wy: Observable[];
+  freelancer: Observable[];
+  contractor: Observable[];
+  mercenary: Observable[];
+  dutch: Observable[];
+  marshal: Observable[];
   synthetics: Observable[];
   predators: Observable[];
   animals: Observable[];

commit 2e0dee2078fd157e68e2e546ea929c07fee43b7f

ERT  no longer show up in the orbit escape section (#6910)
# About the pull request

<!-- Remove this text and explain what the purpose of your PR is.

Mention if you have tested your changes. If you changed a map, make sure
you used the mapmerge tool.
If this is an Issue Correction, you can type "Fixes Issue #169420" to
link the PR to the corresponding Issue number #169420.

Remember: something that is self-evident to you might not be to others.
Explain your rationale fully, even if you feel it goes without saying.
-->
ERT factions no longer show up in the escape section.

This issue was caused by #5058

# Explain why it's good for the game
Makes it clearer who has actually escaped and not ERT returning to their
base.

# Testing Photographs and Procedure
<details>
<summary>Screenshots & Videos</summary>

Put screenshots and videos here with an empty line between the
screenshots and the `<details>` tags.

</details>


# Changelog
:cl:
fix: ERT no longer show up in the orbit escape section
/:cl:

code/__DEFINES/mode.dm
@@ -280,7 +280,8 @@ DEFINE_BITFIELD(whitelist_status, list(
 
 #define FACTION_LIST_MARINE list(FACTION_MARINE)
 #define FACTION_LIST_HUMANOID list(FACTION_MARINE, FACTION_PMC, FACTION_WY, FACTION_WY_DEATHSQUAD, FACTION_CLF, FACTION_CONTRACTOR, FACTION_MARSHAL, FACTION_UPP, FACTION_FREELANCER, FACTION_SURVIVOR, FACTION_NEUTRAL, FACTION_COLONIST, FACTION_MERCENARY, FACTION_DUTCH, FACTION_HEFA, FACTION_GLADIATOR, FACTION_PIRATE, FACTION_PIZZA, FACTION_SOUTO, FACTION_YAUTJA, FACTION_ZOMBIE, FACTION_TWE)
-#define FACTION_LIST_ERT list(FACTION_HEFA, FACTION_GLADIATOR, FACTION_PIRATE, FACTION_PIZZA, FACTION_SOUTO)
+#define FACTION_LIST_ERT_OTHER list(FACTION_HEFA, FACTION_GLADIATOR, FACTION_PIRATE, FACTION_PIZZA, FACTION_SOUTO)
+#define FACTION_LIST_ERT_ALL list(FACTION_PMC, FACTION_WY_DEATHSQUAD, FACTION_WY, FACTION_CLF, FACTION_CONTRACTOR, FACTION_UPP, FACTION_FREELANCER, FACTION_MERCENARY, FACTION_DUTCH, FACTION_HEFA, FACTION_GLADIATOR, FACTION_PIRATE, FACTION_PIZZA, FACTION_SOUTO, FACTION_MARSHAL, FACTION_TWE)
 #define FACTION_LIST_WY list(FACTION_PMC, FACTION_WY_DEATHSQUAD, FACTION_WY)
 #define FACTION_LIST_UPP list(FACTION_UPP)
 #define FACTION_LIST_CLF list(FACTION_CLF)

code/modules/mob/dead/observer/orbit.dm
@@ -144,7 +144,7 @@
 				else
 					serialized["background_color"] = human.assigned_equipment_preset?.minimap_background
 
-				if(SSticker.mode.is_in_endgame == TRUE && !is_mainship_level(M.z) && !(human.faction in FACTION_LIST_ERT))
+				if(SSticker.mode.is_in_endgame == TRUE && !is_mainship_level(M.z) && !(human.faction in FACTION_LIST_ERT_ALL))
 					escaped += list(serialized)
 				else if(human.faction in FACTION_LIST_WY)
 					wy += list(serialized)
@@ -152,7 +152,7 @@
 					synthetics += list(serialized)
 				else if(isyautja(human))
 					predators += list(serialized)
-				else if(human.faction in FACTION_LIST_ERT)
+				else if(human.faction in FACTION_LIST_ERT_OTHER)
 					ert_members += list(serialized)
 				else if(human.faction in FACTION_LIST_UPP)
 					upp += list(serialized)
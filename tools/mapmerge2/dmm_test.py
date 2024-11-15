abe5ff18fc0a857b883c0652f79029bee7bda224
Drulikar <drathek@gmail.com>
2024-11-15 02:18:48 -0600

    Why is this so jank

--- tools/mapmerge2/dmm_test.py
+++ tools/mapmerge2/dmm_test.py
@@ -20,7 +20,7 @@ def _self_test():
         pass
 
     # Read the ancestor commit.
-    repo.remotes["upstream"].fetch()
+    repo.remotes["upstream"].fetch(repo.remotes["upstream"].fetch_refspecs)
     ancestor = repo.merge_base(repo.head.target, repo.revparse_single("refs/remotes/upstream/master").id)
     ancestor_commit = None
     if not ancestor:

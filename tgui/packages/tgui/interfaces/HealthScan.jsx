d305e518a400b0277b5f303bb8043279b64f2121
Katherine Kiefer <sterlite2902@gmail.com>
2025-01-19 18:22:13 +1100

    move it to the side lmao

--- tgui/packages/tgui/interfaces/HealthScan.jsx
+++ tgui/packages/tgui/interfaces/HealthScan.jsx
@@ -13,6 +13,8 @@ import {
 } from '../components';
 import { Window } from '../layouts';
 
+const sectionWidth = 35;
+
 export const HealthScan = (props) => {
   const { act, data } = useBackend();
   const {
@@ -74,213 +76,220 @@ export const HealthScan = (props) => {
     holocard_message = 'Patient has no active holocard.';
   }
   return (
-    <Window width={500} height={bodyscanner ? 700 : 600} theme={theme}>
+    <Window width={862} height={bodyscanner ? 700 : 600} theme={theme}>
       <Window.Content scrollable>
-        <Section title={'Patient: ' + patient}>
-          {hugged && ghostscan ? (
-            <NoticeBox danger>
-              Patient has been implanted with an alien embryo!
-            </NoticeBox>
-          ) : null}
-          {dead ? <NoticeBox danger>Patient is deceased!</NoticeBox> : null}
-          {ssd ? (
-            <NoticeBox warning color="grey">
-              {ssd}
-            </NoticeBox>
-          ) : null}
-          <LabeledList>
-            <LabeledList.Item label="Health">
-              {health >= 0 ? (
-                <ProgressBar
-                  value={health / 100}
-                  ranges={{
-                    good: [0.7, Infinity],
-                    average: [0.2, 0.7],
-                    bad: [-Infinity, 0.2],
-                  }}
-                >
-                  {health}% healthy
-                </ProgressBar>
-              ) : (
-                <ProgressBar
-                  value={1 + health / 100}
-                  ranges={{
-                    bad: [-Infinity, Infinity],
-                  }}
-                >
-                  {health}% healthy
-                </ProgressBar>
-              )}
-            </LabeledList.Item>
-            {dead ? (
-              <LabeledList.Item label="Condition">
-                <Box color={permadead ? 'red' : 'green'} bold={1}>
-                  {permadead
-                    ? heart_broken
-                      ? 'Myocardial rupture, surgical intervention required'
-                      : 'Permanently deceased'
-                    : Synthetic
-                      ? 'Central power system shutdown, reboot with a reset key possible'
-                      : 'Cardiac arrest, defibrillation possible'}
-                </Box>
-              </LabeledList.Item>
-            ) : null}
-            <LabeledList.Item label="Damage">
-              <Box inline>
-                <ProgressBar>
-                  Brute:{' '}
-                  <Box inline bold color={'red'}>
-                    {total_brute}
-                  </Box>
-                </ProgressBar>
-              </Box>
-              <Box inline width={'5px'} />
-              <Box inline>
-                <ProgressBar>
-                  Burn:{' '}
-                  <Box inline bold color={'#ffb833'}>
-                    {total_burn}
-                  </Box>
-                </ProgressBar>
-              </Box>
-              <Box inline width={'5px'} />
-              <Box inline>
-                <ProgressBar>
-                  Toxin:{' '}
-                  <Box inline bold color={'green'}>
-                    {toxin}
-                  </Box>
-                </ProgressBar>
-              </Box>
-              <Box inline width={'5px'} />
-              <Box inline>
-                <ProgressBar>
-                  Oxygen:{' '}
-                  <Box inline bold color={'blue'}>
-                    {oxy}
-                  </Box>
-                </ProgressBar>
-              </Box>
-              <Box inline width={'5px'} />
-              {!!clone && (
-                <Box inline>
-                  <ProgressBar>
-                    Clone:{' '}
-                    <Box inline color={'teal'}>
-                      {clone}
+        <Stack>
+          <Stack.Item>
+            <Section title={'Patient: ' + patient} maxWidth={sectionWidth}>
+              {hugged && ghostscan ? (
+                <NoticeBox danger>
+                  Patient has been implanted with an alien embryo!
+                </NoticeBox>
+              ) : null}
+              {dead ? <NoticeBox danger>Patient is deceased!</NoticeBox> : null}
+              {ssd ? (
+                <NoticeBox warning color="grey">
+                  {ssd}
+                </NoticeBox>
+              ) : null}
+              <LabeledList>
+                <LabeledList.Item label="Health">
+                  {health >= 0 ? (
+                    <ProgressBar
+                      value={health / 100}
+                      ranges={{
+                        good: [0.7, Infinity],
+                        average: [0.2, 0.7],
+                        bad: [-Infinity, 0.2],
+                      }}
+                    >
+                      {health}% healthy
+                    </ProgressBar>
+                  ) : (
+                    <ProgressBar
+                      value={1 + health / 100}
+                      ranges={{
+                        bad: [-Infinity, Infinity],
+                      }}
+                    >
+                      {health}% healthy
+                    </ProgressBar>
+                  )}
+                </LabeledList.Item>
+                {dead ? (
+                  <LabeledList.Item label="Condition">
+                    <Box color={permadead ? 'red' : 'green'} bold={1}>
+                      {permadead
+                        ? heart_broken
+                          ? 'Myocardial rupture, surgical intervention required'
+                          : 'Permanently deceased'
+                        : Synthetic
+                          ? 'Central power system shutdown, reboot with a reset key possible'
+                          : 'Cardiac arrest, defibrillation possible'}
                     </Box>
-                  </ProgressBar>
-                </Box>
-              )}
-            </LabeledList.Item>
-            <LabeledList.Item label="Holocard">
-              <NoticeBox color={holocard} inline>
-                {holocard_message}
-              </NoticeBox>
-
-              <Button
-                inline
-                style={{ marginLeft: '2%' }}
-                onClick={() => act('change_holo_card')}
-              >
-                Change
-              </Button>
-            </LabeledList.Item>
-          </LabeledList>
-        </Section>
-        {has_chemicals ? <ScannerChems /> : null}
-        <Section>
-          <LabeledList>
-            {has_blood ? (
-              <LabeledList.Item label={'Blood Type ' + blood_type}>
-                <Box
-                  color={
-                    bloodpct > 0.9 ? 'green' : bloodpct > 0.7 ? 'orange' : 'red'
-                  }
-                >
-                  {Math.round(blood_amount / 5.6)}%, {blood_amount}cl
-                </Box>
-              </LabeledList.Item>
-            ) : null}
-            <LabeledList.Item label={'Body Temperature'}>
-              {body_temperature}
-            </LabeledList.Item>
-            <LabeledList.Item label={'Pulse'}>{pulse}</LabeledList.Item>
-          </LabeledList>
-          {implants ||
-          hugged ||
-          core_fracture ||
-          (lung_ruptured && bodyscanner) ? (
-            <Divider />
-          ) : null}
-          {implants && detail_level !== 1 ? (
-            <NoticeBox danger>
-              {implants} embedded object{implants > 1 ? 's' : ''} detected!
-              {healthanalyser ? ' Advanced scanner required for location.' : ''}
-            </NoticeBox>
-          ) : null}
-          {(implants || hugged) && detail_level === 1 ? (
-            <NoticeBox danger>
-              {implants + (hugged ? 1 : 0)} unknown bod
-              {implants + (hugged ? 1 : 0) > 1 ? 'ies' : 'y'} detected!
-            </NoticeBox>
-          ) : null}
-          {lung_ruptured && bodyscanner ? (
-            <NoticeBox danger>Ruptured lung detected!</NoticeBox>
-          ) : null}
-          {core_fracture && healthanalyser ? (
-            <NoticeBox danger>
-              Bone fractures detected! Advanced scanner required for location.
-            </NoticeBox>
-          ) : null}
-        </Section>
-        {limbs_damaged ? <ScannerLimbs /> : null}
-        {damaged_organs?.length && bodyscanner ? <ScannerOrgans /> : null}
-        {diseases ? (
-          <Section title="Diseases">
-            <LabeledList>
-              {diseases.map((disease) => (
-                <LabeledList.Item
-                  key={disease.name}
-                  label={disease.name[0].toUpperCase() + disease.name.slice(1)}
-                >
-                  <Box inline bold={1}>
-                    Type : {disease.type}, possible cure : {disease.cure}
+                  </LabeledList.Item>
+                ) : null}
+                <LabeledList.Item label="Damage">
+                  <Box inline>
+                    <ProgressBar>
+                      Brute:{' '}
+                      <Box inline bold color={'red'}>
+                        {total_brute}
+                      </Box>
+                    </ProgressBar>
                   </Box>
                   <Box inline width={'5px'} />
                   <Box inline>
-                    <ProgressBar
-                      width="200px"
-                      value={disease.stage / disease.max_stage}
-                      ranges={{
-                        good: [-Infinity, 20],
-                        average: [20, 50],
-                        bad: [50, Infinity],
-                      }}
-                    >
-                      Stage:{disease.stage}/{disease.max_stage}
+                    <ProgressBar>
+                      Burn:{' '}
+                      <Box inline bold color={'#ffb833'}>
+                        {total_burn}
+                      </Box>
                     </ProgressBar>
                   </Box>
-                </LabeledList.Item>
-              ))}
-            </LabeledList>
-          </Section>
-        ) : null}
-        {advice ? (
-          <Section title="Medication Advice">
-            <Stack vertical>
-              {advice.map((advice) => (
-                <Stack.Item key={advice.advice}>
+                  <Box inline width={'5px'} />
                   <Box inline>
-                    <Icon name={advice.icon} ml={0.2} color={advice.color} />
-                    <Box inline width={'5px'} />
-                    {advice.advice}
+                    <ProgressBar>
+                      Toxin:{' '}
+                      <Box inline bold color={'green'}>
+                        {toxin}
+                      </Box>
+                    </ProgressBar>
                   </Box>
-                </Stack.Item>
-              ))}
-            </Stack>
-          </Section>
-        ) : null}
+                  <Box inline width={'5px'} />
+                  <Box inline>
+                    <ProgressBar>
+                      Oxygen:{' '}
+                      <Box inline bold color={'blue'}>
+                        {oxy}
+                      </Box>
+                    </ProgressBar>
+                  </Box>
+                  <Box inline width={'5px'} />
+                  {!!clone && (
+                    <Box inline>
+                      <ProgressBar>
+                        Clone:{' '}
+                        <Box inline color={'teal'}>
+                          {clone}
+                        </Box>
+                      </ProgressBar>
+                    </Box>
+                  )}
+                </LabeledList.Item>
+                <LabeledList.Item label="Holocard">
+                  <NoticeBox color={holocard} inline>
+                    {holocard_message}
+                  </NoticeBox>
+
+                  <Button
+                    inline
+                    style={{ marginLeft: '2%' }}
+                    onClick={() => act('change_holo_card')}
+                  >
+                    Change
+                  </Button>
+                </LabeledList.Item>
+              </LabeledList>
+            </Section>
+            {has_chemicals ? <ScannerChems /> : null}
+            <Section maxWidth={sectionWidth}>
+              <LabeledList>
+                {has_blood ? (
+                  <LabeledList.Item label={'Blood Type ' + blood_type}>
+                    <Box
+                      color={
+                        bloodpct > 0.9 ? 'green' : bloodpct > 0.7 ? 'orange' : 'red'
+                      }
+                    >
+                      {Math.round(blood_amount / 5.6)}%, {blood_amount}cl
+                    </Box>
+                  </LabeledList.Item>
+                ) : null}
+                <LabeledList.Item label={'Body Temperature'}>
+                  {body_temperature}
+                </LabeledList.Item>
+                <LabeledList.Item label={'Pulse'}>{pulse}</LabeledList.Item>
+              </LabeledList>
+              {implants ||
+              hugged ||
+              core_fracture ||
+              (lung_ruptured && bodyscanner) ? (
+                <Divider />
+              ) : null}
+              {implants && detail_level !== 1 ? (
+                <NoticeBox danger>
+                  {implants} embedded object{implants > 1 ? 's' : ''} detected!
+                  {healthanalyser ? ' Advanced scanner required for location.' : ''}
+                </NoticeBox>
+              ) : null}
+              {(implants || hugged) && detail_level === 1 ? (
+                <NoticeBox danger>
+                  {implants + (hugged ? 1 : 0)} unknown bod
+                  {implants + (hugged ? 1 : 0) > 1 ? 'ies' : 'y'} detected!
+                </NoticeBox>
+              ) : null}
+              {lung_ruptured && bodyscanner ? (
+                <NoticeBox danger>Ruptured lung detected!</NoticeBox>
+              ) : null}
+              {core_fracture && healthanalyser ? (
+                <NoticeBox danger>
+                  Bone fractures detected! Advanced scanner required for location.
+                </NoticeBox>
+              ) : null}
+            </Section>
+            {diseases ? (
+              <Section title="Diseases" maxWidth={sectionWidth}>
+                <LabeledList>
+                  {diseases.map((disease) => (
+                    <LabeledList.Item
+                      key={disease.name}
+                      label={disease.name[0].toUpperCase() + disease.name.slice(1)}
+                    >
+                      <Box inline bold={1}>
+                        Type : {disease.type}, possible cure : {disease.cure}
+                      </Box>
+                      <Box inline width={'5px'} />
+                      <Box inline>
+                        <ProgressBar
+                          width="200px"
+                          value={disease.stage / disease.max_stage}
+                          ranges={{
+                            good: [-Infinity, 20],
+                            average: [20, 50],
+                            bad: [50, Infinity],
+                          }}
+                        >
+                          Stage:{disease.stage}/{disease.max_stage}
+                        </ProgressBar>
+                      </Box>
+                    </LabeledList.Item>
+                  ))}
+                </LabeledList>
+              </Section>
+            ) : null}
+            {advice ? (
+              <Section title="Medication Advice" maxWidth={sectionWidth}>
+                <Stack vertical>
+                  {advice.map((advice) => (
+                    <Stack.Item key={advice.advice}>
+                      <Box inline>
+                        <Icon name={advice.icon} ml={0.2} color={advice.color} />
+                        <Box inline width={'5px'} />
+                        {advice.advice}
+                      </Box>
+                    </Stack.Item>
+                  ))}
+                </Stack>
+              </Section>
+            ) : null}
+
+          </Stack.Item>
+          <Stack.Item>
+            {limbs_damaged ? <ScannerLimbs /> : null}
+            {damaged_organs?.length && bodyscanner ? <ScannerOrgans /> : null}
+          </Stack.Item>
+        </Stack>
       </Window.Content>
     </Window>
   );
@@ -340,7 +349,7 @@ const ScannerLimbs = (props) => {
   });
 
   return (
-    <Section title="Limbs Damaged">
+    <Section title="Limbs Damaged" maxWidth={sectionWidth}>
       <Stack vertical fill>
         <Flex width="100%" height="20px">
           <Flex.Item basis="85px" />
@@ -461,7 +470,7 @@ const ScannerOrgans = (props) => {
   const { damaged_organs } = data;
 
   return (
-    <Section title="Organ(s) Damaged">
+    <Section title="Organ(s) Damaged" maxWidth={sectionWidth}>
       <LabeledList>
         {damaged_organs.map((organ) => (
           <LabeledList.Item

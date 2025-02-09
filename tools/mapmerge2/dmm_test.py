072b8080ba123c5f7dcef3b200b6e3715827c3d2
Drulikar <drathek@gmail.com>
2025-02-08 23:24:50 -0600

    fix head

--- tools/mapmerge2/dmm_test.py
+++ tools/mapmerge2/dmm_test.py
@@ -24,15 +24,16 @@ def _self_test():
 
     # Read the HEAD and ancestor commits
     # Assumption: origin on the runner is what we'd normally call upstream
-    ancestor = None
+    head = None
     ancestor_commit = None
     try:
         # if HEAD is a merge commit:
-        ancestor = repo.merge_base(repo.revparse_single("HEAD^2").id, repo.revparse_single("refs/remotes/origin/master").id)
+        head = repo.revparse_single("HEAD^2").id
     except KeyError:
         # if HEAD isn't a merge commit:
-        ancestor = repo.merge_base(repo.head.target, repo.revparse_single("refs/remotes/origin/master").id)
+        head = repo.head.target
         pass
+    ancestor = repo.merge_base(head, repo.revparse_single("refs/remotes/origin/master").id)
     if not ancestor:
         print("Unable to determine merge base!")
     else:

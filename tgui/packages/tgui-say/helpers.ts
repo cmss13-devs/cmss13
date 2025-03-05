b1674f5ac86857bf1d6831ca396aec3a366993c5
Kashargul <144968721+Kashargul@users.noreply.github.com>
2025-03-05 23:44:39 +0100

    tgui say update

--- tgui/packages/tgui-say/helpers.ts
+++ tgui/packages/tgui-say/helpers.ts
@@ -1,32 +1,32 @@
 import { Channel } from './ChannelIterator';
-import { WINDOW_SIZES } from './constants';
+import { RADIO_PREFIXES, WindowSize } from './constants';
 
 /**
  * Once byond signals this via keystroke, it
  * ensures window size, visibility, and focus.
  */
-export const windowOpen = (channel: Channel) => {
+export function windowOpen(channel: Channel): void {
   setWindowVisibility(true);
   Byond.sendMessage('open', { channel });
-};
+}
 
 /**
  * Resets the state of the window and hides it from user view.
  * Sending "close" logs it server side.
  */
-export const windowClose = () => {
+export function windowClose(): void {
   setWindowVisibility(false);
   Byond.winset('map', {
     focus: true,
   });
   Byond.sendMessage('close');
-};
+}
 
 /**
  * Modifies the window size.
  */
-export const windowSet = (size = WINDOW_SIZES.small) => {
-  let sizeStr = `${WINDOW_SIZES.width}x${size}`;
+export function windowSet(size = WindowSize.Small): void {
+  let sizeStr = `${WindowSize.Width}x${size}`;
 
   Byond.winset('tgui_say.browser', {
     size: sizeStr,
@@ -35,12 +35,34 @@ export const windowSet = (size = WINDOW_SIZES.small) => {
   Byond.winset('tgui_say', {
     size: sizeStr,
   });
-};
+}
 
 /** Helper function to set window size and visibility */
-const setWindowVisibility = (visible: boolean) => {
+function setWindowVisibility(visible: boolean): void {
   Byond.winset('tgui_say', {
     'is-visible': visible,
-    size: `${WINDOW_SIZES.width}x${WINDOW_SIZES.small}`,
+    size: `${WindowSize.Width}x${WindowSize.Small}`,
   });
-};
+}
+
+const CHANNEL_REGEX = /^[:.#]\w\s/;
+
+/** Tests for a channel prefix, returning it or none */
+export function getPrefix(
+  value: string,
+): keyof typeof RADIO_PREFIXES | undefined {
+  if (!value || value.length < 3 || !CHANNEL_REGEX.test(value)) {
+    return;
+  }
+
+  let adjusted = value
+    .slice(0, 3)
+    ?.toLowerCase()
+    ?.replace('.', ':') as keyof typeof RADIO_PREFIXES;
+
+  if (!RADIO_PREFIXES[adjusted]) {
+    return;
+  }
+
+  return adjusted;
+}

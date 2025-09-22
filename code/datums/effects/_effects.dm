cmss13-devs/cmss13

commit c40a8778028d3c24a1963a34b0a755e80a18aca4

[Re] Gardener Drone: Changes and Improvements. (#9572)
Thanks to **Archivator** for comming up with marine-sided fruit
description.
Thanks to **Adan Stafford | JAW5** for creating better description for
spore fruit that distinguished them from other fruits.

Re-open PR: #9327
_(closed by stale bot)_

# About the pull request

**Improves General Gardener gameplay and understanding by:**

- Gives more information when examinating fruits/hardened weeds, like
how much they heal, what they do, how long they last etc. Marines cannot
see those infomations anymore, they now get basic descriptions for
fruits.
- Gardener now pickup fruits 50% faster than any other caste, this only
apply to unrooting and feeding fruit, it does not apply to consuming
fruit from ground. (it makes sense, because they specialize in gardening
so they do it quicker, like how medics heal themself faster than others)
- Returns gardener fruit counter as status tab info, change made from
#8647 broke original counter on button.
- Resprites item fruits to have twitchy roots, so its more easy to
recognize when they are uprooted from not mature ones.
- If marine uproots fruit (or find one) they now get slight "hint" for
them to maybe bring this to research.
- Ability to place fruits next to resin holes, don't worry, you
**cannot** place them on top to hide them.
- Gives ability to instantly mature any fruit with resin surge, in
exchange it will apply dynamic cooldown based on fruit growth using
previous code maturity + cooldown time.
- Gives visual balloon alert when consuming fruit or when fruit expires,
so player knows when effect of fruit ends.
- Gives audiable clue when fruits expires, so player know by intuition
that text that popped up is expiration one. (if they read too slow)

Also, don't say giving xenos detailed information is broken, marines
have ability to open weapon status tab, and see how much damage it does,
how far it shoot, armor penetration etc. Its more QoL change than
anything else.

code/datums/effects/heal_over_time.dm
@@ -32,3 +32,11 @@
 	affected_mob.updatehealth()
 
 	return TRUE
+
+/datum/effects/heal_over_time/Destroy()
+	if(affected_atom)
+		var/mob/living/carbon/xenomorph/xeno = affected_atom
+		if(istype(xeno))
+			xeno.balloon_alert(xeno, "our regeneration speed returns to normal.", text_color = "#17991b80")
+			playsound(xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
+	return ..()

code/datums/effects/plasma_over_time.dm
@@ -31,3 +31,11 @@
 	affected_mob.gain_plasma(plasma_each_process)
 
 	return TRUE
+
+/datum/effects/plasma_over_time/Destroy()
+	if(affected_atom)
+		var/mob/living/carbon/xenomorph/xeno = affected_atom
+		if(istype(xeno))
+			xeno.balloon_alert(xeno, "our plasma rush subsides.", text_color = "#1e6072")
+			playsound(xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
+	return ..()

code/datums/effects/xeno_strains/gain_xeno_cooldown_reduction_on_slash.dm
@@ -26,19 +26,21 @@
 
 /datum/effects/gain_xeno_cooldown_reduction_on_slash/Destroy()
 	if(affected_atom)
-		var/mob/living/carbon/xenomorph/X  = affected_atom
-		X.cooldown_reduction_percentage -= current_reduction
-		to_chat(X, SPAN_XENOWARNING("You feel your frenzy wanes! Your cooldowns are back to normal."))
-		if(X.cooldown_reduction_percentage < 0)
-			X.cooldown_reduction_percentage = 0
+		var/mob/living/carbon/xenomorph/xeno  = affected_atom
+		xeno.cooldown_reduction_percentage -= current_reduction
+		to_chat(xeno, SPAN_XENOWARNING("We feel our frenzy wane! Our cooldowns are back to normal."))
+		xeno.balloon_alert(xeno, "we feel our frenzy wane!", text_color = "#99461780")
+		playsound(xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
+		if(xeno.cooldown_reduction_percentage < 0)
+			xeno.cooldown_reduction_percentage = 0
 
 	return ..()
 
 /datum/effects/gain_xeno_cooldown_reduction_on_slash/proc/increase_cooldown_reduction()
 	SIGNAL_HANDLER
 	if(affected_atom && current_reduction < max_reduction_amount)
-		var/mob/living/carbon/xenomorph/X  = affected_atom
+		var/mob/living/carbon/xenomorph/xeno  = affected_atom
 		var/previous_reduction = current_reduction
 		current_reduction = min(current_reduction + reduction_amount_per_slash, max_reduction_amount)
 		var/delta = current_reduction - previous_reduction
-		X.cooldown_reduction_percentage += delta
+		xeno.cooldown_reduction_percentage += delta

code/datums/effects/xeno_strains/xeno_speed.dm
@@ -44,4 +44,6 @@
 			LAZYREMOVE(xeno.modifier_sources, effect_modifier_source)
 		if(effect_end_message)
 			to_chat(xeno, effect_end_message)
+		xeno.balloon_alert(xeno, "our speed fall back to normal.", text_color = "#5B248C")
+		playsound(xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
 	return ..()

code/datums/xeno_shields/xeno_shield.dm
@@ -44,9 +44,15 @@
 
 // Anything special to do on removal
 /datum/xeno_shield/proc/on_removal()
+	if(linked_xeno && istype(linked_xeno, /mob/living/carbon/xenomorph) && shield_source == XENO_SHIELD_SOURCE_GARDENER)
+		linked_xeno.balloon_alert(linked_xeno, "our carapace shell crumbles!", text_color = "#17997280")
+		playsound(linked_xeno, "shield_shatter", 25, 1)
 	return
 
 /datum/xeno_shield/proc/begin_decay()
+	if(linked_xeno && istype(linked_xeno, /mob/living/carbon/xenomorph) && shield_source == XENO_SHIELD_SOURCE_GARDENER)
+		linked_xeno.balloon_alert(linked_xeno, "our carapace shell begins to decay!", text_color = "#17997280")
+		playsound(linked_xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
 	START_PROCESSING(SSobj, src)
 	processing = TRUE
 

code/modules/mob/living/carbon/xenomorph/strains/castes/drone/gardener.dm
@@ -1,6 +1,6 @@
 /datum/xeno_strain/gardener
 	name = DRONE_GARDENER
-	description = "You trade your choice of resin secretions, your corrosive acid, and your ability to transfer plasma for a tiny bit of extra health regeneration on weeds and several new abilities, including the ability to plant hardier weeds, temporarily reinforce structures with your plasma, and to plant up to six potent resin fruits for your sisters by secreting your vital fluids at the cost of a bit of your health for each fruit you shape. You can use Resin Surge to speed up the growth of your fruits."
+	description = "You trade your choice of resin secretions, your corrosive acid, and your ability to transfer plasma for a tiny bit of extra health regeneration on weeds and several new abilities, including the ability to plant hardier weeds, temporarily reinforce structures with your plasma, and to plant up to six potent resin fruits for your sisters by secreting your vital fluids at the cost of a bit of your health for each fruit you shape. Resin Surge can force your fruits to mature rapidly. As shaper of fruits, you can harvest and feed any fruit 50% faster."
 	flavor_description = "The glory of gardening: hands in the weeds, head in the dark, heart with resin."
 
 	actions_to_remove = list(
@@ -84,15 +84,15 @@
 		to_chat(xeno, SPAN_WARNING("These weeds do not belong to our hive; they reject our fruit."))
 		return
 
-	if(locate(/obj/effect/alien/resin/trap) in range(1, target_turf))
-		to_chat(xeno, SPAN_XENOWARNING("This location is too close to a resin hole!"))
+	if(locate(/obj/effect/alien/resin/trap) in range(0, target_turf))
+		to_chat(xeno, SPAN_XENOWARNING("We cannot plant our fruit over a resin hole!"))
 		return
 
 	if(locate(/obj/effect/alien/resin/fruit) in target_turf)
-		to_chat(xeno, SPAN_XENOWARNING("This location is too close to another fruit!"))
+		to_chat(xeno, SPAN_XENOWARNING("Our fruit has already taken root in this space!"))
 		return
 
-	if (check_and_use_plasma_owner())
+	if(check_and_use_plasma_owner())
 		if(length(xeno.current_fruits) >= xeno.max_placeable)
 			to_chat(xeno, SPAN_XENOWARNING("We cannot sustain another fruit, one will wither away to allow this one to live!"))
 			var/obj/effect/alien/resin/fruit/old_fruit = xeno.current_fruits[1]
@@ -110,10 +110,6 @@
 		xeno.updatehealth()
 		playsound(xeno.loc, "alien_resin_build", 25)
 		xeno.current_fruits.Add(fruit)
-
-		var/number_of_fruit = length(xeno.current_fruits)
-		button.set_maptext(SMALL_FONTS_COLOR(7, number_of_fruit, "#e69d00"), 19, 2)
-		update_button_icon()
 		xeno.update_icons()
 
 	apply_cooldown()
@@ -232,13 +228,13 @@
 
 /datum/action/xeno_action/activable/resin_surge/use_ability(atom/target_atom, mods)
 	var/mob/living/carbon/xenomorph/xeno = owner
-	if (!istype(xeno))
+	if(!istype(xeno))
 		return
 
-	if (!action_cooldown_check())
+	if(!action_cooldown_check())
 		return
 
-	if (!xeno.check_state(TRUE))
+	if(!xeno.check_state(TRUE))
 		return
 
 	if(mods[CLICK_CATCHER])
@@ -253,7 +249,7 @@
 			to_chat(xeno, SPAN_WARNING("That's too far away!"))
 			return
 
-	if (!check_and_use_plasma_owner())
+	if(!check_and_use_plasma_owner())
 		return
 
 	var/turf/target_turf = get_turf(target_atom)
@@ -269,7 +265,7 @@
 	if(door_present || wall_present)
 		var/structure_to_buff = door || wall
 		var/buff_already_present = FALSE
-		if(door_present )
+		if(door_present)
 			for(var/datum/effects/xeno_structure_reinforcement/sf in door.effects_list)
 				buff_already_present = TRUE
 				break
@@ -284,15 +280,16 @@
 			SPAN_XENONOTICE("We surge the resin around [structure_to_buff], making it temporarily nigh unbreakable!"), null, 5)
 		else
 			to_chat(xeno, SPAN_XENONOTICE("We haplessly try to surge resin around [structure_to_buff], but it's already reinforced. It'll take a moment for us to recover."))
-			xeno_cooldown = xeno_cooldown * 0.5
+			xeno_cooldown *= 0.5
 
 	else if(F && F.hivenumber == xeno.hivenumber)
 		if(F.mature)
 			to_chat(xeno, SPAN_XENONOTICE("The [F] is already mature. The [src.name] does nothing."))
-			xeno_cooldown = xeno_cooldown * 0.5
+			xeno_cooldown *= 0.5
 		else
-			to_chat(xeno, SPAN_XENONOTICE("We surge the resin around the [F], speeding its growth somewhat!"))
-			F.reduce_timer(5 SECONDS)
+			to_chat(xeno, SPAN_XENONOTICE("We pour all our energy equal to [F] growth, bringing it to swift maturity!"))
+			F.reduce_timer(60 SECONDS) //We want surge to mature any fruit instantly, but you receive dynamic cooldown depending on fruit growth time.
+			xeno_cooldown *= dynamic_fruit_surge_cooldown(F)
 
 	else if(target_weeds && istype(target_turf, /turf/open) && target_weeds.hivenumber == xeno.hivenumber)
 		xeno.visible_message(SPAN_XENODANGER("\The [xeno] surges the resin, creating an unstable wall!"),
@@ -312,19 +309,29 @@
 		channel_in_progress = FALSE
 		xeno.visible_message(SPAN_XENODANGER("\The [xeno] surges deep resin, creating an unstable sticky resin patch!"),
 		SPAN_XENONOTICE("We surge the deep resin, creating an unstable sticky resin patch!"), null, 5)
-		for (var/turf/targetTurf in orange(1, target_turf))
+		for(var/turf/targetTurf in orange(1, target_turf))
 			if(!locate(/obj/effect/alien/resin/sticky) in targetTurf)
 				new /obj/effect/alien/resin/sticky/thin/weak(targetTurf, xeno.hivenumber)
 		if(!locate(/obj/effect/alien/resin/sticky) in target_turf)
 			new /obj/effect/alien/resin/sticky/thin/weak(target_turf, xeno.hivenumber)
 
 	else
-		xeno_cooldown = xeno_cooldown * 0.5
+		xeno_cooldown *= 0.5
 
 	apply_cooldown()
 
 	xeno_cooldown = initial(xeno_cooldown)
 	return ..()
+
+/datum/action/xeno_action/activable/resin_surge/proc/dynamic_fruit_surge_cooldown(obj/effect/alien/resin/fruit/F)
+	var/time_to_mature = F.time_to_mature
+
+	var/calculate_equasion = time_to_mature / (10 SECONDS + 5 SECONDS) //xeno_cooldown + old_reduce_timer
+	var/calculate_multiplier = calculate_equasion * 5 SECONDS // * old_reduce_timer
+	var/calculate_pre_final = time_to_mature - calculate_multiplier
+	var/calculate_final_cooldown = (calculate_pre_final / 10) * 0.1 //why divided by 10? becouse it don't support SECONDS, it see them as DECISECONDS.
+
+	return calculate_final_cooldown
 
 /datum/action/xeno_action/verb/verb_resin_surge()
 	set category = "Alien"
@@ -347,6 +354,11 @@
 	spread_on_semiweedable = TRUE
 	fruit_growth_multiplier = 0.8
 	weed_strength = WEED_LEVEL_HARDY
+
+/obj/effect/alien/weeds/node/gardener/get_examine_text(mob/user)
+	. = ..()
+	if(isxeno(user) || isobserver(user))
+		. += SPAN_NOTICE("We sense that this weeds will benefit our resin fruits, increasing growth speed by [SPAN_BOLDNOTICE("20%")].")
 
 /datum/action/xeno_action/verb/verb_plant_gardening_weeds()
 	set category = "Alien"
@@ -381,6 +393,11 @@
 
 	fruit_sac_overlay_icon.color = fruit_sac_color
 	bound_xeno.overlays += fruit_sac_overlay_icon
+
+/datum/behavior_delegate/drone_gardener/append_to_stat()
+	. = list()
+	. += "Fruits sustained: [length(bound_xeno.current_fruits)] / [bound_xeno.max_placeable]"
+
 /*
 Swapping to greater fruit changes the color to #17991B
 Swapping to spore fruit changes the color to #994617

code/modules/mob/living/carbon/xenomorph/strains/castes/drone/healer.dm
@@ -29,8 +29,8 @@
 	drone.tackle_chance_modifier -= 5
 
 	drone.max_placeable = 3
-	drone.available_fruits = list(/obj/effect/alien/resin/fruit)
-	drone.selected_fruit = /obj/effect/alien/resin/fruit
+	drone.available_fruits = list(/obj/effect/alien/resin/fruit/lesser)
+	drone.selected_fruit = /obj/effect/alien/resin/fruit/lesser
 
 	drone.recalculate_everything()
 
@@ -186,6 +186,7 @@
 
 /datum/behavior_delegate/drone_healer/append_to_stat()
 	. = list()
+	. += "Fruits sustained: [length(bound_xeno.current_fruits)] / [bound_xeno.max_placeable]"
 	. += "Transferred health amount: [transferred_amount]/[required_transferred_amount]"
 	if(transferred_amount >= required_transferred_amount)
 		. += "Sacrifice will grant you new life."

commit 6978f18b76fdb590497a6988017d97c6d2743963

Prevent Fruits balloon alert spam for every not related Destroy() proc call. (#9877)
# About the pull request

This is a fix for issue caused by #9572, i did make tests for shield
fruit, becouse there many xenos using shield abilities, but i forgot
about xenos that do healing abilities (i forgor)

also i forgot to credit fixing issue on #9572:

Fixes: #8660

# Explain why it's good for the game

Less balloon alert spam caused by healing, like healing drones, queen
heal, warrior slash healing, some bullets effects triggering balloons,
it should be triggered only by fruits.

# Changelog
:cl: Venuska1117
fix: Fruit balloon alert now only trigger after consuming fruit, instead
for every not related Destroy() proc call.
/:cl:

<!-- Both :cl:'s are required for the changelog to work! -->

---------

Co-authored-by: Venuska1117 <venus117@o2.pl>

code/datums/effects/_effects.dm
@@ -36,6 +36,7 @@
 	var/obj_icon_state_path = null //The icon_state path for objs
 	var/mob_icon_state_path = null //The icon_state path for mobs
 	var/datum/cause_data/cause_data = null //Cause data for statistics
+	var/show_baloon_alert = FALSE //Used to limit balloon alerts
 
 /datum/effects/New(atom/thing, mob/from = null, last_dmg_source = null, zone = "chest")
 	if(!validate_atom(thing) || QDELETED(thing))

code/datums/effects/heal_over_time.dm
@@ -6,13 +6,14 @@
 	var/ticks_between_heals = 1
 	var/heal_each_process = 0
 
-/datum/effects/heal_over_time/New(atom/A, heal_amount = 0, healing_time = 5, time_between_heals = 1, limb_name = null)
+/datum/effects/heal_over_time/New(atom/A, heal_amount = 0, healing_time = 5, time_between_heals = 1, limb_name = null, show_baloon_alert = FALSE)
 	..(A, null, null, limb_name)
 
 	duration = healing_time
 	total_heal_amount = heal_amount
 	ticks_between_heals = time_between_heals
 	heal_each_process = (heal_amount / healing_time) * time_between_heals
+	src.show_baloon_alert = show_baloon_alert
 
 /datum/effects/heal_over_time/validate_atom(atom/A)
 	if(isobj(A))
@@ -36,7 +37,7 @@
 /datum/effects/heal_over_time/Destroy()
 	if(affected_atom)
 		var/mob/living/carbon/xenomorph/xeno = affected_atom
-		if(istype(xeno))
+		if(istype(xeno) && show_baloon_alert)
 			xeno.balloon_alert(xeno, "our regeneration speed returns to normal.", text_color = "#17991b80")
 			playsound(xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
 	return ..()

code/datums/effects/plasma_over_time.dm
@@ -6,13 +6,14 @@
 	var/ticks_between_plasmas = 1
 	var/plasma_each_process = 0
 
-/datum/effects/plasma_over_time/New(atom/A, plasma_amount = 0, plasma_time = 5, time_between_plasmas = 1)
+/datum/effects/plasma_over_time/New(atom/A, plasma_amount = 0, plasma_time = 5, time_between_plasmas = 1, show_baloon_alert = FALSE)
 	..(A, null, null)
 
 	duration = plasma_time
 	total_plasma_amount = plasma_amount
 	ticks_between_plasmas = time_between_plasmas
 	plasma_each_process = (plasma_amount / plasma_time) * time_between_plasmas
+	src.show_baloon_alert = show_baloon_alert
 
 /datum/effects/plasma_over_time/validate_atom(atom/A)
 	if(!isxeno(A))
@@ -35,7 +36,7 @@
 /datum/effects/plasma_over_time/Destroy()
 	if(affected_atom)
 		var/mob/living/carbon/xenomorph/xeno = affected_atom
-		if(istype(xeno))
+		if(istype(xeno) && show_baloon_alert)
 			xeno.balloon_alert(xeno, "our plasma rush subsides.", text_color = "#1e6072")
 			playsound(xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
 	return ..()

code/datums/effects/xeno_strains/gain_xeno_cooldown_reduction_on_slash.dm
@@ -7,7 +7,7 @@
 	var/reduction_amount_per_slash = 0
 	var/current_reduction = 0
 
-/datum/effects/gain_xeno_cooldown_reduction_on_slash/New(atom/A, mob/from = null, max_reduction_amount = 0, reduction_per_slash = 0, duration = 0, effect_source = null)
+/datum/effects/gain_xeno_cooldown_reduction_on_slash/New(atom/A, mob/from = null, max_reduction_amount = 0, reduction_per_slash = 0, duration = 0, effect_source = null, show_baloon_alert = FALSE)
 	. = ..(A, from, null, null)
 
 	src.effect_source = effect_source
@@ -18,6 +18,7 @@
 	COMSIG_HUMAN_ALIEN_ATTACK
 	), PROC_REF(increase_cooldown_reduction))
 	QDEL_IN(src, duration)
+	src.show_baloon_alert = show_baloon_alert
 
 /datum/effects/gain_xeno_cooldown_reduction_on_slash/validate_atom(atom/A)
 	if(isxeno(A))
@@ -28,9 +29,10 @@
 	if(affected_atom)
 		var/mob/living/carbon/xenomorph/xeno  = affected_atom
 		xeno.cooldown_reduction_percentage -= current_reduction
-		to_chat(xeno, SPAN_XENOWARNING("We feel our frenzy wane! Our cooldowns are back to normal."))
-		xeno.balloon_alert(xeno, "we feel our frenzy wane!", text_color = "#99461780")
-		playsound(xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
+		if(istype(xeno) && show_baloon_alert)
+			to_chat(xeno, SPAN_XENOWARNING("We feel our frenzy wane! Our cooldowns are back to normal."))
+			xeno.balloon_alert(xeno, "we feel our frenzy wane!", text_color = "#99461780")
+			playsound(xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
 		if(xeno.cooldown_reduction_percentage < 0)
 			xeno.cooldown_reduction_percentage = 0
 

code/datums/effects/xeno_strains/xeno_speed.dm
@@ -6,7 +6,7 @@
 	var/effect_modifier_source = null
 	var/effect_end_message = null
 
-/datum/effects/xeno_speed/New(atom/A, mob/from = null, last_dmg_source = null, zone = "chest", ttl = 3.5 SECONDS, set_speed_modifier = 0, set_modifier_source = null, set_end_message = SPAN_XENONOTICE("You feel lethargic..."))
+/datum/effects/xeno_speed/New(atom/A, mob/from = null, last_dmg_source = null, zone = "chest", ttl = 3.5 SECONDS, set_speed_modifier = 0, set_modifier_source = null, set_end_message = SPAN_XENONOTICE("You feel lethargic..."), show_baloon_alert = FALSE)
 	. = ..(A, from, last_dmg_source, zone)
 	if(QDELETED(src))
 		return
@@ -20,6 +20,7 @@
 	effect_modifier_source = set_modifier_source
 	effect_end_message = set_end_message
 	added_effect = TRUE
+	src.show_baloon_alert = show_baloon_alert
 
 /datum/effects/xeno_speed/validate_atom(atom/A)
 	if(!isxeno(A))
@@ -44,6 +45,7 @@
 			LAZYREMOVE(xeno.modifier_sources, effect_modifier_source)
 		if(effect_end_message)
 			to_chat(xeno, effect_end_message)
-		xeno.balloon_alert(xeno, "our speed fall back to normal.", text_color = "#5B248C")
-		playsound(xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
+		if(istype(xeno) && show_baloon_alert)
+			xeno.balloon_alert(xeno, "our speed fall back to normal.", text_color = "#5B248C")
+			playsound(xeno, 'sound/effects/squish_and_exhaust.ogg', 25, 1)
 	return ..()

code/modules/cm_aliens/structures/fruit.dm
@@ -219,7 +219,7 @@
 	if(recipient && !QDELETED(recipient))
 		recipient.gain_health(heal_amount)
 		//Every second, heal them for 20.
-		new /datum/effects/heal_over_time(recipient, regeneration_amount_total, regeneration_ticks, 1)
+		new /datum/effects/heal_over_time(recipient, regeneration_amount_total, regeneration_ticks, 1, show_baloon_alert = TRUE)
 		to_chat(recipient, SPAN_XENOBOLDNOTICE("We recover a bit from our injuries, and begin to regenerate rapidly."))
 		recipient.balloon_alert(recipient, "our flesh mends, and the regeneration quickens!", text_color = "#17991B")
 	if(do_consume)
@@ -255,7 +255,7 @@
 	if(mature && recipient && !QDELETED(recipient))
 		recipient.add_xeno_shield(clamp(overshield_amount, 0, recipient.maxHealth * 0.3), XENO_SHIELD_SOURCE_GARDENER, duration = shield_duration, decay_amount_per_second = shield_decay)
 		//Every second, heal them for 5.
-		new /datum/effects/heal_over_time(recipient, regeneration_amount_total, regeneration_ticks, 1)
+		new /datum/effects/heal_over_time(recipient, regeneration_amount_total, regeneration_ticks, 1, show_baloon_alert = TRUE)
 		to_chat(recipient, SPAN_XENOBOLDNOTICE("We feel our defense being bolstered, and begin to slowly regenerate."))
 		recipient.balloon_alert(recipient, "our regeneration quickens and carapace thickens!", text_color = "#179973")
 	if(do_consume)
@@ -291,7 +291,7 @@
 		for(var/datum/effects/gain_xeno_cooldown_reduction_on_slash/E in recipient.effects_list)
 			if(E.effect_source == "spore")
 				qdel(E)
-		new /datum/effects/gain_xeno_cooldown_reduction_on_slash(recipient, bound_xeno, max_cooldown_reduction, cooldown_per_slash, 90 SECONDS, "spore")
+		new /datum/effects/gain_xeno_cooldown_reduction_on_slash(recipient, bound_xeno, max_cooldown_reduction, cooldown_per_slash, 90 SECONDS, "spore", show_baloon_alert = TRUE)
 		to_chat(recipient, SPAN_XENOBOLDNOTICE("We feel a frenzy coming onto us! Our abilities will cool off faster as we slash!"))
 		recipient.balloon_alert(recipient, "we feel a frenzy coming onto us!", text_color = "#994617", delay = 1 SECONDS)
 	if(do_consume)
@@ -342,7 +342,7 @@
 
 /obj/effect/alien/resin/fruit/speed/consume_effect(mob/living/carbon/xenomorph/recipient, do_consume = TRUE)
 	if(mature && recipient && !QDELETED(recipient))
-		new /datum/effects/xeno_speed(recipient, ttl = speed_duration, set_speed_modifier = speed_buff_amount, set_modifier_source = XENO_FRUIT_SPEED, set_end_message = SPAN_XENOWARNING("We feel the effects of the [name] wane..."))
+		new /datum/effects/xeno_speed(recipient, ttl = speed_duration, set_speed_modifier = speed_buff_amount, set_modifier_source = XENO_FRUIT_SPEED, set_end_message = SPAN_XENOWARNING("We feel the effects of the [name] wane..."), show_baloon_alert = TRUE)
 		to_chat(recipient, SPAN_XENOBOLDNOTICE("The [name] invigorates us to move faster!"))
 		recipient.balloon_alert(recipient, "we feel invigorated to run faster!", text_color = "#5B248C", delay = 1 SECONDS)
 	if(do_consume)
@@ -373,7 +373,7 @@
 /obj/effect/alien/resin/fruit/plasma/consume_effect(mob/living/carbon/xenomorph/recipient, do_consume = TRUE)
 	if(mature && recipient && recipient.plasma_max > 0 && !QDELETED(recipient))
 		//With the current values (240, 15, 3), this will give the recipient 48 plasma every 3 seconds, for a total of 240 in 15 seconds.
-		new /datum/effects/plasma_over_time(recipient, plasma_amount, plasma_time, time_between_plasmas)
+		new /datum/effects/plasma_over_time(recipient, plasma_amount, plasma_time, time_between_plasmas, show_baloon_alert = TRUE)
 		to_chat(recipient, SPAN_XENOBOLDNOTICE("The [name] boosts our plasma regeneration!"))
 		recipient.balloon_alert(recipient, "we feel our plasma rapidly regenerate!", text_color = "#287A90")
 	if(do_consume)
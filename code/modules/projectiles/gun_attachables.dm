cmss13-devs/cmss13

commit 30c8cd4146fa9e18451e7ea570f8608c0010d1a0

HPR and RPG resprite (#10425)
# About the pull request

Fully resprites M41AE2 and M5 RPG (classic camo, but they camo in all
camo variants)

<img width="371" height="454" alt="dreamseeker_QRvl7X51XF"
src="https://github.com/user-attachments/assets/dcabf43c-f09f-40af-97ff-fe411ea3aba6"
/>

<img width="286" height="366" alt="dreamseeker_rfRARVzlBp"
src="https://github.com/user-attachments/assets/58cfab66-a24a-4ddd-82ee-c2d4c763b4b6"
/>

<img width="369" height="339" alt="dreamseeker_7LgrLN0CDK"
src="https://github.com/user-attachments/assets/1a747b83-1a7a-4dba-90aa-68d0bf9d7fec"
/>


Reminder how current sprites look compared to our modern gun sprites

<details>

<img width="630" height="516" alt="image"
src="https://github.com/user-attachments/assets/62dbf337-0c7c-4194-97a9-ada5ce28caab"
/>

<img width="590" height="498" alt="dreamseeker_QcwpytEZN3"
src="https://github.com/user-attachments/assets/80ca6087-728a-4379-9a9a-42ea026d5f0b"
/>

</details>

All the camo sets

<details>

<img width="1164" height="193" alt="dreammaker_Mdf6C8B92r"
src="https://github.com/user-attachments/assets/ec62772b-bb58-40b6-a193-2970609d6e22"
/>

</details>

HPR box magazines have been made camo comforting and all RPG rockets
were resprited

<details>

<img width="259" height="145" alt="dreamseeker_chxtNVYUBZ"
src="https://github.com/user-attachments/assets/ae1fd441-029f-4a94-8e31-e1a15872a2c2"
/>

</details>

HPR also got a functional folding stock, which gives some mild
accuraccy/scatter buffs and mobility/aiming speed debuffs (akin to other
folding stocks), also HPR got a unique set of bipods which it now comes
with by default (they can be removed and they are mechanically no
different from other bipods)

<details>

<img width="428" height="221" alt="dreamseeker_xtoxzRm7Eu"
src="https://github.com/user-attachments/assets/34bd829b-ca29-4e0d-ae7b-3c835b4856c0"
/>

<img width="120" height="74" alt="dreamseeker_mLCSwEDrgy"

code/modules/projectiles/gun_attachables.dm
@@ -2188,6 +2188,57 @@ Defined in conflicts.dm of the #defines folder.
 	gun.recalculate_attachment_bonuses()
 	gun.update_overlays(src, "stock")
 
+/obj/item/attachable/stock/rifle/collapsible/m41ae2
+	name = "\improper M41AE2 folding stock"
+	desc = "A standard M41AE2 integrated folding stock."
+	slot = "stock"
+	melee_mod = 5
+	size_mod = 1
+	icon_state = "m41ae2_folding"
+	attach_icon = "m41ae2_folding_a"
+	pixel_shift_x = 29
+	hud_offset_mod = -2
+	collapsible = TRUE
+	stock_activated = FALSE
+	wield_delay_mod = WIELD_DELAY_NONE //starts collapsed so no delay mod
+	collapse_delay = 0.5 SECONDS
+	flags_attach_features = ATTACH_REMOVABLE|ATTACH_ACTIVATION
+	attachment_action_type = /datum/action/item_action/toggle
+
+/obj/item/attachable/stock/rifle/collapsible/m41ae2/New()
+	..()
+
+	//rifle stock starts collapsed so we zero out everything
+	accuracy_mod = 0
+	recoil_mod = 0
+	scatter_mod = 0
+	aim_speed_mod = 0
+	wield_delay_mod = WIELD_DELAY_NONE
+
+/obj/item/attachable/stock/rifle/collapsible/m41ae2/apply_on_weapon(obj/item/weapon/gun/gun)
+	if(stock_activated)
+		accuracy_mod = HIT_ACCURACY_MULT_TIER_1
+		recoil_mod = -RECOIL_AMOUNT_TIER_5
+		scatter_mod = -SCATTER_AMOUNT_TIER_10
+		//it makes stuff worse when one handed
+		aim_speed_mod = CONFIG_GET(number/slowdown_med)
+		hud_offset_mod = -1
+		icon_state = "m41ae2_folding_on"
+		attach_icon = "m41ae2_folding_a_on"
+		wield_delay_mod = WIELD_DELAY_SLOW
+	else
+		accuracy_mod = 0
+		recoil_mod = 0
+		scatter_mod = 0
+		aim_speed_mod = 0
+		hud_offset_mod = -3
+		icon_state = "m41ae2_folding"
+		attach_icon = "m41ae2_folding_a"
+		wield_delay_mod = WIELD_DELAY_NONE //stock is folded so no wield delay
+
+	gun.recalculate_attachment_bonuses()
+	gun.update_overlays(src, "stock")
+
 /obj/item/attachable/stock/xm177
 	name = "\improper collapsible M16 stock"
 	desc = "Very illegal in the state of California."
@@ -3460,6 +3511,8 @@ Defined in conflicts.dm of the #defines folder.
 	var/full_auto_switch = FALSE
 	// Store our old firemode so we can switch to it when undeploying the bipod
 	var/old_firemode = null
+	// if this bipod has a camo skin
+	var/camo_bipod = FALSE
 
 /obj/item/attachable/bipod/New()
 	..()
@@ -3506,6 +3559,8 @@ Defined in conflicts.dm of the #defines folder.
 	else
 		icon_state = initial(icon_state)
 		attach_icon = initial(attach_icon)
+		if(camo_bipod)
+			select_gamemode_skin()
 
 	if(istype(loc, /obj/item/weapon/gun))
 		var/obj/item/weapon/gun/gun = loc
@@ -3676,6 +3731,7 @@ Defined in conflicts.dm of the #defines folder.
 	heavy_bipod = TRUE
 	// Disable gamemode skin for item state, but we explicitly force attach_icon gamemode skins
 	flags_atom = FPRINT|CONDUCT|NO_GAMEMODE_SKIN
+	camo_bipod = TRUE // this bipod has a camo skin
 
 /obj/item/attachable/bipod/vulture/Initialize(mapload, ...)
 	. = ..()
@@ -3707,6 +3763,41 @@ Defined in conflicts.dm of the #defines folder.
 	accuracy_mod = -HIT_ACCURACY_MULT_TIER_5
 	scatter_mod = SCATTER_AMOUNT_NONE
 	recoil_mod = RECOIL_AMOUNT_TIER_5
+
+/obj/item/attachable/bipod/m41ae2
+	name = "machinegun bipod"
+	desc = "A set of rugged telescopic poles to keep a weapon stabilized during firing."
+	icon_state = "bipod_m41ae2"
+	attach_icon = "bipod_m41ae2_a"
+	heavy_bipod = TRUE
+	camo_bipod = TRUE // this bipod has a camo skin
+
+/obj/item/attachable/bipod/m41ae2/Initialize(mapload, ...)
+	. = ..()
+	update_icon()
+
+/obj/item/attachable/bipod/m41ae2/select_gamemode_skin(expected_type, list/override_icon_state, list/override_protection)
+	. = ..() // We are forcing attach_icon skin
+	var/new_attach_icon
+	var/new_icon_state
+	switch(SSmapping.configs[GROUND_MAP].camouflage_type)
+		if("snow")
+			attach_icon = new_attach_icon ? new_attach_icon : "s_" + attach_icon
+			icon_state = new_icon_state ? new_icon_state : "s_" + icon_state
+			. = TRUE
+		if("desert")
+			attach_icon = new_attach_icon ? new_attach_icon : "d_" + attach_icon
+			icon_state = new_icon_state ? new_icon_state : "d_" + icon_state
+			. = TRUE
+		if("classic")
+			attach_icon = new_attach_icon ? new_attach_icon : "c_" + attach_icon
+			icon_state = new_icon_state ? new_icon_state : "c_" + icon_state
+			. = TRUE
+		if("urban")
+			attach_icon = new_attach_icon ? new_attach_icon : "u_" + attach_icon
+			icon_state = new_icon_state ? new_icon_state : "u_" + icon_state
+			. = TRUE
+	return .
 
 /obj/item/attachable/burstfire_assembly
 	name = "burst fire assembly"

code/modules/projectiles/guns/rifles.dm
@@ -1397,6 +1397,7 @@
 	fire_sound = 'sound/weapons/gun_hpr.ogg'
 
 	aim_slowdown = SLOWDOWN_ADS_LMG
+	map_specific_decoration = TRUE
 	current_mag = /obj/item/ammo_magazine/rifle/lmg
 	attachable_allowed = list(
 		/obj/item/attachable/suppressor,
@@ -1412,14 +1413,22 @@
 		/obj/item/attachable/burstfire_assembly,
 		/obj/item/attachable/magnetic_harness,
 		/obj/item/attachable/attached_gun/extinguisher,
+		/obj/item/attachable/stock/rifle/collapsible/m41ae2,
+		/obj/item/attachable/bipod/m41ae2,
 	)
 
+	pixel_x = -1
+
+	starting_attachment_types = list(
+		/obj/item/attachable/stock/rifle/collapsible/m41ae2,
+		/obj/item/attachable/bipod/m41ae2,
+	)
 	flags_gun_features = GUN_CAN_POINTBLANK|GUN_AMMO_COUNTER|GUN_WIELDED_FIRING_ONLY
 	gun_category = GUN_CATEGORY_HEAVY
 	start_automatic = TRUE
 
 /obj/item/weapon/gun/rifle/lmg/set_gun_attachment_offsets()
-	attachable_offset = list("muzzle_x" = 33, "muzzle_y" = 19,"rail_x" = 10, "rail_y" = 23, "under_x" = 23, "under_y" = 12, "stock_x" = 24, "stock_y" = 12)
+	attachable_offset = list("muzzle_x" = 34, "muzzle_y" = 19, "rail_x" = 12, "rail_y" = 23, "under_x" = 27, "under_y" = 13, "stock_x" = 14, "stock_y" = 15)
 
 /obj/item/weapon/gun/rifle/lmg/set_gun_config_values()
 	..()

code/modules/projectiles/guns/specialist/launcher/rocket_launcher.dm
@@ -153,6 +153,7 @@
 			rocket.forceMove(src)
 			replace_ammo(,rocket)
 			to_chat(user, SPAN_NOTICE("You load [rocket] into [src]."))
+			update_icon()
 			if(reload_sound)
 				playsound(user, reload_sound, 25, 1)
 			else
@@ -165,6 +166,7 @@
 		current_mag = rocket
 		rocket.forceMove(src)
 		replace_ammo(,rocket)
+		update_icon()
 	return TRUE
 
 /obj/item/weapon/gun/launcher/rocket/unload(mob/user,  reload_override = 0, drop_override = 0)
@@ -182,6 +184,7 @@
 			SPAN_NOTICE("You unload [ammo] from [src]."))
 			make_rocket(user, drop_override, 0)
 			current_mag.current_rounds = 0
+			update_icon()
 
 //Adding in the rocket backblast. The tile behind the specialist gets blasted hard enough to down and slightly wound anyone
 /obj/item/weapon/gun/launcher/rocket/apply_bullet_effects(obj/projectile/projectile_to_fire, mob/user, i = 1, reflex = 0)
@@ -217,6 +220,14 @@
 /obj/item/weapon/gun/launcher/rocket/m5
 	name = "\improper M5 RPG"
 	desc = "The M5 RPG is the primary anti-armor weapon of the USCM. Used to take out light-tanks and enemy structures, the M5 RPG is a dangerous weapon with a variety of combat uses."
+	map_specific_decoration = TRUE
+	has_empty_icon = TRUE
+	auto_retrieval_slot = WEAR_J_STORE
+	attachable_allowed = list()
+
+/obj/item/weapon/gun/launcher/rocket/m5/Fire(atom/target, mob/living/user, params, reflex, dual_wield)
+	. = ..()
+	update_icon()
 
 //-------------------------------------------------------
 //M5 RPG'S MEAN FUCKING COUSIN

code/modules/projectiles/magazines/rifles.dm
@@ -283,8 +283,31 @@
 	max_rounds = 300
 	gun_type = /obj/item/weapon/gun/rifle/lmg
 	flags_magazine = AMMUNITION_CANNOT_REMOVE_BULLETS|AMMUNITION_REFILLABLE|AMMUNITION_SLAP_TRANSFER
+	flags_atom = FPRINT|CONDUCT|MAP_COLOR_INDEX
 	ammo_band_icon = "+m41ae2_band"
 	ammo_band_icon_empty = "+m41ae2_band_e"
+
+/obj/item/ammo_magazine/rifle/lmg/Initialize(...)
+	. = ..()
+	select_gamemode_skin(type)
+
+/obj/item/ammo_magazine/rifle/lmg/select_gamemode_skin(expected_type, list/override_icon_state, list/override_protection)
+	. = ..()
+	var/new_base_icon_state
+	switch(SSmapping.configs[GROUND_MAP].camouflage_type)
+		if("snow")
+			base_mag_icon = new_base_icon_state ? new_base_icon_state : "s_" + initial(icon_state)
+			. = TRUE
+		if("desert")
+			base_mag_icon = new_base_icon_state ? new_base_icon_state : "d_" + initial(icon_state)
+			. = TRUE
+		if("classic")
+			base_mag_icon = new_base_icon_state ? new_base_icon_state : "c_" + initial(icon_state)
+			. = TRUE
+		if("urban")
+			base_mag_icon = new_base_icon_state ? new_base_icon_state : "u_" + initial(icon_state)
+			. = TRUE
+	return .
 
 /obj/item/ammo_magazine/rifle/lmg/holo_target
 	name = "\improper M41AE2 ammo box (10x24mm holo-target)"

code/modules/projectiles/magazines/specialist.dm
@@ -202,6 +202,7 @@
 	qdel(in_hand.current_mag)
 	in_hand.replace_ammo(user,src)
 	in_hand.current_mag = src
+	in_hand.update_icon()
 	forceMove(in_hand)
 	to_chat(user, SPAN_NOTICE("You load \the [src] into \the [demoman]'s [in_hand]."))
 	if(in_hand.reload_sound)

commit 452357208df42f0befc5ccf810706077b25d988b

Fix 360° HPR (#11502)
# About the pull request
Fixes being able to fire the HPR 360° with bipod without it undeploying,
Caused by #10425 which gave the HPR unique bipod the `heavy_bipod` var
set true, since it's not a documented change in the CL it is considered
an oversight

<img width="898" height="49" alt="image"
src="https://github.com/user-attachments/assets/9c34aa41-8108-4f8c-8e95-7f420197fdb3"
/>

<!-- Remove this text and explain what the purpose of your PR is.

Mention if you have tested your changes. If you changed a map, make sure
you used the mapmerge tool.
If this is an Issue Correction, you can type "Fixes Issue #169420" to
link the PR to the corresponding Issue number #169420.

Remember: something that is self-evident to you might not be to others.
Explain your rationale fully, even if you feel it goes without saying.
-->

# Explain why it's good for the game
Fixes a bug
# Testing Photographs and Procedure
<details>
<summary>Screenshots & Videos</summary>

Put screenshots and videos here with an empty line between the
screenshots and the `<details>` tags.

</details>


# Changelog
:cl:
fix: Fixes HPR being able to fire 360 degrees while bipoded without
undeploying
/:cl:

code/modules/projectiles/gun_attachables.dm
@@ -4180,7 +4180,7 @@ Defined in conflicts.dm of the #defines folder.
 	desc = "A set of rugged telescopic poles to keep a weapon stabilized during firing."
 	icon_state = "bipod_m41ae2"
 	attach_icon = "bipod_m41ae2_a"
-	heavy_bipod = TRUE
+	heavy_bipod = FALSE
 	camo_bipod = TRUE // this bipod has a camo skin
 
 /obj/item/attachable/bipod/m41ae2/Initialize(mapload, ...)
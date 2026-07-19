cmss13-devs/cmss13

commit e83031349172ea988ee62347174211f295a5bb64

Adds Emergency Close Incision surgeries, fixes some surgery issues (#11078)
# About the pull request

Adds the following features:
1. Multiple surgery steps can be skipped instead of only one step being
skipped.
2. Adds two new surgeries that allow you to close an incision without
fully preparing it first: one with a cautery, and one with a surgical
line. They have the same function as cauterizing and sewing a patient
up.
3. Fixes being able to execute certain surgery steps out of order.
4. Prevents the ability to cauterize/sew a patient early during an
incomplete surface or deep surgery.
5. Allows you to pinch bleeders during steps you couldn't pinch, before

# Explain why it's good for the game
Streamlining surgeries and being able to close autopsies faster is good.
1. Code only recognized that only one surgery step can be skipped. Now,
you can skip multiple steps, if they're ever implemented.
2. Fixes the occasional, "aw, fuck, wrong limb, sorry", or "I don't
wanna do two extra steps before closing an autopsy patient, so I'll just
stuff them in while they're all open."
3. Why are you closing the patient after you just drilled their
skull/ribcage in half? You monster. Stop it.
4. So... You removed the corneas, and then you cauterized the eyeballs.
Great. Now your patient is completely blind. Finish major surgeries
before you close your patient up!
5. "Aw fuck. Forgot to pinch after sawing the chest--wait, I can't
pinch? Maybe if I open up the bone--NOW I can pinch." Nah, you can pinch
here, now.

# Testing Photographs and Procedure

<details>
<summary>Screenshots & Videos</summary>

Drathek and I both tested. It works, prommy. Trust.

But if you don't, here's the proof.

![dreamseeker_2025-12-04_20-31-16](https://github.com/user-attachments/assets/6766feb8-a1fd-49e0-b08f-29c37438b9cf)
</details>


# Changelog

:cl: Drathek, Puckaboo2
add: You can close unprepared incisions without having to skip to using
the retractor, now.

code/modules/surgery/generic.dm
@@ -12,7 +12,11 @@
 	required_surgery_skill = SKILL_SURGERY_NOVICE
 	steps = list(
 		/datum/surgery_step/incision,
+		/datum/surgery_step/cauterize/abort,
+		/datum/surgery_step/suture_incision/abort,
 		/datum/surgery_step/clamp_bleeders_step,
+		/datum/surgery_step/cauterize/abort,
+		/datum/surgery_step/suture_incision/abort,
 		/datum/surgery_step/retract_skin,
 	)
 	lying_required = FALSE
@@ -395,6 +399,31 @@
 	log_interact(user, target, "[key_name(user)] failed to cauterize an incision in [key_name(target)]'s [surgery.affected_limb.display_name], aborting [surgery].")
 	return FALSE
 
+/datum/surgery_step/cauterize/abort
+	name = "Abort Surgery"
+	desc = "close the incision early"
+
+/datum/surgery_step/cauterize/abort/skip_step_criteria(mob/user, mob/living/carbon/target, target_zone, obj/item/tool, datum/surgery/surgery)
+	return TRUE //If you opened the wrong limb or you need to close an autopsy incision; this has you covered. Different from amputation abortion.
+
+/datum/surgery_step/cauterize/abort/success(mob/user, mob/living/carbon/target, target_zone, obj/item/tool, tool_type, datum/surgery/surgery)
+	user.affected_message(target,
+		SPAN_NOTICE("You cauterize the incision on [target]'s [surgery.affected_limb.display_name]."),
+		SPAN_NOTICE("[user] cauterizes the incision on your [surgery.affected_limb.display_name]."),
+		SPAN_NOTICE("[user] cauterizes the incision on [target]'s [surgery.affected_limb.display_name]."))
+	switch(target_zone)
+		if("head")
+			target.overlays -= image('icons/mob/humans/dam_human.dmi', "skull_surgery_closed")
+			target.overlays -= image('icons/mob/humans/dam_human.dmi', "skull_surgery_open")
+		if("chest")
+			target.overlays -= image('icons/mob/humans/dam_human.dmi', "chest_surgery_closed")
+			target.overlays -= image('icons/mob/humans/dam_human.dmi', "chest_surgery_open")
+	target.incision_depths[target_zone] = SURGERY_DEPTH_SURFACE
+	surgery.affected_limb.remove_all_bleeding(TRUE, FALSE)
+	target.pain.recalculate_pain()
+	complete(target, surgery)
+	log_interact(user, target, "[key_name(user)] cauterized an incision in [key_name(target)]'s [surgery.affected_limb.display_name], ending [surgery].")
+
 //////////////////////////////////////////////////////////////////
 // BONE-OPENING SURGERIES //
 //////////////////////////////////////////////////////////////////
@@ -406,6 +435,7 @@
 	required_surgery_skill = SKILL_SURGERY_TRAINED
 	steps = list(
 		/datum/surgery_step/saw_encased,
+		/datum/surgery_step/clamp_bleeders_step, //oop i forgor, also cuz you can't clamp bleeders here, normally, for some reason
 		/datum/surgery_step/open_encased_step,
 		/datum/surgery_step/mend_encased,
 	)
@@ -552,6 +582,7 @@
 	steps = list(
 		/datum/surgery_step/close_encased_step,
 		/datum/surgery_step/open_encased_step,
+		/datum/surgery_step/clamp_bleeders_step, //oop i forgor, also cuz you can't clamp bleeders here, normally, for some reason
 		/datum/surgery_step/mend_encased,
 	)
 	pain_reduction_required = PAIN_REDUCTION_HEAVY

code/modules/surgery/bones.dm
@@ -11,6 +11,7 @@
 	required_surgery_skill = SKILL_SURGERY_TRAINED
 	pain_reduction_required = PAIN_REDUCTION_HEAVY
 	steps = list(
+		/datum/surgery_step/clamp_bleeders_step, //oop i forgor, also cuz you can't clamp bleeders here, normally, for some reason
 		/datum/surgery_step/mend_bones,
 		/datum/surgery_step/set_bones,
 	)

code/modules/surgery/surgery_initiator.dm
@@ -117,7 +117,7 @@
 					else
 						hint_msg += ", [current_step.desc]"
 				else
-					hint_msg = "You can't [current_step.desc] with \the [tool]"
+					hint_msg = "You can't [current_step.desc] with [tool]"
 			if(!isnull(hint_msg))
 				to_chat(user, SPAN_WARNING("[hint_msg]."))
 		return FALSE

code/modules/surgery/surgery_procedure.dm
@@ -108,20 +108,31 @@
 			to_chat(user, SPAN_WARNING("You can't perform surgery on the same \
 				[user.zone_selected == "r_hand"||user.zone_selected == "l_hand" ? "hand":"arm"] you're using!"))
 			return FALSE
-	var/datum/surgery_step/current_step = GLOB.surgery_step_list[steps[status]]
-	if(current_step)
-		if(current_step.attempt_step(user, target, user.zone_selected, tool, src, repeating)) //First, try this step.
+	var/next = status
+	var/datum/surgery_step/current_step = GLOB.surgery_step_list[steps[next]]
+	var/list/attempted_steps = list()
+	while(current_step)
+		// attempt the step
+		if(current_step.attempt_step(user, target, user.zone_selected, tool, src, repeating, next-status))
 			return TRUE
-		var/datum/surgery_step/next_step
-		if(current_step.skip_step_criteria(user, target, user.zone_selected, tool, src) && status < length(steps)) //If that doesn't work but the step is optional and not the last in the list, try the next step.
-			next_step = GLOB.surgery_step_list[steps[status + 1]]
-			if(next_step.attempt_step(user, target, user.zone_selected, tool, src, skipped = TRUE))
-				return TRUE
-		if(tool && is_surgery_tool(tool)) //Just because you used the wrong tool doesn't mean you meant to whack the patient with it...
-			if(next_step)
-				to_chat(user, SPAN_WARNING("You can't [current_step.desc] with \the [tool], or [next_step.desc]."))
-			else
-				to_chat(user, SPAN_WARNING("You can't [current_step.desc] with \the [tool]."))
-			return FALSE //...but you might be wanting to use it on them anyway. If on help intent, the help-intent safety will apply for this attack.
-	return FALSE
+		attempted_steps += current_step
+		// check if its an optional step
+		if(!current_step.skip_step_criteria(user, target, user.zone_selected, tool, src))
+			if(tool && is_surgery_tool(tool)) //Just because you used the wrong tool doesn't mean you meant to whack the patient with it...
+				var/hint_msg
+				for(var/datum/surgery_step/step as anything in attempted_steps)
+					if(hint_msg)
+						if(step == current_step)
+							hint_msg += ", or [step.desc]"
+						else
+							hint_msg += ", [step.desc]"
+					else
+						hint_msg = "You can't [step.desc] with [tool]"
+				to_chat(user, SPAN_WARNING("[hint_msg]."))
+			return FALSE
+		// step was optional, try the next if it exists
+		if(++next > length(steps))
+			break
+		current_step = GLOB.surgery_step_list[steps[next]]
 
+	return FALSE

code/modules/surgery/surgery_steps.dm
@@ -238,10 +238,7 @@ affected_limb, or location vars. Also, in that case there may be a wait between 
 		tool.update_icon() // in order to not reset shit too far.
 
 	if(advance)
-		if(skipped) //Skipped previous step.
-			surgery.status += 2
-		else
-			surgery.status++
+		surgery.status += max(skipped + 1, 1)
 		if(surgery.status > length(surgery.steps))
 			complete(target, surgery)
 

code/modules/surgery/tendwounds.dm
@@ -63,3 +63,43 @@
 /datum/surgery_step/suture_incision/failure(mob/user, mob/living/carbon/target, target_zone, obj/item/tool, tool_type, datum/surgery/surgery)
 	log_interact(user, target, "[key_name(user)] failed to suture the incision in [key_name(target)]'s [surgery.affected_limb.display_name] with \the [tool].")
 	return FALSE
+
+//------------------------------------
+//for closing incisions early
+/datum/surgery_step/suture_incision/abort
+	name = "Abort Surgery"
+	desc = "suture the incision early"
+
+/datum/surgery_step/suture_incision/abort/skip_step_criteria(mob/user, mob/living/carbon/target, target_zone, obj/item/tool, datum/surgery/surgery)
+	return TRUE //If you opened the wrong limb or you need to close an autopsy incision; this has you covered. Different from amputation abortion.
+
+/datum/surgery_step/suture_incision/abort/success(mob/user, mob/living/carbon/human/target, target_zone, obj/item/tool, tool_type, datum/surgery/surgery)
+	var/added_sutures = SEND_SIGNAL(surgery.affected_limb, COMSIG_LIMB_ADD_SUTURES, TRUE)
+	if(!added_sutures) //No suture datum to answer the signal
+		new /datum/suture_handler(surgery.affected_limb)
+		added_sutures = SEND_SIGNAL(surgery.affected_limb, COMSIG_LIMB_ADD_SUTURES, TRUE) //This time, with feeling.
+
+	switch(target_zone)
+		if("head")
+			target.overlays -= image('icons/mob/humans/dam_human.dmi', "skull_surgery_closed")
+			target.overlays -= image('icons/mob/humans/dam_human.dmi', "skull_surgery_open")
+		if("chest")
+			target.overlays -= image('icons/mob/humans/dam_human.dmi', "chest_surgery_closed")
+			target.overlays -= image('icons/mob/humans/dam_human.dmi', "chest_surgery_open")
+	if(added_sutures & SUTURED_FULLY)
+		user.affected_message(target,
+			SPAN_NOTICE("You close the incision on [target]'s [surgery.affected_limb.display_name] with a line of neat sutures."),
+			SPAN_NOTICE("[user] closes the incision on your [surgery.affected_limb.display_name] with a line of neat sutures. It stops hurting."),
+			SPAN_NOTICE("[user] closes the incision on [target]'s [surgery.affected_limb.display_name] with a line of neat sutures."))
+		complete(target, surgery)
+
+		log_interact(user, target, "[key_name(user)] finished suturing an incision in [key_name(target)]'s [surgery.affected_limb.display_name] with [tool], ending [surgery].")
+	else
+		user.affected_message(target,
+			SPAN_NOTICE("You close the incision on [target]'s [surgery.affected_limb.display_name] with a line of neat sutures, but some injuries remain."),
+			SPAN_NOTICE("[user] closes the incision on your [surgery.affected_limb.display_name] with a line of neat sutures, but you're still hurting!"),
+			SPAN_NOTICE("[user] closes the incision on [target]'s [surgery.affected_limb.display_name] with a line of neat sutures, but some injuries remain."))
+
+		log_interact(user, target, "[key_name(user)] finished suturing an incision in [key_name(target)]'s [surgery.affected_limb.display_name] with [tool], ending [surgery].")
+
+	target.incision_depths[target_zone] = SURGERY_DEPTH_SURFACE

commit 0593a78d7907291170fb251e848c278a6e2f2911

fix IMS / Laser Scalpel  not skipping steps properly (#12672)
# About the pull request

<!-- Remove this text and explain what the purpose of your PR is.

Mention if you have tested your changes. If you changed a map, make sure
you used the mapmerge tool.
If this addresses a reported issue, you can include "Fixes #92345" to
link the PR to the corresponding Issue number #92345.
For multiple issues you must include the "Fixes" keyword for each, e.g.
"Fixes #92345, Fixes #92346, Fixes #92347". You cannot use multiple
issue numbers with only one keyword.

Remember: something that is self-evident to you might not be to others.
Explain your rationale fully, even if you feel it goes without saying.
-->

Oversight introduced in #11078 - the additional abort steps added mean
that IMS and Laser Scalpel also need to skip more steps

Fixes #12671

# Testing Photographs and Procedure
Local tested by making sure you can use IMS on Chest then Circular Saw,
which was not possible before. If there are side effects, I'm clueless.

# Changelog
:cl:
fix: The IMS and Laser Scalpel will now once again skip their associated
steps.
/:cl:

Co-authored-by: Fira <firazygit@gmail.com>

code/modules/surgery/generic.dm
@@ -61,7 +61,7 @@
 			SPAN_NOTICE("[user] has constructed a prepared incision in your [surgery.affected_limb.display_name]."),
 			SPAN_NOTICE("[user] has constructed a prepared incision in [target]'s [surgery.affected_limb.display_name]."))
 
-		surgery.status += 2 //IMS completes all steps.
+		surgery.status += 6 //IMS completes all steps.
 
 		switch(target_zone) //forces application of overlays
 			if("chest")
@@ -75,7 +75,7 @@
 			SPAN_NOTICE("[user] finishes making a bloodless incision on your [surgery.affected_limb.display_name] with \the [tool]."),
 			SPAN_NOTICE("[user] finishes making a bloodless incision on [target]'s [surgery.affected_limb.display_name] with \the [tool]."))
 
-		surgery.status++ //A laser scalpel may cauterise as it cuts.
+		surgery.status += 3 //A laser scalpel may cauterise as it cuts.
 	else
 		user.affected_message(target,
 			SPAN_NOTICE("You finish the incision on [target]'s [surgery.affected_limb.display_name]."),
@@ -87,7 +87,7 @@
 			incision_bleed.duration = 10 MINUTES //A weak bleed, but it doesn't stop on its own.
 			surgery.affected_limb.bleeding_effects_list += incision_bleed
 		else
-			surgery.status++ // synth skin doesn't cause bleeders
+			surgery.status += 3 // synth skin doesn't cause bleeders
 
 	target.incision_depths[target_zone] = SURGERY_DEPTH_SHALLOW //Descriptionwise this is done by the retractor, but putting it here means people can examine to see if an unfinished surgery has been done.
 	user.add_blood(target.get_blood_color(), BLOOD_HANDS)
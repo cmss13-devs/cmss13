541aa18178743e59a16e2389954c529593e35682
Drulikar <drathek@gmail.com>
2026-09-20 19:43:25 -0500

    bump version

--- dependencies.sh
+++ dependencies.sh
@@ -17,7 +17,7 @@ export NODE_VERSION_LTS=22.14.0
 export BUN_VERSION=1.3.14
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.11
+export SPACEMAN_DMM_VERSION=suite-1.12
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.11.9
